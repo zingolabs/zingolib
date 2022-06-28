00ea83d00ec5b676f38b83987d35e52a93574240
Hazel OHearn <gygaxis@zingolabs.org>
2022-06-28 13:05:02 -0300

    Factor scan_orchard_bundle into incomplete helper

--- lib/src/blaze/fetch_full_transaction.rs
+++ lib/src/blaze/fetch_full_transaction.rs
@@ -9,7 +9,7 @@ use crate::{
 
 use futures::{future::join_all, stream::FuturesUnordered, StreamExt};
 use log::info;
-use orchard::note_encryption::OrchardDomain;
+use orchard::{keys::IncomingViewingKey as OrchardIvk, note_encryption::OrchardDomain};
 use std::{
     collections::HashSet,
     convert::{TryFrom, TryInto},
@@ -27,7 +27,7 @@ use tokio::{
     task::JoinHandle,
 };
 use zcash_client_backend::encoding::encode_payment_address;
-use zcash_note_encryption::try_note_decryption;
+use zcash_note_encryption::{try_note_decryption, Domain, ShieldedOutput, ENC_CIPHERTEXT_SIZE};
 
 use zcash_primitives::{
     consensus::BlockHeight,
@@ -558,52 +558,73 @@ impl FetchFullTxns {
         is_outgoing_transaction: &mut bool,
         outgoing_metadatas: &mut Vec<OutgoingTxMetadata>,
     ) {
-        use orchard::keys::Scope;
-        //Todo: Implement this!
-        let fvks = keys
-            .read()
-            .await
-            .get_all_orchard_keys_of_type::<orchard::keys::FullViewingKey>();
-        if let Some(o_bundle) = transaction.orchard_bundle() {
-            for action in o_bundle.actions().iter() {
-                for (i, ivk) in fvks
-                    .iter()
-                    .map(|fvk| (fvk.to_ivk(Scope::External)))
-                    .enumerate()
-                {
-                    let (note, to, memo_bytes) =
-                        match try_note_decryption(&OrchardDomain::for_action(action), &ivk, action)
-                        {
-                            Some(ret) => ret,
-                            None => continue,
-                        };
-                    let memo_bytes = MemoBytes::from_bytes(memo_bytes.as_slice()).unwrap();
-                    let memo = memo_bytes
-                        .clone()
-                        .try_into()
-                        .unwrap_or(Memo::Future(memo_bytes));
-                    println!("Recieved orchard tx with memo {:?}", memo);
-                    //This logic is all sapling specific! Todo: implement orchard version
-                    /*
-                    if unconfirmed {
-                        wallet_transactions.write().await.add_pending_note(
-                            transaction.txid(),
-                            height,
-                            block_time as u64,
-                            note.clone(),
-                            to,
-                            &extfvks.get(i).unwrap(),
-                        );
-                    }
+        scan_bundle(
+            config,
+            transaction,
+            height,
+            unconfirmed,
+            block_time,
+            keys,
+            wallet_transactions,
+            is_outgoing_transaction,
+            outgoing_metadatas,
+            Keys::okeys,
+            |t: &Transaction| t.orchard_bundle(),
+            orchard::Bundle::actions,
+            |key| OrchardIvk::try_from(&key.key),
+            OrchardDomain::for_action,
+            <[u8; 512]>::as_slice,
+        )
+        .await;
+    }
+}
 
-                    let memo = memo_bytes.clone().try_into().unwrap_or(Memo::Future(memo_bytes));
-                    wallet_transactions
-                        .write()
-                        .await
-                        .add_memo_to_note(&transaction.txid(), note, memo);
-                    */
-                }
-            }
+async fn scan_bundle<K, B, B2, O, D, E>(
+    config: &LightClientConfig,
+    transaction: &Transaction,
+    height: BlockHeight,
+    unconfirmed: bool,
+    block_time: u32,
+    keys: &Arc<RwLock<Keys>>,
+    wallet_transactions: &Arc<RwLock<WalletTxns>>,
+    is_outgoing_transaction: &mut bool,
+    outgoing_metadatas: &mut Vec<OutgoingTxMetadata>,
+    key_reader: impl Fn(&Keys) -> &Vec<K>,
+    transaction_to_bundle: impl Fn(&Transaction) -> Option<&B>,
+    bundle_to_output: impl Fn(&B) -> &B2,
+    key_to_ivk: impl Fn(&K) -> Result<<D as Domain>::IncomingViewingKey, E>,
+    output_to_domain: impl Fn(&O) -> D,
+    memo_slice: impl Fn(&D::Memo) -> &[u8],
+) where
+    K: Clone,
+    for<'a> &'a B2: IntoIterator<Item = &'a O>,
+    D: Domain,
+    O: ShieldedOutput<D, ENC_CIPHERTEXT_SIZE>,
+{
+    let keys = key_reader(&*keys.read().await).clone();
+    let temp: Option<&B> = transaction_to_bundle(transaction);
+    for output in temp
+        .into_iter()
+        .flat_map(|bundle: &B| bundle_to_output(bundle))
+    {
+        for key in &keys {
+            let (note, to, memo_bytes) = match key_to_ivk(key)
+                .map(|ivk| try_note_decryption(&output_to_domain(&output), &ivk, output))
+            {
+                Ok(Some(ret)) => ret,
+                _ => continue,
+            };
+            let memo_bytes = MemoBytes::from_bytes(memo_slice(&memo_bytes)).unwrap();
+            let memo = memo_bytes
+                .clone()
+                .try_into()
+                .unwrap_or(Memo::Future(memo_bytes));
+            println!(
+                "Recieved {} tx with memo {memo:?}",
+                std::any::type_name::<D>()
+            );
+            //When this can work as a full replacement for scan_sapling_bundle, that
+            //will likely mean the orchard functionality here is complete
         }
     }
 }
