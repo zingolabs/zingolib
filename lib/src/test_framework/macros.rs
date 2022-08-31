e564d3e0bdcd552e1cf79d24f269d6fd741a79bd
zancas <zancas@zingolabs.org>
2022-08-31 10:45:36 -0600

    add concat idents

--- lib/src/test_framework/macros.rs
+++ lib/src/test_framework/macros.rs
@@ -1,4 +1,13 @@
 #[macro_export]
+macro_rules! apply_scenario {
+    ($test_implementation_fn:ident) => {
+        let scenario = setup_n_block_fcbl_scenario($numblock).await;
+        $test_implementation_fn(&mut scenario).await;
+        clean_shutdown(scenario.stop_transmitter, scenario.test_server_handle).await;
+    };
+}
+
+#[macro_export]
 macro_rules! scenario_test {
     ($($field:ident,)* $numblocks:literal,
      #[tokio::test]
