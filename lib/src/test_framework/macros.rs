1bf77a0637e1002e5ddda093f52684766f3c48c2
zancas <zancas@zingolabs.org>
2022-08-30 21:49:30 -0600

    wip_stash

--- lib/src/test_framework/macros.rs
+++ lib/src/test_framework/macros.rs
@@ -25,4 +25,28 @@ macro_rules! scenario_test {
             clean_shutdown($stop_transmitter, $test_server_handle).await;
         }
     };
+    ($stop_transmitter:ident,
+     $test_server_handle:ident,
+     $lightclient:ident,
+     $fake_compactblock_list:ident,
+     $config:ident,
+     $numblocks:tt,
+     #[tokio::test]
+     async fn $test_name:ident()
+        $implementation:block
+    ) => {
+        #[tokio::test]
+        async fn $test_name() {
+            let NBlockFCBLScenario {
+                $stop_transmitter,
+                $test_server_handle,
+                $lightclient,
+                $fake_compactblock_list,
+                $config,
+                ..
+            } = setup_n_block_fcbl_scenario($numblocks).await;
+            $implementation
+            clean_shutdown($stop_transmitter, $test_server_handle).await;
+        }
+    };
 }
