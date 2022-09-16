f32a36969f61dc6df995567aa03858c7e44de1cc
Hazel OHearn <gygaxis@zingolabs.org>
2022-09-16 14:59:00 -0300

    Scenario changes

--- cli/examples/scenarios.rs
+++ cli/examples/scenarios.rs
@@ -1,5 +1,7 @@
 #![forbid(unsafe_code)]
-use tokio::runtime::Runtime;
+use std::{path::PathBuf, process::Command, time::Duration};
+
+use tokio::{runtime::Runtime, time::sleep};
 use zingo_cli::regtest::{ChildProcessHandler, RegtestManager};
 use zingoconfig::ZingoConfig;
 use zingolib::{create_zingoconf_with_datadir, lightclient::LightClient};
@@ -7,6 +9,7 @@ use zingolib::{create_zingoconf_with_datadir, lightclient::LightClient};
 fn main() {
     send_mined_sapling_to_orchard()
 }
+
 #[test]
 fn prove_scenario_is_built() {}
 
@@ -24,7 +27,9 @@ fn create_zcash_conf_path(base: &str) -> std::path::PathBuf {
 ///  (1) existing accesibly for test code in: cli/examples/mineraddress_sapling_spendingkey
 ///  (2) corresponding to the address registered as the "mineraddress" field in cli/examples/zcash.conf
 fn coinbasebacked_spendcapable_setup() -> (RegtestManager, ChildProcessHandler, LightClient) {
+    tracing_subscriber::fmt::init();
     let coinbase_spendkey = include_str!("mineraddress_sapling_spendingkey").to_string();
+    let sapling_key = include_str!("sapling_regtest_secret_extended_key").to_string();
     let mut regtest_manager = RegtestManager::new();
     regtest_manager.zcashd_config = create_zcash_conf_path("externalwallet_coinbaseaddress.conf");
     let child_process_handler = regtest_manager.launch(true).unwrap();
@@ -56,21 +61,40 @@ fn send_mined_sapling_to_orchard() {
     let (regtest_manager, _child_process_handler, client) = coinbasebacked_spendcapable_setup();
     regtest_manager.generate_n_blocks(5).unwrap();
     let runtime = Runtime::new().unwrap();
-    runtime.block_on(client.do_sync(true)).unwrap();
+    runtime.block_on(async {
+        sleep(Duration::from_secs(2)).await;
+        let sync_status = client.do_sync(true).await.unwrap();
+        println!("{}", json::stringify_pretty(sync_status, 4));
 
-    let o_addr = runtime.block_on(client.do_new_address("o")).unwrap()[0].take();
-    println!("{o_addr}");
-    let send_status = runtime
-        .block_on(client.do_send(vec![(
-            o_addr.to_string().as_str(),
-            5000,
-            Some("Scenario test: engage!".to_string()),
-        )]))
-        .unwrap();
+        let o_addr = client.do_new_address("o").await.unwrap()[0].take();
+        println!("{o_addr}");
+        let send_status = client
+            .do_send(vec![(
+                o_addr.to_string().as_str(),
+                5000,
+                Some("Scenario test: engage!".to_string()),
+            )])
+            .await
+            .unwrap();
+        println!("Send status: {send_status}");
 
-    regtest_manager.generate_n_blocks(2).unwrap();
+        regtest_manager.generate_n_blocks(2).unwrap();
+        sleep(Duration::from_secs(2)).await;
 
-    let balance = runtime.block_on(client.do_balance());
-    assert_eq!(balance["unverified_orchard_balance"], 5000);
-    assert_eq!(balance["verified_orchard_balance"], 0);
+        client.do_sync(true).await.unwrap();
+        let balance = client.do_balance().await;
+        let transactions = client.do_list_transactions(false).await;
+        println!("{}", json::stringify_pretty(balance.clone(), 4));
+        println!("{}", json::stringify_pretty(transactions, 4));
+        assert_eq!(balance["unverified_orchard_balance"], 5000);
+        assert_eq!(balance["verified_orchard_balance"], 0);
+
+        regtest_manager.generate_n_blocks(4).unwrap();
+        sleep(Duration::from_secs(2)).await;
+        client.do_sync(true).await.unwrap();
+        let balance = client.do_balance().await;
+        println!("{}", json::stringify_pretty(balance.clone(), 4));
+        assert_eq!(balance["unverified_orchard_balance"], 0);
+        assert_eq!(balance["verified_orchard_balance"], 5000);
+    });
 }
