f02bc75936e970198444241101802d6805fa36f3
zancas <zancas@zingolabs.org>
2022-09-16 16:02:58 -0600

    wip

--- cli/tests/data/mod.rs
+++ cli/tests/data/mod.rs
@@ -29,11 +29,10 @@ rpcallowip=127.0.0.1
 
 ### Zcashd Help provides documentation of the following:
 mineraddress=zregtestsapling1fkc26vpg566hgnx33n5uvgye4neuxt4358k68atnx78l5tg2dewdycesmr4m5pn56ffzsa7lyj6
-minetolocalwallet=0 # This is set to false so that we can mine to a wallet, other than the zcashd wallet."
-    
+minetolocalwallet=0 # This is set to false so that we can mine to a wallet, other than the zcashd wallet.";
 
 pub const SECRET_SPEND_AUTH_SAPLING: &str = "\
-secret-extended-key-regtest1qwxwxvvsqqqqpqpscrwl4x0sahmtm7j3wgl2q4n44c8wzkdf5q04wue4wpjsvtw9js33wjet4582cwkhwnrgu82nzps7v7mnk3htzac0qaskl4vjlacs8xstgfedq0yhp8t2t8k8r28telx77vkc9jx506wcl7yxvucwjys2rk59627kv92kgqp8nqujmmt3vnln7ytlwjm53euylkyruft54lg34c7ne2w6sc9c2wam3yne5t2jvh7458hezwyfaxljxvunqwwlancespz6n"
-    
+secret-extended-key-regtest1qwxwxvvsqqqqpqpscrwl4x0sahmtm7j3wgl2q4n44c8wzkdf5q04wue4wpjsvtw9js33wjet4582cwkhwnrgu82nzps7v7mnk3htzac0qaskl4vjlacs8xstgfedq0yhp8t2t8k8r28telx77vkc9jx506wcl7yxvucwjys2rk59627kv92kgqp8nqujmmt3vnln7ytlwjm53euylkyruft54lg34c7ne2w6sc9c2wam3yne5t2jvh7458hezwyfaxljxvunqwwlancespz6n";
+
 pub const SAPLING_ADDRESS_FROM_SPEND_AUTH: &str = "\
-zregtestsapling1fkc26vpg566hgnx33n5uvgye4neuxt4358k68atnx78l5tg2dewdycesmr4m5pn56ffzsa7lyj6"
+zregtestsapling1fkc26vpg566hgnx33n5uvgye4neuxt4358k68atnx78l5tg2dewdycesmr4m5pn56ffzsa7lyj6";
