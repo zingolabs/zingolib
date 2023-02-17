3a84594f6031e4f1b0b108551625fdddc069b904
zancas <zancas@zingolabs.org>
2023-02-17 12:11:10 -0700

    remove unspent from wallet requires a wallet serialization upgrade, this would test that

--- zingocli/tests/integration_tests.rs
+++ zingocli/tests/integration_tests.rs
@@ -124,6 +124,29 @@ fn mine_sapling_to_self() {
 }
 
 #[test]
+fn remove_unspent_from_wallet() {
+    let (regtest_manager, child_process_handler, mut client_builder) =
+        scenarios::sapling_funded_client();
+    let client = client_builder.build_funded_client(0, false);
+    let client_receiving =
+        client_builder.build_newseed_client(HOSPITAL_MUSEUM_SEED.to_string(), 0, false);
+    Runtime::new().unwrap().block_on(async {
+        utils::increase_height_and_sync_client(&regtest_manager, &client, 5).await;
+
+        let balance = client.do_balance().await;
+        assert_eq!(balance["sapling_balance"], 3_750_000_000u64);
+        client
+            .do_send(vec![(
+                get_base_address!(client_receiving, "unified").as_str(),
+                5_000,
+                Some("this note never makes it to the wallet! or chain".to_string()),
+            )])
+            .await
+            .unwrap()
+    });
+    drop(child_process_handler);
+}
+#[test]
 fn send_mined_sapling_to_orchard() {
     //! This test shows the 5th confirmation changing the state of balance by
     //! debiting unverified_orchard_balance and crediting verified_orchard_balance.  The debit amount is
