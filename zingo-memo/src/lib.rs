5115b85a09578686e9592e8f56c8fb83c209c4eb
Hazel OHearn <gygaxis@zingolabs.org>
2023-03-14 15:23:19 -0300

    Update parsed_memo version comment

--- zingo-memo/src/lib.rs
+++ zingo-memo/src/lib.rs
@@ -4,10 +4,18 @@ use zcash_primitives::consensus::{BlockHeight, MAIN_NETWORK};
 pub mod memo_serde;
 pub mod utils;
 
-/// A parsed memo. Currently there is only one version of this protocol,
-/// which is a list of UAs. The main use-case for this is to record the
+/// A parsed memo. Currently there are two version of this protocol.
+///
+/// Version zero is a list of UAs. The main use-case for this is to record the
 /// UAs sent from, as the blockchain only records the pool-specific receiver
 /// corresponding to the key we sent with.
+///
+/// Version one extends version zero with a vec of heights and indexes.
+/// The intended use for this format is to encode the height-on-chain, and
+/// index-in-block of a set of transactions. This can be used to note previous transactions
+/// of note, allowing for a rescan to find the highest of these memos, and learn
+/// all previous transactions of note (potentially chaining multiple memos), and
+/// remove the need to trial-decrypt any transactions not noted this way.
 #[non_exhaustive]
 #[derive(Debug)]
 pub enum ParsedMemo {
