ea245a7792c4665c357ce51d06f8d8c44a23e1dc
zancas <zancas@zingolabs.org>
2023-05-16 18:34:52 -0600

    make is_incoming/outgoing methods

--- zingolib/src/wallet/data.rs
+++ zingolib/src/wallet/data.rs
@@ -703,7 +703,12 @@ impl TransactionMetadata {
         self.total_value_spent() - (self.value_outgoing() + self.total_change_returned())
     }
     pub fn is_outgoing_transaction(&self) -> bool {
-        self.total_value_spent() > 0
+        !self.outgoing_tx_data.is_empty()
+    }
+    pub fn is_incoming_transaction(&self) -> bool {
+        !(self.sapling_notes.is_empty()
+            && self.orchard_notes.is_empty()
+            && self.received_utxos.is_empty())
     }
     pub fn net_spent(&self) -> u64 {
         assert!(self.is_outgoing_transaction());
