68eaf08df6c3117ed865cc0102aad685fa55fc86
Oscar Pepper <oscarpepper@zingolabs.org>
2023-06-02 14:17:25 +0100

    changed initial min_height from 0 to block height of first tx in summary

--- zingolib/src/lightclient.rs
+++ zingolib/src/lightclient.rs
@@ -789,7 +789,7 @@ impl LightClient {
     }
     pub async fn do_get_birthday(&self) -> u64 {
         let summaries = self.do_list_txsummaries().await;
-        let mut min_height = 0;
+        let mut min_height = u64::from(summaries[0].block_height);
         for summary in summaries {
             if u64::from(summary.block_height) < min_height {
                 min_height = u64::from(summary.block_height);
