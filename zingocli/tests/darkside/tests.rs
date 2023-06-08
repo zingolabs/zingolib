27c2465b0d864cc423ccc577cd89f105c361820e
Hazel OHearn <gygaxis@zingolabs.org>
2023-06-08 12:54:15 -0300

    Add genesis block constant to darkside

--- zingocli/tests/darkside/tests.rs
+++ zingocli/tests/darkside/tests.rs
@@ -125,75 +125,21 @@ async fn prepare_darksidewalletd(uri: http::Uri) -> Result<(), String> {
 
     // reset with parameters
     connector
-        .reset(663_150, String::from("2bb40e60"), String::from("regtest"))
+        .reset(1, String::from("2bb40e60"), String::from("regtest"))
         .await
         .unwrap();
 
     connector
         .stage_blocks_stream(vec![String::from(
-            crate::darkside::constants::ORCHARD_FUNDED_100ZEC_BLOCK,
+            crate::darkside::constants::GENESIS_BLOCK,
         )])
         .await?;
 
-    sleep(std::time::Duration::new(2, 0)).await;
-
-    let sapling_activation_tree = TreeState {
-        network: String::from("main"),
-        height: 663150,
-        hash: String::from("0000000002fd3be4c24c437bd22620901617125ec2a3a6c902ec9a6c06f734fc"),
-        time: 1576821833,
-        sapling_tree: String::from("01ec6278a1bed9e1b080fd60ef50eb17411645e3746ff129283712bc4757ecc833001001b4e1d4a26ac4a2810b57a14f4ffb69395f55dde5674ecd2462af96f9126e054701a36afb68534f640938bdffd80dfcb3f4d5e232488abbf67d049b33a761e7ed6901a16e35205fb7fe626a9b13fc43e1d2b98a9c241f99f93d5e93a735454073025401f5b9bcbf3d0e3c83f95ee79299e8aeadf30af07717bda15ffb7a3d00243b58570001fa6d4c2390e205f81d86b85ace0b48f3ce0afb78eeef3e14c70bcfd7c5f0191c0000011bc9521263584de20822f9483e7edb5af54150c4823c775b2efc6a1eded9625501a6030f8d4b588681eddb66cad63f09c5c7519db49500fc56ebd481ce5e903c22000163f4eec5a2fe00a5f45e71e1542ff01e937d2210c99f03addcce5314a5278b2d0163ab01f46a3bb6ea46f5a19d5bdd59eb3f81e19cfa6d10ab0fd5566c7a16992601fa6980c053d84f809b6abcf35690f03a11f87b28e3240828e32e3f57af41e54e01319312241b0031e3a255b0d708750b4cb3f3fe79e3503fe488cc8db1dd00753801754bb593ea42d231a7ddf367640f09bbf59dc00f2c1d2003cc340e0c016b5b13"),
-        orchard_tree: String::from(""),
-    };
-
-    connector.add_tree_state(sapling_activation_tree).await?;
-
-    let first_transaction_state = TreeState {
-        network: String::from("main"),
-        height: 663173,
-        hash: String::from("0000000001b80f69c0b4a02228b59f2793ba9a4c3e8742fe56b9c9e331a6005d"),
-        time: 1576823459,
-        sapling_tree: String::from("018310f940d4b52399e656d8a090bd87f64a4abe80fffb2b4a882675def41f943c0114dd70a8a8a22869785b86b1dcad5e9508f419aad84ef8e83d50ec061117022310000199517be06af7c07c2d393c590c62add4dbcd9cc3402167521786f91a5d01d538000001989561014441f9f9043e11c01e220730df2219c090fa02f58d278fb7f447271601fa6d4c2390e205f81d86b85ace0b48f3ce0afb78eeef3e14c70bcfd7c5f0191c0000011bc9521263584de20822f9483e7edb5af54150c4823c775b2efc6a1eded9625501a6030f8d4b588681eddb66cad63f09c5c7519db49500fc56ebd481ce5e903c22000163f4eec5a2fe00a5f45e71e1542ff01e937d2210c99f03addcce5314a5278b2d0163ab01f46a3bb6ea46f5a19d5bdd59eb3f81e19cfa6d10ab0fd5566c7a16992601fa6980c053d84f809b6abcf35690f03a11f87b28e3240828e32e3f57af41e54e01319312241b0031e3a255b0d708750b4cb3f3fe79e3503fe488cc8db1dd00753801754bb593ea42d231a7ddf367640f09bbf59dc00f2c1d2003cc340e0c016b5b13"),
-        orchard_tree: String::from(""),
-    };
-
-    connector.add_tree_state(first_transaction_state).await?;
-
-    let second_transaction_state = TreeState {
-        network: String::from("main"),
-        height: 663187,
-        hash: String::from("00000000027fa4bfd6c012325a44eef7211a6162b5979507f07603333e9b3068"),
-        time: 1576824317,
-        sapling_tree: String::from("018d5b2b12a89cabbeb6c98bde09fbea143d49ea8e4dbf1d612e4906e73e1af96b001000018b01c7e7b2b183d022fc35e351e6423aee8885debc899036d0bc3b389c9f161501dba0595ce728b41452a9c595341074cf01e1152abe401db2b30a9ab007ad006e0001989561014441f9f9043e11c01e220730df2219c090fa02f58d278fb7f447271601fa6d4c2390e205f81d86b85ace0b48f3ce0afb78eeef3e14c70bcfd7c5f0191c0000011bc9521263584de20822f9483e7edb5af54150c4823c775b2efc6a1eded9625501a6030f8d4b588681eddb66cad63f09c5c7519db49500fc56ebd481ce5e903c22000163f4eec5a2fe00a5f45e71e1542ff01e937d2210c99f03addcce5314a5278b2d0163ab01f46a3bb6ea46f5a19d5bdd59eb3f81e19cfa6d10ab0fd5566c7a16992601fa6980c053d84f809b6abcf35690f03a11f87b28e3240828e32e3f57af41e54e01319312241b0031e3a255b0d708750b4cb3f3fe79e3503fe488cc8db1dd00753801754bb593ea42d231a7ddf367640f09bbf59dc00f2c1d2003cc340e0c016b5b13"),
-        orchard_tree: String::from(""),
-    };
-
-    connector.add_tree_state(second_transaction_state).await?;
-
-    let third_transaction_state = TreeState {
-        network: String::from("main"),
-        height: 663189,
-        hash: String::from("00000000025a03b6b11364b9c01a5fc9a927eca98ae899bb88f0ddf2de77fda3"),
-        time: 1576824596,
-        sapling_tree: String::from("01f1c5f08a96b8e0befe04ff061dd51ed96ffcc7fcd96d011dc06255692989731c001001ea6dbe4e95ec503600ee3260105ffcc2ceb63eb34ec323c794eebfc49b7beb2c018b01c7e7b2b183d022fc35e351e6423aee8885debc899036d0bc3b389c9f161501dba0595ce728b41452a9c595341074cf01e1152abe401db2b30a9ab007ad006e0001989561014441f9f9043e11c01e220730df2219c090fa02f58d278fb7f447271601fa6d4c2390e205f81d86b85ace0b48f3ce0afb78eeef3e14c70bcfd7c5f0191c0000011bc9521263584de20822f9483e7edb5af54150c4823c775b2efc6a1eded9625501a6030f8d4b588681eddb66cad63f09c5c7519db49500fc56ebd481ce5e903c22000163f4eec5a2fe00a5f45e71e1542ff01e937d2210c99f03addcce5314a5278b2d0163ab01f46a3bb6ea46f5a19d5bdd59eb3f81e19cfa6d10ab0fd5566c7a16992601fa6980c053d84f809b6abcf35690f03a11f87b28e3240828e32e3f57af41e54e01319312241b0031e3a255b0d708750b4cb3f3fe79e3503fe488cc8db1dd00753801754bb593ea42d231a7ddf367640f09bbf59dc00f2c1d2003cc340e0c016b5b13"),
-        orchard_tree: String::from(""),
-    };
-
-    connector.add_tree_state(third_transaction_state).await?;
-
-    let fourth_transaction_state = TreeState {
-        network: String::from("main"),
-        height: 663194,
-        hash: String::from("000000000096e4f59faae478c0ea5cb982c1cba223a92d15fbcf74795a0b4e5d"),
-        time: 1576824759,
-        sapling_tree: String::from("01f1c5f08a96b8e0befe04ff061dd51ed96ffcc7fcd96d011dc06255692989731c001001ea6dbe4e95ec503600ee3260105ffcc2ceb63eb34ec323c794eebfc49b7beb2c018b01c7e7b2b183d022fc35e351e6423aee8885debc899036d0bc3b389c9f161501dba0595ce728b41452a9c595341074cf01e1152abe401db2b30a9ab007ad006e0001989561014441f9f9043e11c01e220730df2219c090fa02f58d278fb7f447271601fa6d4c2390e205f81d86b85ace0b48f3ce0afb78eeef3e14c70bcfd7c5f0191c0000011bc9521263584de20822f9483e7edb5af54150c4823c775b2efc6a1eded9625501a6030f8d4b588681eddb66cad63f09c5c7519db49500fc56ebd481ce5e903c22000163f4eec5a2fe00a5f45e71e1542ff01e937d2210c99f03addcce5314a5278b2d0163ab01f46a3bb6ea46f5a19d5bdd59eb3f81e19cfa6d10ab0fd5566c7a16992601fa6980c053d84f809b6abcf35690f03a11f87b28e3240828e32e3f57af41e54e01319312241b0031e3a255b0d708750b4cb3f3fe79e3503fe488cc8db1dd00753801754bb593ea42d231a7ddf367640f09bbf59dc00f2c1d2003cc340e0c016b5b13"),
-        orchard_tree: String::from(""),
-    };
-
-    connector.add_tree_state(fourth_transaction_state).await?;
+    connector.stage_blocks_create(2, 2, 0).await.unwrap();
 
     sleep(std::time::Duration::new(2, 0)).await;
-    connector.apply_staged(663_200).await?;
+
+    connector.apply_staged(3).await?;
 
     Ok(())
 }
@@ -213,7 +159,7 @@ async fn test_simple_sync() {
             darkside_handler.darkside_dir.clone(),
             "still champion voice habit trend flight survey between bitter process artefact blind carbon truly provide dizzy crush flush breeze blouse charge solid fish spread"
         )
-            .build_new_faucet(663150, true).await;
+            .build_new_faucet(1, true).await;
 
     let result = light_client.do_sync(true).await.unwrap();
 
@@ -223,8 +169,8 @@ async fn test_simple_sync() {
         else { panic!("Expected object, got {result:?}") };
 
     assert_eq!(res_value["result"], "success");
-    assert_eq!(res_value["latest_block"], 663200);
-    assert_eq!(res_value["total_blocks_synced"], 50);
+    assert_eq!(res_value["latest_block"], 3);
+    assert_eq!(res_value["total_blocks_synced"], 3);
 }
 
 #[tokio::test]
