ed54541efbc9cd8354a105d0ca8118c436b317e9
zancas <zancas@zingolabs.org>
2023-06-16 14:09:27 -0600

    more timer-helper functions

--- zingocli/tests/utils/mod.rs
+++ zingocli/tests/utils/mod.rs
@@ -1,3 +1,4 @@
+use std::path::PathBuf;
 use std::string::String;
 use std::time::Duration;
 
@@ -27,6 +28,18 @@ fn timestamp() -> u64 {
 }
 pub fn timer_annotation(test_name: String) -> JsonValue {
     json::object! { "test_name": test_name, "timestamp": timestamp(), "git_description": git_description() }
+}
+fn path_to_times(annotation: &JsonValue) -> PathBuf {
+    let basename = format!("{}.json", annotation["test_name"].to_string());
+    let file_name = PathBuf::from(basename);
+    let timing_dir = PathBuf::from(
+        std::env::var("CARGO_MANIFEST_DIR").expect("To be inside a manifested space."),
+    )
+    .join("tests/times");
+    timing_dir.join(file_name)
+}
+pub fn record_time(annotation: JsonValue) {
+    let data_store = path_to_times(&annotation);
 }
 async fn get_synced_wallet_height(client: &LightClient) -> Result<u32, String> {
     client.do_sync(true).await?;
