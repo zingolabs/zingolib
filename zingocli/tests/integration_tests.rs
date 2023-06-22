2594a9ea266de282f5577ac4ef2ca8349c784ac3
zancas <zancas@zingolabs.org>
2023-06-21 20:04:59 -0600

    clean unused, ignore buggy

--- zingocli/tests/integration_tests.rs
+++ zingocli/tests/integration_tests.rs
@@ -2853,7 +2853,6 @@ async fn basic_faucet_count_sap_outputs() {
     let (regtest_manager, child_process_handler, faucet) = scenarios::faucet().await;
     assert_eq!(faucet.wallet.get_anchor_height().await, 1);
     assert_eq!(faucet.do_sync_status().await.sapling_outputs, 1);
-    use zingolib::BATCHSIZE;
     let mut count = 1;
     for _ in 0..1152 {
         zingo_testutils::increase_height_and_sync_client(&regtest_manager, &faucet, 1)
@@ -2867,6 +2866,7 @@ async fn basic_faucet_count_sap_outputs() {
 }
 mod benchmarks {
     use super::*;
+    #[ignore]
     #[tokio::test]
     async fn sync_1153_baseline_recipient_synctime() {
         let mut annotation =
@@ -2889,6 +2889,7 @@ mod benchmarks {
 
         drop(child_process_handler);
     }
+    #[ignore]
     #[tokio::test]
     async fn sync_1153_baseline_faucet_synctime() {
         let mut annotation =
