1cbb151ad15e510994b4be4dfbe2977e2342911d
Oscar Pepper <oscarpepper@zingolabs.org>
2023-07-18 13:57:33 +0100

    set back to true

--- zingolib/src/lib.rs
+++ zingolib/src/lib.rs
@@ -45,7 +45,7 @@ pub fn load_clientconfig(
     let config = ZingoConfig {
         lightwalletd_uri: Arc::new(RwLock::new(lightwallet_uri)),
         chain,
-        monitor_mempool: false,
+        monitor_mempool: true,
         reorg_buffer_offset: zingoconfig::REORG_BUFFER_OFFSET,
         zingo_wallet_dir: data_dir,
     };
