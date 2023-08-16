9911407ad2aeb3c92265e956172f7c51edc0de6c
zancas <zancas@zingolabs.org>
2023-08-16 12:52:47 -0600

    excise more bench specific code

--- zingo-testutils/src/lib.rs
+++ zingo-testutils/src/lib.rs
@@ -4,7 +4,6 @@ use zcash_address::unified::{Fvk, Ufvk};
 use zingolib::wallet::keys::unified::WalletCapability;
 use zingolib::wallet::WalletBase;
 pub mod regtest;
-use std::fs::OpenOptions;
 use std::io::Read;
 use std::path::{Path, PathBuf};
 use std::string::String;
@@ -13,7 +12,6 @@ use std::time::Duration;
 use json::JsonValue;
 use log::debug;
 use regtest::RegtestManager;
-use serde::{Deserialize, Serialize};
 use tokio::time::sleep;
 use zingoconfig::{ChainType, ZingoConfig};
 use zingolib::lightclient::LightClient;
@@ -61,88 +59,6 @@ pub async fn build_fvk_client_and_capability(
     (viewkey_client, watch_wc)
 }
 
-#[derive(Debug, Clone, PartialEq, Deserialize, Serialize)]
-pub struct DurationAnnotation {
-    pub timestamp: u64,
-    pub git_description: String,
-    pub test_name: String,
-    pub duration: Duration,
-    pub scenario: String,
-}
-impl DurationAnnotation {
-    pub fn new(scenario: String, test_name: String, duration: Duration) -> Self {
-        DurationAnnotation {
-            timestamp: timestamp(),
-            git_description: zingolib::git_description()
-                .to_string()
-                .trim_end()
-                .to_string(),
-            scenario,
-            test_name,
-            duration,
-        }
-    }
-}
-impl std::fmt::Display for DurationAnnotation {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            r#""test_name": {}, "timestamp": {}, "git_description": {}, "duration": {}"#,
-            self.test_name,
-            self.timestamp,
-            self.git_description,
-            self.duration.as_millis() as u64
-        )
-    }
-}
-fn timestamp() -> u64 {
-    std::time::SystemTime::now()
-        .duration_since(std::time::UNIX_EPOCH)
-        .unwrap()
-        .as_secs()
-}
-fn path_to_times(file_name: PathBuf) -> PathBuf {
-    let timing_dir = PathBuf::from(
-        std::env::var("CARGO_MANIFEST_DIR").expect("To be inside a manifested space."),
-    )
-    .join("tests/times");
-    timing_dir.join(file_name)
-}
-pub fn read_duration_annotation_file(target: PathBuf) -> Vec<DurationAnnotation> {
-    let data_set: Vec<DurationAnnotation> = if let Ok(data) = std::fs::read_to_string(target) {
-        serde_json::from_str(&data[..]).expect("To deserialize a string")
-    } else {
-        vec![]
-    };
-    data_set
-}
-pub fn get_duration_annotations(storage_file: PathBuf) -> Vec<DurationAnnotation> {
-    read_duration_annotation_file(storage_file)
-}
-pub fn record_time(annotation: &DurationAnnotation) {
-    let version_info = zingolib::git_description()
-        .to_string()
-        .trim_end()
-        .to_string();
-    let storage_path = format!("{}_sync_duration_annotation.json", version_info).to_string();
-    let storage_location = path_to_times(PathBuf::from(storage_path));
-    let mut data_set = get_duration_annotations(storage_location.clone());
-    data_set.push(annotation.clone());
-
-    //let json_dataset = array!(data_set);
-    let mut time_file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open(storage_location)
-        .expect("to access a data_store file");
-    std::io::Write::write_all(
-        &mut time_file,
-        serde_json::to_string(&data_set)
-            .expect("to serialiaze")
-            .as_bytes(),
-    )
-    .expect("To write out a new data vector");
-}
 async fn get_synced_wallet_height(client: &LightClient) -> Result<u32, String> {
     client.do_sync(true).await?;
     Ok(client
@@ -771,22 +687,5 @@ pub mod scenarios {
                 recipient,
             )
         }
-    }
-}
-#[cfg(test)]
-mod tests {
-    use super::*;
-    #[test]
-    fn deserialize_json_into_duration_annotation() {
-        let test_name = String::from("test_test_name");
-        let ta = DurationAnnotation::new(
-            "scenario".to_string(),
-            test_name,
-            Duration::from_millis(1_000),
-        );
-        let ta2 = ta.clone();
-        let ta_serde_json = serde_json::to_value(ta).unwrap();
-        let ta: DurationAnnotation = serde_json::from_value(ta_serde_json).unwrap();
-        assert_eq!(ta, ta2);
     }
 }
