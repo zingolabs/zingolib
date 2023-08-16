65cc1b51b07d9a414a81ddaee28dd2d791abb728
zancas <zancas@zingolabs.org>
2023-08-16 09:00:07 -0600

    migrate minimal code into analysis_utils

--- zingo-testutils/src/lib.rs
+++ zingo-testutils/src/lib.rs
@@ -723,49 +723,12 @@ pub mod scenarios {
     }
 
     pub mod chainload {
-        use crate::{build_fvk_client_and_capability, build_fvks_from_wallet_capability};
-
         use super::*;
 
         pub async fn unsynced_basic() -> ChildProcessHandler {
             setup::ScenarioBuilder::new_load_1153_saplingcb_regtest_chain()
                 .child_process_handler
                 .unwrap()
-        }
-        pub async fn unsynced_viewonlyclient_1153() -> (
-            RegtestManager,
-            ChildProcessHandler,
-            LightClient,
-            LightClient,
-        ) {
-            let mut sb = setup::ScenarioBuilder::new_load_1153_saplingcb_regtest_chain();
-            let zingo_config = zingolib::load_clientconfig(
-                sb.client_builder.server_id.clone(),
-                Some(sb.client_builder.zingo_datadir.clone()),
-                zingoconfig::ChainType::Regtest,
-                true,
-            )
-            .unwrap();
-            // Create a lightclient to extract a capability from.
-            let original_recipient = sb.client_builder.build_new_faucet(0, false).await;
-            // Extract viewing keys
-            let wallet_capability = original_recipient
-                .wallet
-                .wallet_capability()
-                .read()
-                .await
-                .clone();
-            // Delete the client after getting the capability.
-            // Extract the orchard fvk
-            let [o_fvk, s_fvk, t_fvk] = build_fvks_from_wallet_capability(&wallet_capability);
-            let (viewing_client, _) =
-                build_fvk_client_and_capability(&[&o_fvk, &s_fvk, &t_fvk], &zingo_config).await;
-            (
-                sb.regtest_manager,
-                sb.child_process_handler.unwrap(),
-                original_recipient,
-                viewing_client,
-            )
         }
         pub async fn faucet_recipient_1153() -> (
             RegtestManager,
