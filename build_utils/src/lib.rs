6c1da790ba25df9e0c02dcd711025fa4dcb13bdd
zancas <zancas@zingolabs.org>
2023-09-01 10:41:31 -0600

    add the actual utils!

---
+++ build_utils/src/lib.rs
@@ -0,0 +1,22 @@
+use std::io::Write;
+use std::{env, fs::File, path::Path, process::Command};
+
+pub fn git_description() {
+    let output = Command::new("git")
+        .args(["describe", "--dirty"])
+        .output()
+        .expect("Failed to execute git command");
+
+    let git_description = String::from_utf8(output.stdout).unwrap();
+
+    // Write the git description to a file which will be included in the crate
+    let out_dir = env::var("OUT_DIR").unwrap();
+    let dest_path = Path::new(&out_dir).join("git_description.rs");
+    let mut f = File::create(dest_path).unwrap();
+    writeln!(
+        f,
+        "pub fn git_description() -> &'static str {{\"{}\"}}",
+        git_description
+    )
+    .unwrap();
+}
