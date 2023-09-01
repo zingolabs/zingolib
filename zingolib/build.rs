9723c595376c6887e6398bf64dd11611bda8082f
zancas <zancas@zingolabs.org>
2023-09-01 11:03:13 -0600

    remove redundant use

--- zingolib/build.rs
+++ zingolib/build.rs
@@ -1,6 +1,4 @@
 // build.rs
-use build_utils;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure().build_server(true).compile(
         &["proto/service.proto", "proto/compact_formats.proto"],
