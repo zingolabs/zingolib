227860cd38b2158cd85d8ec642f35a4059440a5e
Oscar Pepper <oscarpepper@zingolabs.org>
2023-10-04 13:20:57 +0100

    added unit tests to check confs are formatted correctly

--- zingo-testutils/src/data.rs
+++ zingo-testutils/src/data.rs
@@ -31,8 +31,7 @@ nuparams=76b809bb:1 # Sapling
 nuparams=2bb40e60:1 # Blossom
 nuparams=f5b9230b:1 # Heartwood
 nuparams=e9ff75a6:1 # Canopy
-nuparams=c2d6d0b4:{orchard_activation_height} # Orchard (NU5)
-
+nuparams=c2d6d0b4:{orchard_activation_height} # NU5 (Orchard)
 
 ### MetaData Storage and Retrieval
 # txindex:
@@ -42,7 +41,6 @@ txindex=1
 # https://zcash.readthedocs.io/en/latest/rtd_pages/insight_explorer.html?highlight=insightexplorer#additional-getrawtransaction-fields
 insightexplorer=1
 experimentalfeatures=1
-
 
 ### RPC Server Interface Options:
 # https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#json-rpc-options
@@ -55,7 +53,8 @@ rpcallowip=127.0.0.1
 # https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#zcash-conf-guide
 listen=0
 
-{extra}")
+{extra}"
+            )
         }
         pub fn funded(
             mineraddress: &str,
@@ -63,19 +62,63 @@ listen=0
             orchard_activation_height: BlockHeight,
         ) -> String {
             basic(rpcport, orchard_activation_height,
-                &format!(
-                    "\
+                &format!("\
 ### Zcashd Help provides documentation of the following:
 mineraddress={mineraddress}
 minetolocalwallet=0 # This is set to false so that we can mine to a wallet, other than the zcashd wallet."
                 )
             )
         }
+
+        #[test]
+        fn funded_zcashd_conf() {
+            assert_eq!(
+                funded(
+                    super::super::REGSAP_ADDR_FROM_ABANDONART,
+                    "1234",
+                    BlockHeight::from_u32(10)
+                ),
+                format!("\
+### Blockchain Configuration
+regtest=1
+nuparams=5ba81b19:1 # Overwinter
+nuparams=76b809bb:1 # Sapling
+nuparams=2bb40e60:1 # Blossom
+nuparams=f5b9230b:1 # Heartwood
+nuparams=e9ff75a6:1 # Canopy
+nuparams=c2d6d0b4:10 # NU5 (Orchard)
+
+### MetaData Storage and Retrieval
+# txindex:
+# https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#miscellaneous-options
+txindex=1
+# insightexplorer:
+# https://zcash.readthedocs.io/en/latest/rtd_pages/insight_explorer.html?highlight=insightexplorer#additional-getrawtransaction-fields
+insightexplorer=1
+experimentalfeatures=1
+
+### RPC Server Interface Options:
+# https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#json-rpc-options
+rpcuser=xxxxxx
+rpcpassword=xxxxxx
+rpcport=1234
+rpcallowip=127.0.0.1
+
+# Buried config option to allow non-canonical RPC-PORT:
+# https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#zcash-conf-guide
+listen=0
+
+### Zcashd Help provides documentation of the following:
+mineraddress=zregtestsapling1fmq2ufux3gm0v8qf7x585wj56le4wjfsqsj27zprjghntrerntggg507hxh2ydcdkn7sx8kya7p
+minetolocalwallet=0 # This is set to false so that we can mine to a wallet, other than the zcashd wallet."
+                )
+            );
+        }
     }
     pub mod lightwalletd {
-
         pub fn basic(rpcport: &str) -> String {
-            format! {"\
+            format!(
+                "\
 # # Default zingo lib lightwalletd conf YAML for regtest mode # #
 grpc-bind-addr: 127.0.0.1:{rpcport}
 cache-size: 10
@@ -85,7 +128,28 @@ zcash-conf-path: ../conf/zcash.conf
 
 # example config for TLS
 #tls-cert: /secrets/lightwallted/example-only-cert.pem
-#tls-key: /secrets/lightwallted/example-only-cert.key"}
+#tls-key: /secrets/lightwallted/example-only-cert.key"
+            )
+        }
+
+        #[test]
+        fn basic_lightwalletd_conf() {
+            assert_eq!(
+                basic("1234"),
+                format!(
+                    "\
+# # Default zingo lib lightwalletd conf YAML for regtest mode # #
+grpc-bind-addr: 127.0.0.1:1234
+cache-size: 10
+log-file: ../logs/lwd.log
+log-level: 10
+zcash-conf-path: ../conf/zcash.conf
+
+# example config for TLS
+#tls-cert: /secrets/lightwallted/example-only-cert.pem
+#tls-key: /secrets/lightwallted/example-only-cert.key"
+                )
+            )
         }
     }
 }
