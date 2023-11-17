7265aeaa894500816d86279ca5d0be82b4a20866
zancas <zancas@zingolabs.org>
2023-11-17 11:34:52 -0700

    rm obsolete gate

--- darkside-tests/tests/mod.rs
+++ darkside-tests/tests/mod.rs
@@ -1,10 +1,6 @@
-#[cfg(feature = "darkside_tests")]
 mod constants;
-#[cfg(feature = "darkside_tests")]
 pub mod tests;
-#[cfg(feature = "darkside_tests")]
 mod utils;
-#[cfg(feature = "darkside_tests")]
 pub mod darkside_types {
     tonic::include_proto!("cash.z.wallet.sdk.rpc");
 }
