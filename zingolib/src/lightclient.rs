063856fcf77f8f782bb6b13c6a817f49a18d526a
fluidvanadium <fluidvanadium@zingolabs.org>
2023-11-22 18:29:00 +0000

    replace summaries unconfirmed with status

--- zingolib/src/lightclient.rs
+++ zingolib/src/lightclient.rs
@@ -746,7 +746,7 @@ impl LightClient {
                         transaction_md.block_height,
                         transaction_md.datetime,
                         transaction_md.price,
-                        transaction_md.unconfirmed,
+                        !transaction_md.status.is_confirmed(),
                     );
                     summaries.push(ValueTransfer {
                         block_height,
@@ -1700,7 +1700,7 @@ impl LightClient {
             transaction_md.block_height,
             transaction_md.datetime,
             transaction_md.price,
-            transaction_md.unconfirmed,
+            !transaction_md.status.is_confirmed(),
         );
         match (
             transaction_md.is_outgoing_transaction(),
