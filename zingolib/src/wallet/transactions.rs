a6b8ac31f629a19b1d7f925b6a7c06844f8a0f18
fluidvanadium <fluidvanadium@zingolabs.org>
2023-11-22 18:33:50 +0000

    use status in write

--- zingolib/src/wallet/transactions.rs
+++ zingolib/src/wallet/transactions.rs
@@ -194,7 +194,7 @@ impl TransactionMetadataSet {
                 .collect::<Vec<(&TxId, &TransactionMetadata)>>();
             // Don't write down metadata for transactions in the mempool, we'll rediscover
             // it on reload
-            transaction_metadatas.retain(|metadata| !metadata.1.unconfirmed);
+            transaction_metadatas.retain(|metadata| metadata.1.status.is_confirmed());
             transaction_metadatas.sort_by(|a, b| a.0.partial_cmp(b.0).unwrap());
 
             Vector::write(&mut writer, &transaction_metadatas, |w, (k, v)| {
