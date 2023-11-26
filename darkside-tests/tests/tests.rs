832519aaf5b3c4c6aff553c29c8a1564dcb97116
Oscar Pepper <oscarpepper@zingolabs.org>
2023-11-26 15:22:25 -0700

    started chainbuild

--- darkside-tests/tests/tests.rs
+++ darkside-tests/tests/tests.rs
@@ -50,7 +50,79 @@ async fn simple_sync() {
         }
     );
 }
-#[ignore]
+
+#[tokio::test]
+async fn interrupt_sync_chainbuild() {
+    let (handler, connector) = init_darksidewalletd().await.unwrap();
+
+    // stage blocks and initial recipient funds
+    connector.stage_blocks_create(2, 99_999, 0).await.unwrap();
+    stage_transaction(
+        &connector,
+        2,
+        constants::ABANDON_TO_DARKSIDE_SAP_10_000_000_ZAT,
+    )
+    .await;
+
+    // apply initial recipient funds
+    connector.apply_staged(2).await.unwrap();
+
+    // build darkside client and send to self sapling
+    let mut client_builder = ClientBuilder::new(connector.0.clone(), handler.darkside_dir.clone());
+    let regtest_network = RegtestNetwork::all_upgrades_active();
+    let recipient = client_builder
+        .build_client(DARKSIDE_SEED.to_string(), 0, true, regtest_network)
+        .await;
+    let txid = recipient
+        .do_send(vec![(
+            &get_base_address!(recipient, "sapling"),
+            50_000,
+            None,
+        )])
+        .await
+        .unwrap();
+    recipient.do_sync(false).await.unwrap();
+    println!("{}", recipient.do_list_transactions().await.pretty(2));
+
+    let connector = DarksideConnector(server_id.clone());
+    let mut streamed_raw_txns = connector.get_incoming_transactions().await.unwrap();
+    let raw_tx = streamed_raw_txns.message().await.unwrap().unwrap();
+    // There should only be one transaction incoming
+    assert!(streamed_raw_txns.message().await.unwrap().is_none());
+    connector
+        .stage_transactions_stream(vec![(raw_tx.data.clone(), 4)])
+        .await
+        .unwrap();
+    connector.stage_blocks_create(4, 1, 0).await.unwrap();
+    update_tree_states_for_transaction(&server_id, raw_tx.clone(), 4).await;
+    connector.apply_staged(4).await.unwrap();
+    // apply blockchain
+    connector.apply_staged(2).await.unwrap();
+
+    let regtest_network = RegtestNetwork::all_upgrades_active();
+    let light_client = ClientBuilder::new(connector.0.clone(), handler.darkside_dir.clone())
+        .build_client(DARKSIDE_SEED.to_string(), 0, true, regtest_network)
+        .await;
+    // let _result = light_client.do_sync(true).await.unwrap();
+
+    light_client.do_sync(true).await.unwrap();
+
+    dbg!(light_client.do_balance().await);
+    println!(
+        "{}",
+        json::stringify_pretty(light_client.do_list_notes(true).await, 4)
+    );
+
+    // apply blockchain
+    connector.apply_staged(3).await.unwrap();
+
+    light_client.do_sync(true).await.unwrap();
+    dbg!(light_client.do_balance().await);
+    println!(
+        "{}",
+        json::stringify_pretty(light_client.do_list_notes(true).await, 4)
+    );
+}
 #[tokio::test]
 async fn interrupt_sync() {
     let (handler, connector) = init_darksidewalletd().await.unwrap();
@@ -91,26 +163,6 @@ async fn interrupt_sync() {
         "{}",
         json::stringify_pretty(light_client.do_list_notes(true).await, 4)
     );
-
-    // println!("{}", result);
-
-    // assert!(result.success);
-    // assert_eq!(result.latest_block, 3);
-    // assert_eq!(result.total_blocks_synced, 3);
-    // assert_eq!(
-    //     light_client.do_balance().await,
-    //     PoolBalances {
-    //         sapling_balance: Some(0),
-    //         verified_sapling_balance: Some(0),
-    //         spendable_sapling_balance: Some(0),
-    //         unverified_sapling_balance: Some(0),
-    //         orchard_balance: Some(100000000),
-    //         verified_orchard_balance: Some(100000000),
-    //         spendable_orchard_balance: Some(100000000),
-    //         unverified_orchard_balance: Some(0),
-    //         transparent_balance: Some(0)
-    //     }
-    // );
 }
 #[tokio::test]
 async fn spend_orchard_notes() {
