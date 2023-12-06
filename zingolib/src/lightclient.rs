fc8e3410cf7257e867f61687c8335a60de3c38a3
fluidvanadium <fluidvanadium@zingolabs.org>
2023-12-06 04:06:10 +0000

    list sapling_notes uses get_height
    coerced height for sapling data export

--- zingolib/src/lightclient.rs
+++ zingolib/src/lightclient.rs
@@ -1948,7 +1948,7 @@ impl LightClient {
                             let address = LightWallet::note_address::<zcash_primitives::sapling::note_encryption::SaplingDomain<zingoconfig::ChainType>>(&self.config.chain, note_metadata, &self.wallet.wallet_capability());
                             let spendable = transaction_metadata.status.is_confirmed_after_or_at(&anchor_height) && note_metadata.spent.is_none() && note_metadata.unconfirmed_spent.is_none();
 
-                            let created_block:u32 = transaction_metadata.block_height.into();
+                            let created_block:u32 = transaction_metadata.status.get_height().into();
                             Some(object!{
                                 "created_in_block"   => created_block,
                                 "datetime"           => transaction_metadata.datetime,
