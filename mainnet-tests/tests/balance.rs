a75f40c73206f1ffca9408534556d638c49b0f0c
fluidvanadium <fluidvanadium@zingolabs.org>
2023-12-28 17:43:43 +0000

    using tokio test

--- mainnet-tests/tests/balance.rs
+++ mainnet-tests/tests/balance.rs
@@ -1,6 +1,8 @@
-#[test]
-fn equality() {
-    if 0 == 1 {
-        panic!();
-    }
+use zingolib::lightclient::LightClient;
+
+#[tokio::test]
+async fn equality() {
+    let x = 0;
+    // let client = LightClient::new();
+    // client.do_sync(false).await;
 }
