c8e3a751f5dac4355e33a29e3dea1b04696e4bec
Hazel OHearn <gygaxis@zingolabs.org>
2024-01-10 16:44:17 -0400

    example working. Improvements to make: Customizable/proptestable values, fix horrible screen output of watch and zingocli fighting, remove need to run 'sudo killall watch' after

--- network_updown.sh
+++ network_updown.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ip link set wlp1s0 down
 sleep $1
 ip link set wlp1s0 up
--- zingocli/examples/iplink_interrupt.rs
+++ zingocli/examples/iplink_interrupt.rs
@@ -1,1 +1,16 @@
-fn main() {}
+fn main() {
+    start_iplink_onoff(2, 10);
+    zingo_cli::run_cli();
+}
+
+fn start_iplink_onoff(sleeptime: u8, uptime: u8) {
+    std::process::Command::new("watch")
+        .args([
+            "-n",
+            &(sleeptime + uptime).to_string(),
+            "./network_updown.sh",
+            &sleeptime.to_string(),
+        ])
+        .spawn()
+        .unwrap();
+}
