ea88a34483e9cda27c2ce0cededdd23d6aae479d
Hazel OHearn <gygaxis@zingolabs.org>
2024-01-11 15:26:17 -0400

    replace shell script with multiple calls from rust

--- zingocli/examples/iplink_interrupt.rs
+++ zingocli/examples/iplink_interrupt.rs
@@ -1,16 +1,24 @@
+use std::{
+    thread::{self, sleep},
+    time::Duration,
+};
+
 fn main() {
     start_iplink_onoff(2, 10);
     zingo_cli::run_cli();
 }
 
-fn start_iplink_onoff(sleeptime: u8, uptime: u8) {
-    std::process::Command::new("watch")
-        .args([
-            "-n",
-            &(sleeptime + uptime).to_string(),
-            "./network_updown.sh",
-            &sleeptime.to_string(),
-        ])
-        .spawn()
-        .unwrap();
+fn start_iplink_onoff(sleeptime: u64, uptime: u64) {
+    let _ = thread::spawn(move || loop {
+        std::process::Command::new("ip")
+            .args(["link", "set", "wlp1s0", "down"])
+            .output()
+            .unwrap();
+        sleep(Duration::from_secs(sleeptime));
+        std::process::Command::new("ip")
+            .args(["link", "set", "wlp1s0", "up"])
+            .output()
+            .unwrap();
+        sleep(Duration::from_secs(uptime));
+    });
 }
