8243698ca8996e29d17e02deeb2044ea4c9b3bcb
zancas <zancas@zingolabs.org>
2024-01-12 08:24:41 -0700

    make local const and start on stale_fresh_test

--- zingocli/tests/argument_parsing.rs
+++ zingocli/tests/argument_parsing.rs
@@ -1,15 +1,18 @@
+use zingoconfig::DEFAULT_WALLET_NAME;
+
+const CLI_PARSE_TEST_DATA: &str = "./cli_parse_test_data";
+
 #[test]
 fn clargs_view_key_birthday_fresh_wallet_dir() {
+    let cli_parse_data_path: &std::path::Path = std::path::Path::new(CLI_PARSE_TEST_DATA);
     let expected_output: &str = r#"{
   "ufvk": "uview1ah2qw247djujxwu5tdr20t7naaqvd5qkljxtm0yrw92tzy6fvafdhv7uchvsrzxaqskc7mxphzafgn5aca6pscrdx8xqu95ruyefng0hcctsnle4jq9f97gvymlf78pal7zqwf3yfej6han20pvhj0j0ew05dzq685kx29phyy5ffuw63wxmvesm9r23jhccfrdm9yxr5tz4hkw40t0ak5j4lgr67zdcl9rhluaqwatudjhaq0lep5ffcc8yrruvf0tz9zmxl5zfg9lx728mwdz4705wsr9fr8h4u7nc5ja8c560j45jn0jqty8hcqjedjakjkj04najmvzk4fr0g9kevshe6verg8h9pr4phx86wsc2xa5rdp78zrr5upuyqanhk98w4u3vs9mssdnxrwe9xf4qlffqq46faaxuvnsst4mn87eyk4j6h4jm6t3kzcmwh0waw8u5500yqyfm35ndcerzkx8xr5xaznqrma0zy69qvgz7nnyq",
   "birthday": 2363649
 }
 "#;
     // Remove the 'foo' directory if it exists
-    let cli_parse_test_data = "./cli_parse_test_data";
-    let cpdp = std::path::Path::new(cli_parse_test_data);
-    if cpdp.exists() {
-        std::fs::remove_dir_all(cli_parse_test_data)
+    if cli_parse_data_path.exists() {
+        std::fs::remove_dir_all(CLI_PARSE_TEST_DATA)
             .expect("Failed to remove existing foo directory");
     }
 
@@ -18,7 +21,7 @@ fn clargs_view_key_birthday_fresh_wallet_dir() {
         .args(&["run", "--"])
         .args(&["--view-key", zingolib::testvectors::MAINNET_ALPHA_VIEWKEY]) // shortened for brevity
         .args(&["--birthday", "2363649"])
-        .args(&["--fresh-wallet-dir", cli_parse_test_data])
+        .args(&["--fresh-wallet-dir", CLI_PARSE_TEST_DATA])
         .args(&["--nosync"])
         .args(&["exportufvk"])
         .output()
@@ -29,13 +32,13 @@ fn clargs_view_key_birthday_fresh_wallet_dir() {
 
     // Additional assertions based on the expected behavior of your application
     // For example, check if the 'foo' directory was created
-    assert!(std::fs::metadata(cpdp).is_ok());
+    assert!(std::fs::metadata(cli_parse_data_path).is_ok());
     assert_eq!(
         std::string::String::from_utf8_lossy(&output.stdout),
         expected_output
     );
-    if std::fs::metadata(cli_parse_test_data).is_ok() {
-        std::fs::remove_dir_all(cli_parse_test_data)
+    if std::fs::metadata(CLI_PARSE_TEST_DATA).is_ok() {
+        std::fs::remove_dir_all(CLI_PARSE_TEST_DATA)
             .expect("Failed to remove existing foo directory");
     }
 }
@@ -56,4 +59,19 @@ fn clargs_view_key_birthday_seed_phrase() {
         .expect("Failed to execute cargo run command");
     assert!(!output.status.success());
     assert!(std::string::String::from_utf8_lossy(&output.stderr).contains(expected_error_fragment));
+}
+#[test]
+fn collide_stale_and_fresh() {
+    // Remove the 'foo' directory if it exists
+    let cpdp = std::path::Path::new(CLI_PARSE_TEST_DATA);
+    if cpdp.exists() {
+        std::fs::remove_dir_all(CLI_PARSE_TEST_DATA)
+            .expect("Failed to remove existing foo directory");
+    }
+    let dir_path = std::path::Path::new(CLI_PARSE_TEST_DATA);
+    let file_path = dir_path.join(DEFAULT_WALLET_NAME);
+    std::fs::create_dir_all(&dir_path).unwrap();
+    std::fs::File::create(&file_path).unwrap();
+    let target = cpdp.join(file_path);
+    dbg!(target);
 }
