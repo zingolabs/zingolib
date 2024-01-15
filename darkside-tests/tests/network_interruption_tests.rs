096de0701a08274966d46c4c4a69a5154dd2e785
Oscar Pepper <oscarpepper@zingolabs.org>
2024-01-15 19:40:00 +0000

    added grpc delay to trigger just before the call, changed network interupt test and added some helpers

--- darkside-tests/tests/network_interruption_tests.rs
+++ darkside-tests/tests/network_interruption_tests.rs
@@ -13,64 +13,77 @@ use darkside_tests::utils::{
 };
 use tokio::time::sleep;
 use zingo_testutils::start_proxy_and_connect_lightclient;
-use zingolib::{get_base_address, lightclient::PoolBalances, wallet::Pool};
+use zingolib::{get_base_address, lightclient::PoolBalances, testvectors::seeds, wallet::Pool};
 
-// Test not finished, requires gRPC network interrupter
+// Test not finished, not failing
 #[ignore]
 #[tokio::test]
 async fn network_interrupt_chainbuild() {
-    const BLOCKCHAIN_HEIGHT: u64 = 2_000;
-    let chainbuild_file = create_chainbuild_file("network_interrupt_single_tx");
+    const BLOCKCHAIN_HEIGHT: u64 = 100_000;
+    let chainbuild_file = create_chainbuild_file("network_interrupt");
     let mut scenario = DarksideScenario::default().await;
-
     scenario.build_faucet(Pool::Sapling).await;
+    scenario
+        .build_client(seeds::HOSPITAL_MUSEUM_SEED.to_string(), 4)
+        .await;
 
-    // stage a send to self every thousand blocks
+    // stage a sapling to orchard send-to-self every thousand blocks
     for thousands_blocks_count in 1..BLOCKCHAIN_HEIGHT / 1000 {
         scenario
-            .generate_blocks(thousands_blocks_count * 1000 - 1, thousands_blocks_count)
+            .stage_and_apply_blocks(thousands_blocks_count * 1000 - 2, 0)
             .await;
         scenario.get_faucet().do_sync(false).await.unwrap();
         scenario
             .send_and_write_transaction(
                 DarksideSender::Faucet,
-                &get_base_address!(scenario.get_faucet(), "unified"),
-                40_000,
+                &get_base_address!(scenario.get_lightclient(0), "sapling"),
+                50_000,
                 &chainbuild_file,
             )
             .await;
+        scenario
+            .apply_blocks(thousands_blocks_count * 1000 - 1)
+            .await;
+        scenario.get_lightclient(0).do_sync(false).await.unwrap();
+        scenario
+            .shield_and_write_transaction(DarksideSender::IndexedClient(0), &chainbuild_file)
+            .await;
     }
     // stage and apply final blocks
-    scenario.generate_blocks(BLOCKCHAIN_HEIGHT, 150).await;
-    scenario.get_faucet().do_sync(false).await.unwrap();
+    scenario.stage_and_apply_blocks(BLOCKCHAIN_HEIGHT, 0).await;
+    scenario.get_lightclient(0).do_sync(false).await.unwrap();
 
     println!("do balance:");
-    dbg!(scenario.get_faucet().do_balance().await);
+    dbg!(scenario.get_lightclient(0).do_balance().await);
     println!("do list_notes:");
     println!(
         "{}",
-        json::stringify_pretty(scenario.get_faucet().do_list_notes(true).await, 4)
+        json::stringify_pretty(scenario.get_lightclient(0).do_list_notes(true).await, 4)
     );
 }
 #[tokio::test]
 async fn network_interrupt_test() {
-    const BLOCKCHAIN_HEIGHT: u64 = 2_000;
-    let transaction_set = load_chainbuild_file("network_interrupt_single_tx");
+    const BLOCKCHAIN_HEIGHT: u64 = 100_000;
+    let transaction_set = load_chainbuild_file("network_interrupt");
     let mut scenario = DarksideScenario::default().await;
-
     scenario.build_faucet(Pool::Sapling).await;
+    scenario
+        .build_client(seeds::HOSPITAL_MUSEUM_SEED.to_string(), 4)
+        .await;
 
     // stage a send to self every thousand blocks
     for thousands_blocks_count in 1..BLOCKCHAIN_HEIGHT / 1000 {
         scenario
-            .generate_blocks(thousands_blocks_count * 1000 - 1, thousands_blocks_count)
+            .stage_and_apply_blocks(thousands_blocks_count * 1000 - 2, 0)
             .await;
+        scenario.stage_next_transaction(&transaction_set).await;
         scenario
-            .stage_transaction(&transaction_set[(thousands_blocks_count - 1) as usize])
+            .apply_blocks(thousands_blocks_count * 1000 - 1)
             .await;
+        scenario.stage_next_transaction(&transaction_set).await;
     }
     // stage and apply final blocks
-    scenario.generate_blocks(BLOCKCHAIN_HEIGHT, 150).await;
+    scenario.stage_and_apply_blocks(BLOCKCHAIN_HEIGHT, 0).await;
 
     let mut conditional_logic =
         HashMap::<&'static str, Box<dyn Fn(&Arc<AtomicBool>) + Send + Sync>>::new();
@@ -90,38 +103,38 @@ async fn network_interrupt_test() {
     );
 
     let proxy_status =
-        start_proxy_and_connect_lightclient(scenario.get_faucet(), conditional_logic);
+        start_proxy_and_connect_lightclient(scenario.get_lightclient(0), conditional_logic);
     tokio::task::spawn(async move {
         loop {
-            sleep(Duration::from_secs(30)).await;
+            sleep(Duration::from_secs(3)).await;
             proxy_status.store(true, std::sync::atomic::Ordering::Relaxed);
             println!("Set proxy status to true");
         }
     });
 
-    scenario.get_faucet().do_sync(true).await.unwrap();
+    scenario.get_lightclient(0).do_sync(false).await.unwrap();
 
-    println!("do balance:");
-    dbg!(scenario.get_faucet().do_balance().await);
+    // println!("do balance:");
+    // dbg!(scenario.get_lightclient(0).do_balance().await);
+    // println!("do list_notes:");
+    // println!(
+    //     "{}",
+    //     json::stringify_pretty(scenario.get_lightclient(0).do_list_notes(true).await, 4)
+    // );
     assert_eq!(
-        scenario.get_faucet().do_balance().await,
+        scenario.get_lightclient(0).do_balance().await,
         PoolBalances {
             sapling_balance: Some(0),
             verified_sapling_balance: Some(0),
             spendable_sapling_balance: Some(0),
             unverified_sapling_balance: Some(0),
-            orchard_balance: Some(9_990_000),
-            verified_orchard_balance: Some(9_990_000),
+            orchard_balance: Some(3_960_000),
+            verified_orchard_balance: Some(3_960_000),
             unverified_orchard_balance: Some(0),
-            spendable_orchard_balance: Some(9_990_000),
+            spendable_orchard_balance: Some(3_960_000),
             transparent_balance: Some(0),
         }
     );
-    // println!("do list_notes:");
-    // println!(
-    //     "{}",
-    //     json::stringify_pretty(scenario.get_faucet().do_list_notes(true).await, 4)
-    // );
 }
 
 mod assorted_interrupt_attempts {
@@ -137,14 +150,16 @@ mod assorted_interrupt_attempts {
         // stage a send to self every thousand blocks
         for thousands_blocks_count in 1..BLOCKCHAIN_HEIGHT / 1000 {
             scenario
-                .generate_blocks(thousands_blocks_count * 1000 - 1, thousands_blocks_count)
+                .stage_and_apply_blocks(thousands_blocks_count * 1000 - 1, thousands_blocks_count)
                 .await;
             scenario
                 .stage_transaction(&transaction_set[(thousands_blocks_count - 1) as usize])
                 .await;
         }
         // stage and apply final blocks
-        scenario.generate_blocks(BLOCKCHAIN_HEIGHT, 150).await;
+        scenario
+            .stage_and_apply_blocks(BLOCKCHAIN_HEIGHT, 150)
+            .await;
 
         scenario.build_faucet(Pool::Sapling).await;
 
