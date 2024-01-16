9eb68a8a92246038a2feb481d2cec7caf2b9fc6d
zancas <zancas@zingolabs.org>
2024-01-16 06:46:18 -0700

    (for now at least) the test framework is pub(crate) and cfg(test)

--- zingolib/src/lib.rs
+++ zingolib/src/lib.rs
@@ -1,7 +1,6 @@
 #![forbid(unsafe_code)]
 #[macro_use]
 extern crate rust_embed;
-mod test_framework;
 
 pub mod blaze;
 pub mod commands;
@@ -12,7 +11,7 @@ pub mod wallet;
 #[cfg(feature = "test")]
 pub use zingo_testvectors as testvectors;
 #[cfg(feature = "test")]
-pub(crate) mod test_utils;
+pub(crate) mod test_framework;
 
 // This line includes the generated `git_description()` function directly into this scope.
 include!(concat!(env!("OUT_DIR"), "/git_description.rs"));
