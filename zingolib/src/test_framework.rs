c8edb44ef730b280d5738f7157bcd9c0dca28c70
Hazel OHearn <gygaxis@zingolabs.org>
2024-01-17 14:21:28 -0400

    Add shieldednoteinterface builder to test framework

--- zingolib/src/test_framework.rs
+++ zingolib/src/test_framework.rs
@@ -1,6 +1,8 @@
+use crate::wallet::traits::FromBytes;
 pub(crate) mod macros;
 
-use zcash_primitives::transaction::TxId;
+use incrementalmerkletree::Position;
+use zcash_primitives::{memo::Memo, transaction::TxId};
 
 macro_rules! build_method {
     ($name:ident, $localtype:ty) => {
@@ -10,7 +12,7 @@ macro_rules! build_method {
         }
     };
 }
-use crate::wallet::notes::TransparentNote;
+use crate::wallet::notes::{ShieldedNoteInterface, TransparentNote};
 pub struct TransparentNoteBuilder {
     address: Option<String>,
     txid: Option<TxId>,
@@ -62,6 +64,69 @@ impl Default for TransparentNoteBuilder {
             spent_at_height: Some(None),
             spent: Some(None),
             unconfirmed_spent: Some(None),
+        }
+    }
+}
+
+pub struct ShieldedNoteBuilder<T: ShieldedNoteInterface> {
+    diversifier: Option<T::Diversifier>,
+    note: Option<T::Note>,
+    position_of_commitment_to_witness: Option<Option<Position>>,
+    nullifier: Option<Option<T::Nullifier>>,
+    spent: Option<Option<(TxId, u32)>>,
+    unconfirmed_spent: Option<Option<(TxId, u32)>>,
+    memo: Option<Option<Memo>>,
+    is_change: Option<bool>,
+    have_spending_key: Option<bool>,
+    output_index: Option<Option<u32>>,
+}
+
+impl<T: ShieldedNoteInterface> ShieldedNoteBuilder<T> {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    // Methods to set each field
+    build_method!(diversifier, T::Diversifier);
+    build_method!(note, T::Note);
+    build_method!(position_of_commitment_to_witness, Option<Position>);
+    build_method!(nullifier, Option<T::Nullifier>);
+    build_method!(spent, Option<(TxId, u32)>);
+    build_method!(unconfirmed_spent, Option<(TxId, u32)>);
+    build_method!(memo, Option<Memo>);
+    build_method!(is_change, bool);
+    build_method!(have_spending_key, bool);
+    build_method!(output_index, Option<u32>);
+
+    // Build method
+    pub fn build(self) -> T {
+        ShieldedNoteInterface::from_parts(
+            self.diversifier.unwrap(),
+            self.note.unwrap(),
+            self.position_of_commitment_to_witness.unwrap(),
+            self.nullifier.unwrap(),
+            self.spent.unwrap(),
+            self.unconfirmed_spent.unwrap(),
+            self.memo.unwrap(),
+            self.is_change.unwrap(),
+            self.have_spending_key.unwrap(),
+            self.output_index.unwrap(),
+        )
+    }
+}
+
+impl<T: ShieldedNoteInterface> Default for ShieldedNoteBuilder<T> {
+    fn default() -> Self {
+        Self {
+            diversifier: Some(T::Diversifier::from_bytes([0; 11])),
+            note: None,
+            position_of_commitment_to_witness: Some(None),
+            nullifier: Some(None),
+            spent: Some(None),
+            unconfirmed_spent: Some(None),
+            memo: Some(None),
+            is_change: Some(false),
+            have_spending_key: Some(false),
+            output_index: Some(None),
         }
     }
 }
