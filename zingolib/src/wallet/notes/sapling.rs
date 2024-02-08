60a4c5314a7615c7655acfcb3df200fa6c346e97
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-08 20:18:41 +0000

    notes/sapling.rs mod

--- zingolib/src/wallet/notes/sapling.rs
+++ zingolib/src/wallet/notes/sapling.rs
@@ -1,28 +1,26 @@
+use std::io::Write;
+
+use byteorder::{ReadBytesExt, WriteBytesExt};
 use incrementalmerkletree::Position;
-use zcash_primitives::{memo::Memo, transaction::TxId};
+use zcash_primitives::{
+    memo::Memo,
+    transaction::{components::OutPoint, TxId},
+};
 
-use super::{data::TransactionRecord, Pool};
+use super::super::{data::TransactionRecord, Pool};
 
-pub mod interface;
-pub use interface::ShieldedNoteInterface;
-pub mod transparent;
-pub use transparent::TransparentNote;
-pub mod sapling;
-pub use sapling::SaplingNote;
-
-#[derive(Debug)]
-pub struct OrchardNote {
-    pub diversifier: orchard::keys::Diversifier,
-    pub note: orchard::note::Note,
+pub struct SaplingNote {
+    pub diversifier: sapling_crypto::Diversifier,
+    pub note: sapling_crypto::Note,
 
     // The position of this note's value commitment in the global commitment tree
     // We need to create a witness to it, to spend
-    pub witnessed_position: Option<Position>,
+    pub(crate) witnessed_position: Option<Position>,
 
     // The note's index in its containing transaction
     pub(crate) output_index: Option<u32>,
 
-    pub(super) nullifier: Option<orchard::note::Nullifier>,
+    pub(super) nullifier: Option<sapling_crypto::Nullifier>,
 
     pub spent: Option<(TxId, u32)>, // If this note was confirmed spent. Todo: as related to unconfirmed spent, this is potential data incoherence
 
@@ -32,15 +30,35 @@ pub struct OrchardNote {
     pub memo: Option<Memo>,
     pub is_change: bool,
 
-    // If the spending key is available in the wallet (i.e., whether to keep witness up-to-date)
+    // If the spending key is available in the wallet (i.e., whether to keep witness up-to-date) Todo should this data point really be here?
     pub have_spending_key: bool,
 }
 
-impl ShieldedNoteInterface for OrchardNote {
-    type Diversifier = orchard::keys::Diversifier;
-    type Note = orchard::note::Note;
-    type Node = orchard::tree::MerkleHashOrchard;
-    type Nullifier = orchard::note::Nullifier;
+impl std::fmt::Debug for SaplingNote {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("SaplingNoteData")
+            .field("diversifier", &self.diversifier)
+            .field("note", &self.note)
+            .field("nullifier", &self.nullifier)
+            .field("spent", &self.spent)
+            .field("unconfirmed_spent", &self.unconfirmed_spent)
+            .field("memo", &self.memo)
+            .field("diversifier", &self.diversifier)
+            .field("note", &self.note)
+            .field("nullifier", &self.nullifier)
+            .field("spent", &self.spent)
+            .field("unconfirmed_spent", &self.unconfirmed_spent)
+            .field("memo", &self.memo)
+            .field("is_change", &self.is_change)
+            .finish_non_exhaustive()
+    }
+}
+
+impl ShieldedNoteInterface for SaplingNote {
+    type Diversifier = sapling_crypto::Diversifier;
+    type Note = sapling_crypto::Note;
+    type Node = sapling_crypto::Node;
+    type Nullifier = sapling_crypto::Nullifier;
 
     fn diversifier(&self) -> &Self::Diversifier {
         &self.diversifier
@@ -51,10 +69,10 @@ impl ShieldedNoteInterface for OrchardNote {
     }
 
     fn from_parts(
-        diversifier: Self::Diversifier,
-        note: Self::Note,
+        diversifier: sapling_crypto::Diversifier,
+        note: sapling_crypto::Note,
         witnessed_position: Option<Position>,
-        nullifier: Option<Self::Nullifier>,
+        nullifier: Option<sapling_crypto::Nullifier>,
         spent: Option<(TxId, u32)>,
         unconfirmed_spent: Option<(TxId, u32)>,
         memo: Option<Memo>,
@@ -77,12 +95,13 @@ impl ShieldedNoteInterface for OrchardNote {
     }
 
     fn get_deprecated_serialized_view_key_buffer() -> Vec<u8> {
-        vec![0u8; 96]
+        vec![0u8; 169]
     }
 
     fn have_spending_key(&self) -> bool {
         self.have_spending_key
     }
+
     fn is_change(&self) -> bool {
         self.is_change
     }
@@ -108,7 +127,7 @@ impl ShieldedNoteInterface for OrchardNote {
     }
 
     fn pool() -> Pool {
-        Pool::Orchard
+        Pool::Sapling
     }
 
     fn spent(&self) -> &Option<(TxId, u32)> {
@@ -120,13 +139,13 @@ impl ShieldedNoteInterface for OrchardNote {
     }
 
     fn transaction_metadata_notes(wallet_transaction: &TransactionRecord) -> &Vec<Self> {
-        &wallet_transaction.orchard_notes
+        &wallet_transaction.sapling_notes
     }
 
     fn transaction_metadata_notes_mut(
         wallet_transaction: &mut TransactionRecord,
     ) -> &mut Vec<Self> {
-        &mut wallet_transaction.orchard_notes
+        &mut wallet_transaction.sapling_notes
     }
 
     fn pending_spent(&self) -> &Option<(TxId, u32)> {
@@ -144,9 +163,11 @@ impl ShieldedNoteInterface for OrchardNote {
     fn witnessed_position(&self) -> &Option<Position> {
         &self.witnessed_position
     }
+
     fn witnessed_position_mut(&mut self) -> &mut Option<Position> {
         &mut self.witnessed_position
     }
+
     fn output_index(&self) -> &Option<u32> {
         &self.output_index
     }
