5b9a40700b6025ee08c20e94c4c659c9d9988001
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-15 17:52:11 +0000

    ... and cargo fixed it

--- zingolib/src/wallet/ledger/backend_walletwrite.rs
+++ zingolib/src/wallet/ledger/backend_walletwrite.rs
@@ -9,23 +9,23 @@ impl WalletWrite for ZingoLedger {
 
     fn create_account(
         &mut self,
-        seed: &SecretVec<u8>,
-        birthday: zcash_client_backend::data_api::AccountBirthday,
+        _seed: &SecretVec<u8>,
+        _birthday: zcash_client_backend::data_api::AccountBirthday,
     ) -> Result<(zcash_primitives::zip32::AccountId, UnifiedSpendingKey), Self::Error> {
         todo!()
     }
 
     fn get_next_available_address(
         &mut self,
-        account: zcash_primitives::zip32::AccountId,
-        request: zcash_client_backend::keys::UnifiedAddressRequest,
+        _account: zcash_primitives::zip32::AccountId,
+        _request: zcash_client_backend::keys::UnifiedAddressRequest,
     ) -> Result<Option<zcash_client_backend::address::UnifiedAddress>, Self::Error> {
         todo!()
     }
 
     fn put_blocks(
         &mut self,
-        blocks: Vec<
+        _blocks: Vec<
             zcash_client_backend::data_api::ScannedBlock<
                 sapling_crypto::Nullifier,
                 orchard::keys::Scope,
@@ -37,35 +37,35 @@ impl WalletWrite for ZingoLedger {
 
     fn update_chain_tip(
         &mut self,
-        tip_height: zcash_primitives::consensus::BlockHeight,
+        _tip_height: zcash_primitives::consensus::BlockHeight,
     ) -> Result<(), Self::Error> {
         todo!()
     }
 
     fn store_decrypted_tx(
         &mut self,
-        received_tx: zcash_client_backend::data_api::DecryptedTransaction,
+        _received_tx: zcash_client_backend::data_api::DecryptedTransaction,
     ) -> Result<(), Self::Error> {
         todo!()
     }
 
     fn store_sent_tx(
         &mut self,
-        sent_tx: &zcash_client_backend::data_api::SentTransaction,
+        _sent_tx: &zcash_client_backend::data_api::SentTransaction,
     ) -> Result<(), Self::Error> {
         todo!()
     }
 
     fn truncate_to_height(
         &mut self,
-        block_height: zcash_primitives::consensus::BlockHeight,
+        _block_height: zcash_primitives::consensus::BlockHeight,
     ) -> Result<(), Self::Error> {
         todo!()
     }
 
     fn put_received_transparent_utxo(
         &mut self,
-        output: &zcash_client_backend::wallet::WalletTransparentOutput,
+        _output: &zcash_client_backend::wallet::WalletTransparentOutput,
     ) -> Result<Self::UtxoRef, Self::Error> {
         todo!()
     }
