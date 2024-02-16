3fbfbe41a762f2914d70134028bbe8411e386a64
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-16 17:22:07 +0000

    a failing test of ZingoLedger!

--- zingolib/src/wallet/ledger.rs
+++ zingolib/src/wallet/ledger.rs
@@ -37,3 +37,52 @@ impl ZingoLedger {
         self.witness_trees.as_mut().map(WitnessTrees::clear);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use zcash_client_backend::{
+        address::Address,
+        data_api::wallet::input_selection::GreedyInputSelector,
+        fees::{self, standard::SingleOutputChangeStrategy, ChangeStrategy, DustOutputPolicy},
+        zip321::{self, Payment},
+    };
+    use zcash_primitives::transaction::{
+        components::amount::NonNegativeAmount, fees::StandardFeeRule,
+    };
+    use zingoconfig::ChainType;
+
+    use super::ZingoLedger;
+
+    #[test]
+    fn test() {
+        let amount = NonNegativeAmount::const_from_u64(20000);
+        let recipient_address =
+            Address::decode(&ChainType::Mainnet, &"squirrel".to_string()).unwrap();
+        let request = zip321::TransactionRequest::new(vec![Payment {
+        recipient_address,
+        amount,
+        memo: None,
+        label: None,
+        message: None,
+        other_params: vec![],
+    }])
+    .expect(
+        "It should not be possible for this to violate ZIP 321 request construction invariants.",
+    );
+
+        let change_strategy = SingleOutputChangeStrategy::new(StandardFeeRule::Zip317, None);
+        let input_selector = GreedyInputSelector::<ZingoLedger, _>::new(
+            change_strategy,
+            DustOutputPolicy::default(),
+        );
+
+        // propose_transfer(
+        //     wallet_db,
+        //     params,
+        //     spend_from_account,
+        //     &input_selector,
+        //     request,
+        //     min_confirmations,
+        // )
+    }
+}
