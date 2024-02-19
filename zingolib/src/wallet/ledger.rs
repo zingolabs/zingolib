e8640f6ad2b08580aa272448383032e6c4a13841
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-19 18:26:02 +0000

    fixed test_propose_transfer

--- zingolib/src/wallet/ledger.rs
+++ zingolib/src/wallet/ledger.rs
@@ -52,10 +52,10 @@ mod tests {
     use super::ZingoLedger;
 
     #[test]
-    fn test() {
+    fn test_propose_transfer() {
         let amount = NonNegativeAmount::const_from_u64(20000);
         let recipient_address =
-            Address::decode(&ChainType::Mainnet, &"squirrel".to_string()).unwrap();
+            Address::decode(&ChainType::Testnet, &"utest17wwv8nuvdnpjsxtu6ndz6grys5x8wphcwtzmg75wkx607c7cue9qz5kfraqzc7k9dfscmylazj4nkwazjj26s9rhyjxm0dcqm837ykgh2suv0at9eegndh3kvtfjwp3hhhcgk55y9d2ys56zkw8aaamcrv9cy0alj0ndvd0wll4gxhrk9y4yy9q9yg8yssrencl63uznqnkv7mk3w05".to_string()).unwrap();
         let request = zip321::TransactionRequest::new(vec![Payment {
         recipient_address,
         amount,
