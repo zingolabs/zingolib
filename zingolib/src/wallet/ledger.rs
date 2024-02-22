c1ce2631473d91489c8b3560de9cba8bee54198b
fluidvanadium <fluidvanadium@zingolabs.org>
2024-02-22 17:18:37 +0000

    upgraded test to use valid random orchard address

--- zingolib/src/wallet/ledger.rs
+++ zingolib/src/wallet/ledger.rs
@@ -41,15 +41,23 @@ impl ZingoLedger {
 mod tests {
     use std::num::NonZeroU32;
 
+    use incrementalmerkletree::Position;
+    use orchard::{
+        note::{Nullifier, RandomSeed},
+        note_encryption::OrchardDomain,
+        value::NoteValue,
+    };
     use zcash_client_backend::{
         address::Address,
         data_api::wallet::input_selection::GreedyInputSelector,
         fees::{self, standard::SingleOutputChangeStrategy, ChangeStrategy, DustOutputPolicy},
         zip321::{self, Payment},
     };
-    use zcash_primitives::transaction::{
-        components::amount::NonNegativeAmount, fees::StandardFeeRule,
+    use zcash_primitives::{
+        consensus::BlockHeight,
+        transaction::{components::amount::NonNegativeAmount, fees::StandardFeeRule, TxId},
     };
+    use zingo_status::confirmation_status::ConfirmationStatus;
     use zingoconfig::ChainType;
 
     use crate::error::ZingoLibError;
@@ -63,16 +71,15 @@ mod tests {
         let recipient_address =
             Address::decode(&ChainType::Testnet, &"utest17wwv8nuvdnpjsxtu6ndz6grys5x8wphcwtzmg75wkx607c7cue9qz5kfraqzc7k9dfscmylazj4nkwazjj26s9rhyjxm0dcqm837ykgh2suv0at9eegndh3kvtfjwp3hhhcgk55y9d2ys56zkw8aaamcrv9cy0alj0ndvd0wll4gxhrk9y4yy9q9yg8yssrencl63uznqnkv7mk3w05".to_string()).unwrap();
         let request = zip321::TransactionRequest::new(vec![Payment {
-        recipient_address,
-        amount,
-        memo: None,
-        label: None,
-        message: None,
-        other_params: vec![],
-    }])
-    .expect(
-        "It should not be possible for this to violate ZIP 321 request construction invariants.",
-    );
+            recipient_address,
+            amount,
+            memo: None,
+            label: None,
+            message: None,
+            other_params: vec![],
+        }])
+        .expect(
+            "It should not be possible for this to violate ZIP 321 request construction invariants.",);
 
         let change_strategy = SingleOutputChangeStrategy::new(StandardFeeRule::Zip317, None);
         let input_selector = GreedyInputSelector::<ZingoLedger, _>::new(
@@ -89,7 +96,40 @@ mod tests {
                 .unwrap(),
             None
         );
-        // ledger.add_new_note(, , , , , , , , )
+
+        dbg!("inventing transaction");
+        let txid = TxId::from_bytes([0; 32]);
+        let status = ConfirmationStatus::Confirmed(BlockHeight::from(100));
+        let timestamp = 123456;
+
+        dbg!("inventing nullifier");
+        let nullifier = Nullifier::from_bytes(&[0; 32]).unwrap();
+        dbg!("inventing address");
+        let internal_orchard_address = orchard::Address::from_raw_address_bytes(&[7; 43]).unwrap(); //this vector of 7s happens to work
+        dbg!("inventing note");
+        let note = {
+            let random_seed = RandomSeed::from_bytes([0; 32], &nullifier).unwrap();
+            orchard::note::Note::from_parts(
+                internal_orchard_address,
+                NoteValue::from_raw(40000),
+                nullifier,
+                random_seed,
+            )
+            .unwrap()
+        };
+        dbg!("adding note");
+        let position = Position::from(100);
+        ledger.add_new_note::<OrchardDomain>(
+            txid,
+            status,
+            timestamp,
+            note,
+            internal_orchard_address,
+            true,
+            Some(nullifier),
+            0,
+            position,
+        );
 
         let _ = zcash_client_backend::data_api::wallet::propose_transfer::<
             ZingoLedger,
