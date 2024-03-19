cbfb0cd03648cdc4317a75e2e3d6546f719a7a75
fluidvanadium <fluidvanadium@zingolabs.org>
2024-03-19 16:48:46 +0000

    added trait mods

--- zingolib/src/wallet/send_kit.rs
+++ zingolib/src/wallet/send_kit.rs
@@ -1,6 +1,11 @@
 use super::{data::WitnessTrees, record_book::RecordBook};
 use zcash_keys::keys::UnifiedSpendingKey;
 
+pub mod trait_inputsource;
+pub mod trait_walletcommitmenttrees;
+pub mod trait_walletread;
+pub mod trait_walletwrite;
+
 pub struct SendKit<'a> {
     pub key: &'a UnifiedSpendingKey,
     pub record_book: &'a RecordBook<'a>,
