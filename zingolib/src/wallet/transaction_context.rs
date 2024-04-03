47aed7c79f432d39cde964d9648be74b36baed79
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-03 22:38:31 +0000

    rename and rearrange decrypt_transaction mod

--- zingolib/src/wallet/transaction_context.rs
+++ zingolib/src/wallet/transaction_context.rs
@@ -55,46 +55,6 @@ pub mod decrypt_transaction {
     use super::TransactionContext;
 
     impl TransactionContext {
-        #[allow(clippy::too_many_arguments)]
-        async fn execute_bundlescans_internal(
-            &self,
-            transaction: &Transaction,
-            status: ConfirmationStatus,
-            block_time: u32,
-            is_outgoing_transaction: &mut bool,
-            outgoing_metadatas: &mut Vec<OutgoingTxData>,
-            arbitrary_memos_with_txids: &mut Vec<(ParsedMemo, TxId)>,
-            taddrs_set: &HashSet<String>,
-        ) {
-            //todo: investigate scanning all bundles simultaneously
-
-            self.scan_transparent_bundle(
-                transaction,
-                status,
-                block_time,
-                is_outgoing_transaction,
-                taddrs_set,
-            )
-            .await;
-            self.scan_sapling_bundle(
-                transaction,
-                status,
-                block_time,
-                is_outgoing_transaction,
-                outgoing_metadatas,
-                arbitrary_memos_with_txids,
-            )
-            .await;
-            self.scan_orchard_bundle(
-                transaction,
-                status,
-                block_time,
-                is_outgoing_transaction,
-                outgoing_metadatas,
-                arbitrary_memos_with_txids,
-            )
-            .await;
-        }
         pub(crate) async fn scan_full_tx(
             &self,
             transaction: &Transaction,
@@ -124,7 +84,7 @@ pub mod decrypt_transaction {
             }
             let mut outgoing_metadatas = vec![];
             // Execute scanning operations
-            self.execute_bundlescans_internal(
+            self.decrypt_transaction_to_record(
                 transaction,
                 status,
                 block_time,
@@ -179,61 +139,47 @@ pub mod decrypt_transaction {
             }
         }
 
-        async fn update_outgoing_txdatas_with_uas(
+        #[allow(clippy::too_many_arguments)]
+        async fn decrypt_transaction_to_record(
             &self,
-            txid_indexed_zingo_memos: Vec<(ParsedMemo, TxId)>,
+            transaction: &Transaction,
+            status: ConfirmationStatus,
+            block_time: u32,
+            is_outgoing_transaction: &mut bool,
+            outgoing_metadatas: &mut Vec<OutgoingTxData>,
+            arbitrary_memos_with_txids: &mut Vec<(ParsedMemo, TxId)>,
+            taddrs_set: &HashSet<String>,
         ) {
-            for (parsed_zingo_memo, txid) in txid_indexed_zingo_memos {
-                match parsed_zingo_memo {
-                    ParsedMemo::Version0 { uas } => {
-                        for ua in uas {
-                            if let Some(transaction) = self
-                                .transaction_metadata_set
-                                .write()
-                                .await
-                                .current
-                                .get_mut(&txid)
-                            {
-                                if !transaction.outgoing_tx_data.is_empty() {
-                                    let outgoing_potential_receivers = [
-                                        ua.orchard().map(|oaddr| {
-                                            oaddr.b32encode_for_network(&self.config.chain)
-                                        }),
-                                        ua.sapling().map(|zaddr| {
-                                            zaddr.b32encode_for_network(&self.config.chain)
-                                        }),
-                                        ua.transparent().map(|taddr| {
-                                            address_from_pubkeyhash(&self.config, *taddr)
-                                        }),
-                                        Some(ua.encode(&self.config.chain)),
-                                    ];
-                                    transaction
-                                        .outgoing_tx_data
-                                        .iter_mut()
-                                        .filter(|out_meta| {
-                                            outgoing_potential_receivers
-                                                .contains(&Some(out_meta.to_address.clone()))
-                                        })
-                                        .for_each(|out_metadata| {
-                                            out_metadata.recipient_ua =
-                                                Some(ua.encode(&self.config.chain))
-                                        })
-                                }
-                            }
-                        }
-                    }
-                    other_memo_version => {
-                        log::error!(
-                            "Wallet internal memo is from a future version of the protocol\n\
-                        Please ensure that your software is up-to-date.\n\
-                        Memo: {other_memo_version:?}"
-                        )
-                    }
-                }
-            }
-        }
+            //todo: investigate scanning all bundles simultaneously
 
-        async fn scan_transparent_bundle(
+            self.decrypt_transaction_to_record_transparent(
+                transaction,
+                status,
+                block_time,
+                is_outgoing_transaction,
+                taddrs_set,
+            )
+            .await;
+            self.decrypt_transaction_to_record_sapling(
+                transaction,
+                status,
+                block_time,
+                is_outgoing_transaction,
+                outgoing_metadatas,
+                arbitrary_memos_with_txids,
+            )
+            .await;
+            self.decrypt_transaction_to_record_orchard(
+                transaction,
+                status,
+                block_time,
+                is_outgoing_transaction,
+                outgoing_metadatas,
+                arbitrary_memos_with_txids,
+            )
+            .await;
+        }
+        async fn decrypt_transaction_to_record_transparent(
             &self,
             transaction: &Transaction,
             status: ConfirmationStatus,
@@ -320,8 +266,62 @@ pub mod decrypt_transaction {
                 );
             }
         }
+        async fn update_outgoing_txdatas_with_uas(
+            &self,
+            txid_indexed_zingo_memos: Vec<(ParsedMemo, TxId)>,
+        ) {
+            for (parsed_zingo_memo, txid) in txid_indexed_zingo_memos {
+                match parsed_zingo_memo {
+                    ParsedMemo::Version0 { uas } => {
+                        for ua in uas {
+                            if let Some(transaction) = self
+                                .transaction_metadata_set
+                                .write()
+                                .await
+                                .current
+                                .get_mut(&txid)
+                            {
+                                if !transaction.outgoing_tx_data.is_empty() {
+                                    let outgoing_potential_receivers = [
+                                        ua.orchard().map(|oaddr| {
+                                            oaddr.b32encode_for_network(&self.config.chain)
+                                        }),
+                                        ua.sapling().map(|zaddr| {
+                                            zaddr.b32encode_for_network(&self.config.chain)
+                                        }),
+                                        ua.transparent().map(|taddr| {
+                                            address_from_pubkeyhash(&self.config, *taddr)
+                                        }),
+                                        Some(ua.encode(&self.config.chain)),
+                                    ];
+                                    transaction
+                                        .outgoing_tx_data
+                                        .iter_mut()
+                                        .filter(|out_meta| {
+                                            outgoing_potential_receivers
+                                                .contains(&Some(out_meta.to_address.clone()))
+                                        })
+                                        .for_each(|out_metadata| {
+                                            out_metadata.recipient_ua =
+                                                Some(ua.encode(&self.config.chain))
+                                        })
+                                }
+                            }
+                        }
+                    }
+                    other_memo_version => {
+                        log::error!(
+                            "Wallet internal memo is from a future version of the protocol\n\
+                        Please ensure that your software is up-to-date.\n\
+                        Memo: {other_memo_version:?}"
+                        )
+                    }
+                }
+            }
+        }
+
         #[allow(clippy::too_many_arguments)]
-        async fn scan_sapling_bundle(
+        async fn decrypt_transaction_to_record_sapling(
             &self,
             transaction: &Transaction,
             status: ConfirmationStatus,
@@ -330,7 +330,7 @@ pub mod decrypt_transaction {
             outgoing_metadatas: &mut Vec<OutgoingTxData>,
             arbitrary_memos_with_txids: &mut Vec<(ParsedMemo, TxId)>,
         ) {
-            self.scan_bundle::<SaplingDomain>(
+            self.decrypt_transaction_to_record_domain::<SaplingDomain>(
                 transaction,
                 status,
                 block_time,
@@ -341,7 +341,7 @@ pub mod decrypt_transaction {
             .await
         }
         #[allow(clippy::too_many_arguments)]
-        async fn scan_orchard_bundle(
+        async fn decrypt_transaction_to_record_orchard(
             &self,
             transaction: &Transaction,
             status: ConfirmationStatus,
@@ -350,7 +350,7 @@ pub mod decrypt_transaction {
             outgoing_metadatas: &mut Vec<OutgoingTxData>,
             arbitrary_memos_with_txids: &mut Vec<(ParsedMemo, TxId)>,
         ) {
-            self.scan_bundle::<OrchardDomain>(
+            self.decrypt_transaction_to_record_domain::<OrchardDomain>(
                 transaction,
                 status,
                 block_time,
@@ -367,7 +367,7 @@ pub mod decrypt_transaction {
         /// In Orchard the components are "Actions", each of which
         /// _IS_ 1 Spend and 1 Output.
         #[allow(clippy::too_many_arguments)]
-        async fn scan_bundle<D>(
+        async fn decrypt_transaction_to_record_domain<D>(
             &self,
             transaction: &Transaction,
             status: ConfirmationStatus,
