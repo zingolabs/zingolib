ce4fa47a6c8978f14b125bc5d400d69fefa24f43
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-03 18:31:33 +0000

    created TxMapAndMaybeTrees

--- zingolib/src/wallet/transactions.rs
+++ zingolib/src/wallet/transactions.rs
@@ -7,7 +7,7 @@ use super::data::{TransactionRecord, WitnessTrees};
 /// HashMap of all transactions in a wallet, keyed by txid.
 /// Note that the parent is expected to hold a RwLock, so we will assume that all accesses to
 /// this struct are threadsafe/locked properly.
-pub struct TransactionMetadataSet {
+pub struct TxMapAndMaybeTrees {
     pub current: HashMap<TxId, TransactionRecord>,
     pub witness_trees: Option<WitnessTrees>,
 }
@@ -16,14 +16,14 @@ pub mod get;
 pub mod read_write;
 pub mod recording;
 
-impl TransactionMetadataSet {
-    pub(crate) fn new_with_witness_trees() -> TransactionMetadataSet {
+impl TxMapAndMaybeTrees {
+    pub(crate) fn new_with_witness_trees() -> TxMapAndMaybeTrees {
         Self {
             current: HashMap::default(),
             witness_trees: Some(WitnessTrees::default()),
         }
     }
-    pub(crate) fn new_treeless() -> TransactionMetadataSet {
+    pub(crate) fn new_treeless() -> TxMapAndMaybeTrees {
         Self {
             current: HashMap::default(),
             witness_trees: None,
