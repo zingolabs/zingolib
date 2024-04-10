44d931d9a7b9e9eb8d51176289bf00aef8e93e3c
zancas <zancas@zingolabs.org>
2024-04-10 13:38:57 -0600

    match path name to file name

--- zingolib/src/wallet/transactions.rs
+++ zingolib/src/wallet/transactions.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
 use super::data::WitnessTrees;
-use crate::wallet::transaction_record_map::TransactionRecordMap;
+use crate::wallet::transaction_records_by_id::TransactionRecordMap;
 
 /// HashMap of all transactions in a wallet, keyed by txid.
 /// Note that the parent is expected to hold a RwLock, so we will assume that all accesses to
