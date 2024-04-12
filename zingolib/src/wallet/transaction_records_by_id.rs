b80e734bd53349be9f629fe8642b109994cfa804
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-12 16:18:35 +0000

    clippy

--- zingolib/src/wallet/transaction_records_by_id.rs
+++ zingolib/src/wallet/transaction_records_by_id.rs
@@ -127,16 +127,16 @@ impl TransactionRecordsById {
 #[cfg(feature = "test-features")]
 mod tests {
     use crate::wallet::{
-        notes::{NoteRecordIdentifier, SaplingNote},
+        notes::{SaplingNote},
         transaction_record::TransactionRecord,
     };
 
     use super::TransactionRecordsById;
-    use zcash_client_backend::data_api::{InputSource, SpendableNotes};
+    
     use zcash_primitives::{
-        consensus::BlockHeight, transaction::components::amount::NonNegativeAmount,
+        consensus::BlockHeight,
     };
-    use zip32::AccountId;
+    
 
     #[test]
     fn invalidated_note_is_deleted() {
