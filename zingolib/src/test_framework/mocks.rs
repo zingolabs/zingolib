244c212efa7e80ca4243bf951c2d45a785fc1c2d
Oscar Pepper <oscarpepper@zingolabs.org>
2024-04-16 15:03:42 +0100

    removed unnecessary generics and added doc comments and examples

--- zingolib/src/test_framework/mocks.rs
+++ zingolib/src/test_framework/mocks.rs
@@ -2,15 +2,22 @@
 
 use std::collections::BTreeMap;
 
+use incrementalmerkletree::Position;
 use nonempty::NonEmpty;
+use sapling_crypto::value::NoteValue;
+use sapling_crypto::zip32::ExtendedSpendingKey;
+use sapling_crypto::Rseed;
+use zcash_client_backend::fees::TransactionBalance;
 use zcash_client_backend::proposal::{Proposal, ShieldedInputs, Step, StepOutput};
-use zcash_client_backend::wallet::WalletTransparentOutput;
+use zcash_client_backend::wallet::{ReceivedNote, WalletTransparentOutput};
+use zcash_client_backend::zip321::TransactionRequest;
 use zcash_client_backend::PoolType;
-use zcash_client_backend::{fees::TransactionBalance, zip321::TransactionRequest};
-use zcash_primitives::transaction::{components::amount::NonNegativeAmount, fees::zip317::FeeRule};
-use zcash_primitives::{consensus::BlockHeight, transaction::TxId};
+use zcash_primitives::consensus::BlockHeight;
+use zcash_primitives::transaction::{
+    components::amount::NonNegativeAmount, fees::zip317::FeeRule, TxId,
+};
 
-use crate::wallet::notes::TransparentNote;
+use crate::wallet::notes::{NoteRecordIdentifier, TransparentNote};
 
 macro_rules! build_method {
     ($name:ident, $localtype:ty) => {
@@ -72,16 +79,25 @@ impl Default for TransparentNoteBuilder {
     }
 }
 
-pub struct ProposalBuilder<FeeRuleT, NoteRef> {
-    fee_rule: Option<FeeRuleT>,
+/// Provides a builder for constructing a mock [`zcash_client_backend::proposal::Proposal`].
+///
+/// # Examples
+///
+/// ```
+/// use zingolib::test_framework::mocks::ProposalBuilder;
+///
+/// let proposal = ProposalBuilder::default().build();
+/// ````
+#[allow(dead_code)]
+pub struct ProposalBuilder {
+    fee_rule: Option<FeeRule>,
     min_target_height: Option<BlockHeight>,
-    steps: Option<NonEmpty<Step<NoteRef>>>,
+    steps: Option<NonEmpty<Step<NoteRecordIdentifier>>>,
 }
 
-impl<FeeRuleT, NoteRef> ProposalBuilder<FeeRuleT, NoteRef>
-where
-    NoteRef: Clone,
-{
+#[allow(dead_code)]
+impl ProposalBuilder {
+    /// Constructs a new [`ProposalBuilder`] with all fields as `None`.
     pub fn new() -> Self {
         ProposalBuilder {
             fee_rule: None,
@@ -90,11 +106,17 @@ where
         }
     }
 
-    build_method!(fee_rule, FeeRuleT);
+    build_method!(fee_rule, FeeRule);
     build_method!(min_target_height, BlockHeight);
-    build_method!(steps, NonEmpty<Step<NoteRef>>);
+    build_method!(steps, NonEmpty<Step<NoteRecordIdentifier>>);
 
-    pub fn build(self) -> Proposal<FeeRuleT, NoteRef> {
+    /// Builds a proposal after all fields have been set.
+    ///
+    /// # Panics
+    ///
+    /// `build` will panic if any fields of the builder are `None` or if the build failed
+    /// due to invalid values.
+    pub fn build(self) -> Proposal<FeeRule, NoteRecordIdentifier> {
         let step = self.steps.unwrap().first().clone();
         Proposal::single_step(
             step.transaction_request().clone(),
@@ -110,17 +132,37 @@ where
     }
 }
 
-pub struct StepBuilder<NoteRef> {
+impl Default for ProposalBuilder {
+    /// Constructs a new [`ProposalBuilder`] where all fields are preset to default values.
+    fn default() -> Self {
+        ProposalBuilder::new()
+            .fee_rule(FeeRule::standard())
+            .min_target_height(BlockHeight::from_u32(1))
+            .steps(NonEmpty::singleton(StepBuilder::default().build()))
+    }
+}
+
+/// Provides a builder for constructing a mock [`zcash_client_backend::proposal::Step`].
+///
+/// # Examples
+///
+/// ```
+/// use zingolib::test_framework::mocks::StepBuilder;
+///
+/// let step = StepBuilder::default().build();
+/// ````
+pub struct StepBuilder {
     transaction_request: Option<TransactionRequest>,
     payment_pools: Option<BTreeMap<usize, PoolType>>,
     transparent_inputs: Option<Vec<WalletTransparentOutput>>,
-    shielded_inputs: Option<Option<ShieldedInputs<NoteRef>>>,
+    shielded_inputs: Option<Option<ShieldedInputs<NoteRecordIdentifier>>>,
     prior_step_inputs: Option<Vec<StepOutput>>,
     balance: Option<TransactionBalance>,
     is_shielding: Option<bool>,
 }
 
-impl<NoteRef> StepBuilder<NoteRef> {
+impl StepBuilder {
+    /// Constructs a new [`StepBuilder`] with all fields as `None`.
     pub fn new() -> Self {
         StepBuilder {
             transaction_request: None,
@@ -137,12 +179,22 @@ impl<NoteRef> StepBuilder<NoteRef> {
     build_method!(payment_pools, BTreeMap<usize, PoolType>
     );
     build_method!(transparent_inputs, Vec<WalletTransparentOutput>);
-    build_method!(shielded_inputs, Option<ShieldedInputs<NoteRef>>);
+    build_method!(
+        shielded_inputs,
+        Option<ShieldedInputs<NoteRecordIdentifier>>
+    );
     build_method!(prior_step_inputs, Vec<StepOutput>);
     build_method!(balance, TransactionBalance);
     build_method!(is_shielding, bool);
 
-    pub fn build(self) -> Step<NoteRef> {
+    /// Builds a step after all fields have been set.
+    ///
+    /// # Panics
+    ///
+    /// `build` will panic if any fields of the builder are `None` or if the build failed
+    /// due to invalid values.
+    #[allow(dead_code)]
+    pub fn build(self) -> Step<NoteRecordIdentifier> {
         Step::from_parts(
             &[],
             self.transaction_request.unwrap(),
@@ -157,30 +209,43 @@ impl<NoteRef> StepBuilder<NoteRef> {
     }
 }
 
-impl<NoteRef> Default for StepBuilder<NoteRef> {
+impl Default for StepBuilder {
+    /// Constructs a new [`StepBuilder`] where all fields are preset to default values.
     fn default() -> Self {
+        let txid = TxId::from_bytes([0u8; 32]);
+        let seed = [0u8; 32];
+        let dfvk = ExtendedSpendingKey::master(&seed).to_diversifiable_full_viewing_key();
+        let (_, address) = dfvk.default_address();
+        let note = sapling_crypto::Note::from_parts(
+            address,
+            NoteValue::from_raw(20_000),
+            Rseed::AfterZip212([7; 32]),
+        );
+
         Self::new()
             .transaction_request(TransactionRequest::empty())
             .payment_pools(BTreeMap::new())
             .transparent_inputs(vec![])
-            .shielded_inputs(None)
+            // .shielded_inputs(None)
+            .shielded_inputs(Some(ShieldedInputs::from_parts(
+                BlockHeight::from_u32(1),
+                NonEmpty::singleton(ReceivedNote::from_parts(
+                    NoteRecordIdentifier {
+                        txid,
+                        pool: PoolType::Shielded(zcash_client_backend::ShieldedProtocol::Sapling),
+                        index: 0,
+                    },
+                    txid,
+                    0,
+                    zcash_client_backend::wallet::Note::Sapling(note),
+                    zip32::Scope::External,
+                    Position::from(1),
+                )),
+            )))
             .prior_step_inputs(vec![])
             .balance(
                 TransactionBalance::new(vec![], NonNegativeAmount::const_from_u64(20_000)).unwrap(),
             )
             .is_shielding(false)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn mock_proposal() {
-        let proposal = ProposalBuilder::new()
-            .fee_rule(FeeRule::standard())
-            .min_target_height(BlockHeight::from_u32(1))
-            .steps(NonEmpty::singleton(StepBuilder::<()>::default().build()));
     }
 }
