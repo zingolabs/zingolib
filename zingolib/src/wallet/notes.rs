6039f640f05de49aa3d31ea56865d8e6aee37b37
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-16 21:16:13 +0000

    brought NoteRecordIdentifierBuilder to standard

--- zingolib/src/wallet/notes.rs
+++ zingolib/src/wallet/notes.rs
@@ -19,6 +19,12 @@ pub struct NoteRecordIdentifier {
     pub index: u32,
 }
 
+impl NoteRecordIdentifier {
+    pub fn from_parts(txid: TxId, pool: PoolType, index: u32) -> Self {
+        NoteRecordIdentifier { txid, pool, index }
+    }
+}
+
 impl std::fmt::Display for NoteRecordIdentifier {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(
@@ -32,20 +38,50 @@ impl std::fmt::Display for NoteRecordIdentifier {
 #[cfg(any(feature = "test", feature = "test-features"))]
 pub mod mocks {
     //! Mock version of the struct for testing
-    use super::{
-        transparent::mocks::TransparentNoteBuilder, NoteRecordIdentifier, TransparentNote,
-    };
+    use zcash_client_backend::PoolType;
+    use zcash_primitives::transaction::TxId;
 
-    impl NoteRecordIdentifier {
-        #[allow(dead_code)]
-        pub(crate) fn mock() -> Self {
-            let transparent_note = TransparentNoteBuilder::default().build();
-            let index = 5u32;
-            NoteRecordIdentifier {
-                txid: transparent_note.txid,
-                pool: zcash_client_backend::PoolType::Transparent,
-                index,
+    use crate::test_framework::mocks::build_method;
+
+    use super::NoteRecordIdentifier;
+
+    pub(crate) struct NoteRecordIdentifierBuilder {
+        txid: Option<TxId>,
+        pool: Option<PoolType>,
+        index: Option<u32>,
+    }
+    #[allow(dead_code)] //TODO:  fix this gross hack that I tossed in to silence the language-analyzer false positive
+    impl NoteRecordIdentifierBuilder {
+        pub fn new() -> Self {
+            Self {
+                txid: None,
+                pool: None,
+                index: None,
             }
+        }
+        // Methods to set each field
+        build_method!(txid, TxId);
+        build_method!(pool, PoolType);
+        build_method!(index, u32);
+
+        // Build method
+        pub fn build(self) -> NoteRecordIdentifier {
+            NoteRecordIdentifier::from_parts(
+                self.txid.unwrap(),
+                self.pool.unwrap(),
+                self.index.unwrap(),
+            )
+        }
+    }
+
+    impl Default for NoteRecordIdentifierBuilder {
+        fn default() -> Self {
+            Self::new()
+                .txid(TxId::from_bytes([0u8; 32]))
+                .pool(PoolType::Shielded(
+                    zcash_client_backend::ShieldedProtocol::Orchard,
+                ))
+                .index(0)
         }
     }
 }
