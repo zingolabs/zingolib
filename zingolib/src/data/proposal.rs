820e8b562e2d5690c0d73d8368e9c974fdaf23e0
zancas <zancas@zingolabs.org>
2024-04-17 11:56:43 -0600

    add doc-comment to ShieldProposal

--- zingolib/src/data/proposal.rs
+++ zingolib/src/data/proposal.rs
@@ -9,6 +9,11 @@ use crate::wallet::notes::NoteRecordIdentifier;
 /// A proposed send to addresses.
 pub type TransferProposal = Proposal<FeeRule, NoteRecordIdentifier>;
 /// A proposed shielding.
+/// The zcash_client_backend Proposal type exposes a "NoteRef" generic
+/// parameter to track notes that are generated in proposal steps,
+/// but Shielding has no $INSERT CORRECT TERMinilogy HERE steps that produce
+/// outputs which are consume in the same proposal, rather the outputs
+/// are unspent shielded notes.
 pub type ShieldProposal = Proposal<FeeRule, Infallible>;
 
 /// The LightClient holds one proposal at a time while the user decides whether to accept the fee.
