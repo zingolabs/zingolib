5c48313b779b27ab5b236fddc33e382fc31b9f93
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-17 16:47:49 +0000

    added pending_spent_note_is_pending_spent test for TransparentNote

--- zingolib/src/wallet/notes/transparent.rs
+++ zingolib/src/wallet/notes/transparent.rs
@@ -233,3 +233,23 @@ pub mod mocks {
         }
     }
 }
+
+#[cfg(test)]
+pub mod tests {
+    use crate::{
+        test_framework::mocks::default_txid,
+        wallet::notes::{transparent::mocks::TransparentNoteBuilder, NoteInterface},
+    };
+
+    #[test]
+    fn pending_spent_note_is_pending_spent() {
+        let spend = Some((default_txid(), 112358));
+        let note = TransparentNoteBuilder::default()
+            .unconfirmed_spent(spend)
+            .build();
+        assert_eq!(note.is_spent(), false);
+        assert_eq!(note.is_pending_spent(), true);
+        assert_eq!(note.is_spent_or_pending_spent(), true);
+        assert_eq!(note.pending_spent(), &spend);
+    }
+}
