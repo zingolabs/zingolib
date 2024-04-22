07f5649bf1474226a8289d607d6e9520bfa0001f
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-22 19:15:50 +0000

    rearrange transaction_record methods

--- zingolib/src/wallet/transaction_record.rs
+++ zingolib/src/wallet/transaction_record.rs
@@ -179,6 +179,25 @@ impl TransactionRecord {
         sum
     }
 
+    /// Sums all the received notes in the transaction.
+    pub fn total_value_received(&self) -> u64 {
+        self.query_sum_value(OutputQuery::stipulations(
+            true, true, true, true, true, true,
+        ))
+    }
+
+    /// TODO: Add Doc Comment Here!
+    pub fn pool_value_received<D: DomainWalletExt>(&self) -> u64
+    where
+        <D as zcash_note_encryption::Domain>::Note: PartialEq + Clone,
+        <D as zcash_note_encryption::Domain>::Recipient: super::traits::Recipient,
+    {
+        D::to_notes_vec(self)
+            .iter()
+            .map(|note_and_metadata| note_and_metadata.value())
+            .sum()
+    }
+
     /// TODO: Add Doc Comment Here!
     pub fn get_transparent_value_spent(&self) -> u64 {
         self.total_transparent_value_spent
@@ -238,28 +257,9 @@ impl TransactionRecord {
     }
 
     /// TODO: Add Doc Comment Here!
-    pub fn pool_value_received<D: DomainWalletExt>(&self) -> u64
-    where
-        <D as zcash_note_encryption::Domain>::Note: PartialEq + Clone,
-        <D as zcash_note_encryption::Domain>::Recipient: super::traits::Recipient,
-    {
-        D::to_notes_vec(self)
-            .iter()
-            .map(|note_and_metadata| note_and_metadata.value())
-            .sum()
-    }
-
-    /// TODO: Add Doc Comment Here!
     pub fn total_change_returned(&self) -> u64 {
         self.pool_change_returned::<sapling_crypto::note_encryption::SaplingDomain>()
             + self.pool_change_returned::<orchard::note_encryption::OrchardDomain>()
-    }
-
-    /// Sums all the received notes in the transaction.
-    pub fn total_value_received(&self) -> u64 {
-        self.query_sum_value(OutputQuery::stipulations(
-            true, true, true, true, true, true,
-        ))
     }
 
     /// TODO: Add Doc Comment Here!
