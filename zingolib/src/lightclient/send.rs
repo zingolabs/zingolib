4dcb9ab3b4ed5d08c9cfcf2b31bedea1faa1fa61
fluidvanadium <fluidvanadium@zingolabs.org>
2024-04-29 22:29:16 +0000

    do_send_proposed returns DoSendProposedError

--- zingolib/src/lightclient/send.rs
+++ zingolib/src/lightclient/send.rs
@@ -32,7 +32,7 @@ impl LightClient {
         self.do_propose_spend(receivers)
             .await
             .map_err(|e| e.to_string())?;
-        self.do_send_proposed().await
+        self.do_send_proposed().await.map_err(|e| e.to_string())
     }
 
     /// TODO: Add Doc Comment Here!
@@ -112,16 +112,38 @@ impl LightClient {
 
     /// Unstable function to expose the zip317 interface for development
     // TODO: add correct functionality and doc comments / tests
-    pub async fn do_send_proposed(&self) -> Result<NonEmpty<TxId>, String> {
+    pub async fn do_send_proposed(&self) -> Result<NonEmpty<TxId>, DoSendProposedError> {
+        if self
+            .wallet
+            .transaction_context
+            .transaction_metadata_set
+            .read()
+            .await
+            .witness_trees()
+            .is_none()
+        {
+            return Err(DoSendProposedError::NoSpendCapability);
+        }
+
         use std::ops::DerefMut;
 
         use zcash_keys::keys::UnifiedSpendingKey;
 
-        let (sapling_output, sapling_spend) = self.read_sapling_params()?;
-        let sapling_prover = LocalTxProver::from_bytes(&sapling_spend, &sapling_output);
+        if let Some(proposal) = self.latest_proposal.read().await.as_ref() {
+            let submission_height = self
+                .get_submission_height()
+                .await
+                .map_err(DoSendProposedError::SubmissionHeight)?;
 
-        if let Some(transfer_proposal) = self.latest_proposal.read().await.as_ref() {
-            match transfer_proposal {
+            let (sapling_output, sapling_spend) = self
+                .read_sapling_params()
+                .map_err(DoSendProposedError::SaplingParams)?;
+            let sapling_prover = LocalTxProver::from_bytes(&sapling_spend, &sapling_output);
+            let unified_spend_key =
+                UnifiedSpendingKey::try_from(self.wallet.wallet_capability().as_ref())
+                    .map_err(DoSendProposedError::UnifiedSpendKey)?;
+
+            match proposal {
                 crate::lightclient::ZingoProposal::Transfer(transfer_proposal) => {
                     let mut step_results = Vec::with_capacity(transfer_proposal.steps().len());
                     let mut txids = Vec::with_capacity(transfer_proposal.steps().len());
@@ -139,29 +161,19 @@ impl LightClient {
                                 &self.wallet.transaction_context.config.chain,
                                 &sapling_prover,
                                 &sapling_prover,
-                                &UnifiedSpendingKey::try_from(
-                                    self.wallet.wallet_capability().as_ref(),
-                                )
-                                .map_err(|e| e.to_string())?,
+                                &unified_spend_key,
                                 zcash_client_backend::wallet::OvkPolicy::Sender,
                                 transfer_proposal.fee_rule(),
                                 transfer_proposal.min_target_height(),
                                 &step_results,
                                 step,
                             )
-                            .map_err(
-                                |e: zcash_client_backend::data_api::error::Error<
-                                    crate::wallet::tx_map_and_maybe_trees::TxMapAndMaybeTreesTraitError,
-                                    std::convert::Infallible,
-                                    std::convert::Infallible,
-                                    zcash_primitives::transaction::fees::zip317::FeeError,
-                                >| e.to_string(),
-                            )?;
+                            .map_err(DoSendProposedError::Calculation)?;
                         let txid = self
                             .wallet
                             .send_to_addresses_inner(
                                 step_result.transaction(),
-                                self.get_submission_height().await?,
+                                submission_height,
                                 |transaction_bytes| {
                                     crate::grpc_connector::send_transaction(
                                         self.get_server_uri(),
@@ -169,11 +181,12 @@ impl LightClient {
                                     )
                                 },
                             )
-                            .await?;
+                            .await
+                            .map_err(DoSendProposedError::Broadcast)?;
                         step_results.push((step, step_result));
                         txids.push(txid);
                     }
-                    Ok(NonEmpty::from_vec(txids).expect("we can toDO better than an expect error"))
+                    Ok(NonEmpty::from_vec(txids).expect("nonempty"))
                 }
                 crate::lightclient::ZingoProposal::Shield(_) => {
                     todo!();
@@ -181,7 +194,36 @@ impl LightClient {
                 }
             }
         } else {
-            Err("No proposal. Call do_propose first.".to_string())
+            Err(DoSendProposedError::NoProposal)
         }
     }
+}
+
+use thiserror::Error;
+
+/// Errors that can result from do_propose
+#[allow(missing_docs)] // error types document themselves
+#[derive(Debug, Error)]
+pub enum DoSendProposedError {
+    #[error("No witness trees. This is viewkey watch, not spendkey wallet.")]
+    NoSpendCapability,
+    #[error("No proposal. Call do_propose first.")]
+    NoProposal,
+    #[error("Cant get submission height. Server connection?: {}", {0})]
+    SubmissionHeight(String),
+    #[error("Could not load sapling_params: {}", {0})]
+    SaplingParams(String),
+    #[error("Could not find UnifiedSpendKey: {}", {0})]
+    UnifiedSpendKey(std::io::Error),
+    #[error("No proposal. Call do_propose first.")]
+    Calculation(
+        zcash_client_backend::data_api::error::Error<
+            crate::wallet::tx_map_and_maybe_trees::TxMapAndMaybeTreesTraitError,
+            std::convert::Infallible,
+            std::convert::Infallible,
+            zcash_primitives::transaction::fees::zip317::FeeError,
+        >,
+    ),
+    #[error("Broadcast failed: {}", {0})]
+    Broadcast(String),
 }
