838b2a74b3565c563ad1cf6245e4524fda03513f
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-06 21:35:13 +0000

    fixed a warning

--- darkside-tests/src/lib.rs
+++ darkside-tests/src/lib.rs
@@ -1,6 +1,8 @@
-pub mod chain_generic_tests;
 pub mod constants;
 pub mod utils;
 pub mod darkside_types {
     tonic::include_proto!("cash.z.wallet.sdk.rpc");
 }
+
+#[cfg(test)]
+pub mod chain_generic_tests;
