877edaedd6070715a8cbb6e306727507ff7a994e
Oscar Pepper <oscarpepper@zingolabs.org>
2024-05-08 11:57:01 +0100

    fix warnings

--- zingolib/src/commands/error.rs
+++ zingolib/src/commands/error.rs
@@ -3,6 +3,7 @@ use std::fmt;
 #[derive(Debug)]
 pub(crate) enum CommandError {
     ArgsNotJson(json::Error),
+    #[cfg(feature = "zip317")]
     ArgNotJsonOrValidAddress,
     SingleArgNotJsonArray(String),
     EmptyJsonArray,
@@ -15,6 +16,7 @@ pub(crate) enum CommandError {
     NonJsonNumberForAmount(String),
     ConversionFailed(crate::utils::ConversionError),
     InvalidPool,
+    #[cfg(feature = "zip317")]
     MultipleReceivers,
 }
 
@@ -24,6 +26,7 @@ impl fmt::Display for CommandError {
 
         match self {
             ArgsNotJson(e) => write!(f, "failed to parse argument. {}", e),
+            #[cfg(feature = "zip317")]
             ArgNotJsonOrValidAddress => write!(
                 f,
                 "argument cannot be converted to a valid address or parsed as json."
@@ -43,6 +46,7 @@ impl fmt::Display for CommandError {
             NonJsonNumberForAmount(e) => write!(f, "invalid argument. expected a number. {}", e),
             ConversionFailed(e) => write!(f, "conversion failed. {}", e),
             InvalidPool => write!(f, "invalid pool."),
+            #[cfg(feature = "zip317")]
             MultipleReceivers => write!(f, "'send all' can only accept one receiver."),
         }
     }
