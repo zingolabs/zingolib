c74d5837f98f401aaec844574b3b8b4a70eedb89
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-15 02:39:44 +0000

    started assertion function

--- zingo-testutils/src/assertions.rs
+++ zingo-testutils/src/assertions.rs
@@ -1,11 +1,14 @@
 //! contains functions that compare structs to see if they match
 
-use zcash_client_backend::proposal::Proposal;
+use zcash_client_backend::{
+    proposal::{Proposal, Step},
+    zip321::Payment,
+};
 use zcash_primitives::transaction::TxId;
 
 use zingolib::lightclient::LightClient;
 
-/// call this after calling complete_and_broadcast_proposal
+/// call this after calling complete_and_broadcast_proposalx
 /// and again after bumping the chain and syncing
 pub async fn assert_client_matches_proposal<NoteId>(
     client: &LightClient,
@@ -25,5 +28,23 @@ pub async fn assert_client_matches_proposal<NoteId>(
 
     let status = transaction_record.status;
 
-    //todo
+    //multi-step proposals are not yet supported
+    assert_eq!(proposal.steps().len(), 1);
+    let step: &Step<NoteId> = proposal.steps().first();
+    for (_, payment) in step.transaction_request().payments().into_iter() {
+        // assert_client_matches_payment(client, payment, status);
+    }
+
+    step.transparent_inputs();
+    step.shielded_inputs();
+    step.balance();
+}
+
+/// checks that a client has recorded a payment, given a certain status
+pub async fn assert_client_matches_payment<NoteId>(
+    client: &LightClient,
+    payment: Payment,
+    // status: ConfirmationStatus,
+) {
+    // if client.includes_address(payment.address()
 }
