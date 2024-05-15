8774eb20b207217294f156da3a1ab28146f28403
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-15 01:44:39 +0000

    added doc-comments and mods to chain_generic_tests

--- zingo-testutils/src/chain_generic_tests.rs
+++ zingo-testutils/src/chain_generic_tests.rs
@@ -1,156 +1,179 @@
 //! tests that can be run either as lib-to-node or darkside.
 
-use zcash_client_backend::PoolType;
-use zcash_client_backend::PoolType::Shielded;
-use zcash_client_backend::PoolType::Transparent;
-use zcash_client_backend::ShieldedProtocol::Orchard;
-use zcash_client_backend::ShieldedProtocol::Sapling;
-use zcash_primitives::transaction::fees::zip317::MARGINAL_FEE;
+//! this mod tests fair weather behavior i.e. the LightClient is connected to a server that provides expected responses about the state of the blockchain.
+//! there are many ways to mock the chain. for simplicity, and in order to be usable in multiple contexts, the test fixtures in this mod delegate setup and server management to a ChainConductor (anything that implements ConductChain)
+//
 
-use zingolib::lightclient::LightClient;
-use zingolib::wallet::notes::query::OutputQuery;
-use zingolib::wallet::notes::query::OutputSpendStatusQuery;
-use zingolib::{get_base_address, wallet::notes::query::OutputPoolQuery};
-
-#[allow(async_fn_in_trait)]
-#[allow(opaque_hidden_inferred_bound)]
 /// A Lightclient test may involve hosting a server to send data to the LightClient. This trait can be asked to set simple scenarios where a mock LightServer sends data showing a note to a LightClient, the LightClient updates and responds by sending the note, and the Lightserver accepts the transaction and rebroadcasts it...
 /// The initial two implementors are
 /// lib-to-node, which links a lightserver to a zcashd in regtest mode. see `impl ConductChain for LibtoNode
 /// darkside, a mode for the lightserver which mocks zcashd. search 'impl ConductChain for DarksideScenario
-pub trait ConductChain {
-    /// set up the test chain
-    async fn setup() -> Self;
-    /// builds a faucet (funded from mining)
-    async fn create_faucet(&mut self) -> LightClient;
-    /// builds an empty client
-    async fn create_client(&mut self) -> LightClient;
-    /// moves the chain tip forward, confirming transactions that need to be confirmed
-    async fn bump_chain(&mut self);
+pub mod conduct_chain {
+    use zingolib::get_base_address;
+    use zingolib::lightclient::LightClient;
 
-    /// builds a client and funds it in orchard and syncs it
-    async fn fund_client(&mut self, value: u64) -> LightClient {
-        let sender = self.create_faucet().await;
-        let recipient = self.create_client().await;
+    #[allow(async_fn_in_trait)]
+    #[allow(opaque_hidden_inferred_bound)]
+    /// a trait (capability) for operating a server.
+    /// delegates client setup, because different mock servers require different client configuration
+    /// currently, the server conductor is limited to adding to the mock blockchain linearly (bump chain)
+    pub trait ConductChain {
+        /// set up the test chain
+        async fn setup() -> Self;
+        /// builds a faucet (funded from mining)
+        async fn create_faucet(&mut self) -> LightClient;
+        /// builds an empty client
+        async fn create_client(&mut self) -> LightClient;
 
-        self.bump_chain().await;
-        sender.do_sync(false).await.unwrap();
+        /// moves the chain tip forward, creating 1 new block
+        /// and confirming transactions that were received by the server
+        async fn bump_chain(&mut self);
 
-        sender
-            .send_test_only(vec![(
-                (get_base_address!(recipient, "unified")).as_str(),
-                value,
-                None,
-            )])
-            .await
-            .unwrap();
+        /// builds a client and funds it in orchard and syncs it
+        async fn fund_client(&mut self, value: u64) -> LightClient {
+            let sender = self.create_faucet().await;
+            let recipient = self.create_client().await;
 
-        self.bump_chain().await;
+            self.bump_chain().await;
+            sender.do_sync(false).await.unwrap();
 
-        recipient.do_sync(false).await.unwrap();
+            sender
+                .send_test_only(vec![(
+                    (get_base_address!(recipient, "unified")).as_str(),
+                    value,
+                    None,
+                )])
+                .await
+                .unwrap();
 
-        recipient
+            self.bump_chain().await;
+
+            recipient.do_sync(false).await.unwrap();
+
+            recipient
+        }
     }
 }
 
-/// runs a send-to-receiver and receives it in a chain-generic context
-pub async fn propose_and_broadcast_value_to_pool<TE>(send_value: u64, pooltype: PoolType)
-where
-    TE: ConductChain,
-{
-    let mut environment = TE::setup().await;
+/// these functions are each meant to be 'test-in-a-box'
+/// simply plug in a mock server as a chain conductor and provide some values
+pub mod fixtures {
+    use zcash_client_backend::PoolType;
+    use zcash_client_backend::PoolType::Shielded;
+    use zcash_client_backend::PoolType::Transparent;
+    use zcash_client_backend::ShieldedProtocol::Orchard;
+    use zcash_client_backend::ShieldedProtocol::Sapling;
+    use zcash_primitives::transaction::fees::zip317::MARGINAL_FEE;
 
-    println!("chain set up, funding client now");
+    use zingolib::wallet::notes::query::OutputPoolQuery;
+    use zingolib::wallet::notes::query::OutputQuery;
+    use zingolib::wallet::notes::query::OutputSpendStatusQuery;
 
-    let expected_fee = MARGINAL_FEE.into_u64()
-        * match pooltype {
-            Transparent => 3,
-            Shielded(Sapling) => 4,
-            Shielded(Orchard) => 2,
-        };
+    use crate::chain_generic_tests::conduct_chain::ConductChain;
 
-    let sender = environment.fund_client(send_value + expected_fee).await;
+    /// runs a send-to-receiver and receives it in a chain-generic context
+    pub async fn propose_and_broadcast_value_to_pool<TE>(send_value: u64, pooltype: PoolType)
+    where
+        TE: ConductChain,
+    {
+        let mut environment = TE::setup().await;
 
-    println!("client is ready to send");
-    dbg!(sender.query_sum_value(OutputQuery::any()).await);
-    dbg!(send_value);
+        println!("chain set up, funding client now");
 
-    let recipient = environment.create_client().await;
-    let recipient_address = recipient.get_base_address(pooltype).await;
-    let request = recipient
-        .transaction_request_from_send_inputs(vec![(recipient_address.as_str(), send_value, None)])
-        .unwrap();
+        let expected_fee = MARGINAL_FEE.into_u64()
+            * match pooltype {
+                Transparent => 3,
+                Shielded(Sapling) => 4,
+                Shielded(Orchard) => 2,
+            };
 
-    println!("recipient ready");
+        let sender = environment.fund_client(send_value + expected_fee).await;
 
-    sender.propose_send(request).await.unwrap();
-    sender
-        .complete_and_broadcast_stored_proposal()
-        .await
-        .unwrap();
+        println!("client is ready to send");
+        dbg!(sender.query_sum_value(OutputQuery::any()).await);
+        dbg!(send_value);
 
-    environment.bump_chain().await;
+        let recipient = environment.create_client().await;
+        let recipient_address = recipient.get_base_address(pooltype).await;
+        let request = recipient
+            .transaction_request_from_send_inputs(vec![(
+                recipient_address.as_str(),
+                send_value,
+                None,
+            )])
+            .unwrap();
 
-    recipient.do_sync(false).await.unwrap();
+        println!("recipient ready");
 
-    assert_eq!(
-        recipient
-            .query_sum_value(OutputQuery {
-                spend_status: OutputSpendStatusQuery {
-                    unspent: true,
-                    pending_spent: false,
-                    spent: false,
-                },
-                pools: OutputPoolQuery::one_pool(pooltype),
-            })
-            .await,
-        send_value
-    );
-}
+        sender.propose_send(request).await.unwrap();
+        sender
+            .complete_and_broadcast_stored_proposal()
+            .await
+            .unwrap();
 
-/// creates a proposal, sends it and receives it (upcoming: compares that it was executed correctly) in a chain-generic context
-pub async fn send_value_to_pool<TE>(send_value: u64, pooltype: PoolType)
-where
-    TE: ConductChain,
-{
-    let mut environment = TE::setup().await;
+        environment.bump_chain().await;
 
-    dbg!("chain set up, funding client now");
+        recipient.do_sync(false).await.unwrap();
 
-    let sender = environment
-        .fund_client(send_value + 2 * (MARGINAL_FEE.into_u64()))
-        .await;
+        assert_eq!(
+            recipient
+                .query_sum_value(OutputQuery {
+                    spend_status: OutputSpendStatusQuery {
+                        unspent: true,
+                        pending_spent: false,
+                        spent: false,
+                    },
+                    pools: OutputPoolQuery::one_pool(pooltype),
+                })
+                .await,
+            send_value
+        );
+    }
 
-    dbg!("client is ready to send");
-    dbg!(sender.query_sum_value(OutputQuery::any()).await);
-    dbg!(send_value);
+    /// creates a proposal, sends it and receives it (upcoming: compares that it was executed correctly) in a chain-generic context
+    pub async fn send_value_to_pool<TE>(send_value: u64, pooltype: PoolType)
+    where
+        TE: ConductChain,
+    {
+        let mut environment = TE::setup().await;
 
-    let recipient = environment.create_client().await;
-    let recipient_address = recipient.get_base_address(pooltype).await;
+        dbg!("chain set up, funding client now");
 
-    dbg!("recipient ready");
-    dbg!(recipient.query_sum_value(OutputQuery::any()).await);
+        let sender = environment
+            .fund_client(send_value + 2 * (MARGINAL_FEE.into_u64()))
+            .await;
 
-    sender
-        .send_test_only(vec![(dbg!(recipient_address).as_str(), send_value, None)])
-        .await
-        .unwrap();
+        dbg!("client is ready to send");
+        dbg!(sender.query_sum_value(OutputQuery::any()).await);
+        dbg!(send_value);
 
-    environment.bump_chain().await;
+        let recipient = environment.create_client().await;
+        let recipient_address = recipient.get_base_address(pooltype).await;
 
-    recipient.do_sync(false).await.unwrap();
+        dbg!("recipient ready");
+        dbg!(recipient.query_sum_value(OutputQuery::any()).await);
 
-    assert_eq!(
-        recipient
-            .query_sum_value(OutputQuery {
-                spend_status: OutputSpendStatusQuery {
-                    unspent: true,
-                    pending_spent: false,
-                    spent: false,
-                },
-                pools: OutputPoolQuery::one_pool(pooltype),
-            })
-            .await,
-        send_value
-    );
+        sender
+            .send_test_only(vec![(dbg!(recipient_address).as_str(), send_value, None)])
+            .await
+            .unwrap();
+
+        environment.bump_chain().await;
+
+        recipient.do_sync(false).await.unwrap();
+
+        assert_eq!(
+            recipient
+                .query_sum_value(OutputQuery {
+                    spend_status: OutputSpendStatusQuery {
+                        unspent: true,
+                        pending_spent: false,
+                        spent: false,
+                    },
+                    pools: OutputPoolQuery::one_pool(pooltype),
+                })
+                .await,
+            send_value
+        );
+    }
 }
