1be4462537e413cf2591dbf9b646b927c0448f71
zancas <zancas@zingolabs.org>
2024-05-16 15:30:43 -0600

    this appears to be unused kruft

--- zingolib/src/test_framework/macros.rs
+++ zingolib/src/test_framework/macros.rs
@@ -1,20 +1,3 @@
-/// We use this macro to remove repetitive test set-up and teardown in the zingolib unit tests.
-#[macro_export]
-macro_rules! apply_scenario {
-    ($inner_test_name:ident $numblocks:literal) => {
-        concat_idents::concat_idents!(
-            test_name = scenario_, $inner_test_name {
-                #[tokio::test]
-                async fn test_name() {
-                    let (scenario, stop_transmitter, test_server_handle) = $crate::lightclient::test_server::setup_n_block_fcbl_scenario($numblocks).await;
-                    $inner_test_name(scenario).await;
-                    clean_shutdown(stop_transmitter, test_server_handle).await;
-                }
-            }
-        );
-    };
-}
-
 /// Note that do_addresses returns an array, each element is a JSON representation
 /// of a UA.  Legacy addresses can be extracted from the receivers, per:
 /// <https://zips.z.cash/zip-0316>
