0f297af4a11e2ee0f9279056ca7f492482508839
zancas <zancas@zingolabs.org>
2024-05-16 09:13:36 -0600

    cut out do_send and do_shield
    (cherry picked from commit 0a658c9be358113909dcb8d43f02773062da2085)

--- zingolib/src/lightclient/test_features.rs
+++ zingolib/src/lightclient/test_features.rs
@@ -8,8 +8,7 @@ use zcash_primitives::transaction::TxId;
 use crate::{
     data::{proposal::TransferProposal, receivers::transaction_request_from_receivers},
     error::ZingoLibError,
-    utils::conversion::{address_from_str, testing::receivers_from_send_inputs},
-    wallet::Pool,
+    utils::conversion::testing::receivers_from_send_inputs,
 };
 
 use super::{propose::ProposeSendError, send::send_with_proposal::QuickSendError, LightClient};
@@ -55,38 +54,6 @@ impl LightClient {
             .transaction_request_from_send_inputs(address_amount_memo_tuples)
             .expect("should be able to create a transaction request as receivers are valid.");
         self.quick_send(request).await
-    }
-
-    /// Test only lightclient method for calling `do_send` with primitive rust types
-    ///
-    /// # Panics
-    ///
-    /// Panics if the address, amount or memo conversion fails.
-    pub async fn send_from_send_inputs(
-        &self,
-        address_amount_memo_tuples: Vec<(&str, u64, Option<&str>)>,
-    ) -> Result<String, String> {
-        let receivers =
-            receivers_from_send_inputs(address_amount_memo_tuples, &self.config().chain);
-        self.do_send(receivers).await.map(|txid| txid.to_string())
-    }
-
-    /// Test only lightclient method for calling `do_shield` with an address as &str
-    ///
-    /// # Panics
-    ///
-    /// Panics if the address conversion fails.
-    pub async fn shield_from_shield_inputs(
-        &self,
-        pools_to_shield: &[Pool],
-        address: Option<&str>,
-    ) -> Result<String, String> {
-        let address = address.map(|addr| {
-            address_from_str(addr, &self.config().chain).expect("should be a valid address")
-        });
-        self.do_shield(pools_to_shield, address)
-            .await
-            .map(|txid| txid.to_string())
     }
 
     /// gets the first address that will allow a sender to send to a specific pool, as a string
