e906b37b5b6919cadda7007ae929844fa3d698c4
zancas <zancas@zingolabs.org>
2024-05-18 18:09:03 -0600

    prefer explicit literal over constructor

--- zingolib/src/wallet/notes.rs
+++ zingolib/src/wallet/notes.rs
@@ -14,7 +14,8 @@ use zcash_client_backend::PoolType;
 
 use zcash_primitives::transaction::TxId;
 
-/// This triple of values uniquely identifies a value transfer on a zcash blockchain.
+/// This triple of values uniquely over-identifies a value transfer on a zcash blockchain.
+/// "Over" because pool is not necessary for a unique ID.
 #[derive(Clone, Copy, Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct OutputId {
     /// A unique ID of the transaction.  For v5 protocol and later, to enforce
@@ -131,13 +132,37 @@ pub mod tests {
             .build();
         let sapling_spent_note = SaplingNoteBuilder::default().spent(spend).clone().build();
 
-        let unspent_query = OutputSpendStatusQuery::new(true, false, false);
-        let pending_or_spent_query = OutputSpendStatusQuery::new(false, true, true);
-        let spent_query = OutputSpendStatusQuery::new(false, false, true);
+        let unspent_query = OutputSpendStatusQuery {
+            unspent: true,
+            pending_spent: false,
+            spent: false,
+        };
+        let pending_or_spent_query = OutputSpendStatusQuery {
+            unspent: false,
+            pending_spent: true,
+            spent: true,
+        };
+        let spent_query = OutputSpendStatusQuery {
+            unspent: false,
+            pending_spent: false,
+            spent: true,
+        };
 
-        let transparent_query = OutputPoolQuery::new(true, false, false);
-        let shielded_query = OutputPoolQuery::new(false, true, true);
-        let any_pool_query = OutputPoolQuery::new(true, true, true);
+        let transparent_query = OutputPoolQuery {
+            transparent: true,
+            sapling: false,
+            orchard: false,
+        };
+        let shielded_query = OutputPoolQuery {
+            transparent: false,
+            sapling: true,
+            orchard: true,
+        };
+        let any_pool_query = OutputPoolQuery {
+            transparent: true,
+            sapling: true,
+            orchard: true,
+        };
 
         let unspent_transparent_query = OutputQuery::new(unspent_query, transparent_query);
         let unspent_any_pool_query = OutputQuery::new(unspent_query, any_pool_query);
