4c701f96b561951bd1943e06e122da9c34b68d07
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-23 18:36:56 +0000

    add with_assertions mod and an asserted version of send

--- zingo-testutils/src/lightclient.rs
+++ zingo-testutils/src/lightclient.rs
@@ -117,3 +117,36 @@ pub mod from_inputs {
         proposer.propose_send(request).await
     }
 }
+
+pub mod with_assertions {
+    //! lightclient functions with added assertions. used for tests.
+
+    use zingolib::lightclient::LightClient;
+
+    use crate::{
+        assertions::assert_send_outputs_match_client,
+        chain_generic_tests::conduct_chain::ConductChain, lightclient::from_inputs,
+    };
+
+    /// a test-only generic version of send that includes assertions that the proposal was fulfilled
+    /// NOTICE this function bumps the chain and syncs the client
+    pub async fn propose_send_sync_check<CC>(
+        environment: &mut CC,
+        client: &LightClient,
+        raw_receivers: Vec<(&str, u64, Option<&str>)>,
+    ) where
+        CC: ConductChain,
+    {
+        let proposal = from_inputs::propose(&client, raw_receivers).await.unwrap();
+        let txids = client
+            .complete_and_broadcast_stored_proposal()
+            .await
+            .unwrap();
+
+        environment.bump_chain().await;
+
+        client.do_sync(false).await.unwrap();
+
+        assert_send_outputs_match_client(&client, &proposal, &txids).await;
+    }
+}
