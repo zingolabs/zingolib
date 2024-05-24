2f09d2f1dc2c067dfc9805ecbd45e0483f908819
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-24 16:13:40 +0000

    add comments

--- zingo-testutils/src/assertions.rs
+++ zingo-testutils/src/assertions.rs
@@ -28,16 +28,16 @@ pub async fn assert_send_outputs_match_sender<NoteId>(
         let record = records.get(&txids[i]).expect("sender must recognize txid");
         // does this record match this step?
         // may fail in uncertain ways if used on a transaction we dont have an OutgoingViewingKey for
-        let true_fee = records.calculate_transaction_fee(record).unwrap();
-        assert_eq!(true_fee, step.balance().fee_required().into_u64());
-        total_fee += true_fee;
+        let recorded_fee = records.calculate_transaction_fee(record).unwrap();
+        assert_eq!(recorded_fee, step.balance().fee_required().into_u64());
+
+        total_fee += recorded_fee;
     }
     total_fee
 }
 
-/// currently only checks if the fee matches
-/// this currently fails for any broadcast but not confirmed transaction: it seems like get_transaction_fee does not recognize pending spends
-pub async fn assert_send_outputs_match_receiver<NoteId>(
+/// currently only checks if the received total matches
+pub async fn assert_send_outputs_match_recipient<NoteId>(
     client: &LightClient,
     proposal: &Proposal<zcash_primitives::transaction::fees::zip317::FeeRule, NoteId>,
     txids: &NonEmpty<TxId>,
@@ -54,11 +54,9 @@ pub async fn assert_send_outputs_match_receiver<NoteId>(
     for (i, step) in proposal.steps().iter().enumerate() {
         let record = records.get(&txids[i]).expect("sender must recognize txid");
 
-        let mut sum_received = 0;
-        for payment in step.transaction_request().payments().values() {
-            sum_received += payment.amount.into_u64();
-        }
-
-        assert_eq!(sum_received, record.query_sum_value(OutputQuery::any()));
+        assert_eq!(
+            record.query_sum_value(OutputQuery::any()),
+            step.transaction_request().total().unwrap().into_u64()
+        );
     }
 }
