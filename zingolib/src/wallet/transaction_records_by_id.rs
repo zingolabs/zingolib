caf2897784c15e4d46b588e350dd8d5d726ff464
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-27 00:36:50 +0000

    lint hooks

--- zingolib/src/wallet/transaction_records_by_id.rs
+++ zingolib/src/wallet/transaction_records_by_id.rs
@@ -723,7 +723,7 @@ mod tests {
                     .to_owned(),
             )
             .spent(Some(sent))
-            .nullifier(Some(sapling_nullifier.clone()))
+            .nullifier(Some(*sapling_nullifier))
             .to_owned()
     }
     fn spent_orchard_note_builder(
@@ -738,7 +738,7 @@ mod tests {
                     .to_owned(),
             )
             .spent(Some(sent))
-            .nullifier(Some(orchard_nullifier.clone()))
+            .nullifier(Some(*orchard_nullifier))
             .to_owned()
     }
     #[test]
