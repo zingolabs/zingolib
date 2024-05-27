d7f22fb23c933f71e2de9a85f1b18365a24abd89
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-27 22:37:28 +0000

    cargo hooks

--- zingolib/src/wallet/transaction_records_by_id/trait_inputsource.rs
+++ zingolib/src/wallet/transaction_records_by_id/trait_inputsource.rs
@@ -296,9 +296,7 @@ impl InputSource for TransactionRecordsById {
 mod tests {
     use proptest::{prop_assert_eq, proptest};
     use zcash_client_backend::{
-        data_api::{InputSource as _},
-        wallet::{ReceivedNote},
-        ShieldedProtocol,
+        data_api::InputSource as _, wallet::ReceivedNote, ShieldedProtocol,
     };
     use zcash_primitives::{
         consensus::BlockHeight, legacy::TransparentAddress,
@@ -314,7 +312,7 @@ mod tests {
         transaction_record::mocks::{
             nine_note_transaction_record, nine_note_transaction_record_default,
         },
-        transaction_records_by_id::{TransactionRecordsById},
+        transaction_records_by_id::TransactionRecordsById,
     };
 
     #[test]
