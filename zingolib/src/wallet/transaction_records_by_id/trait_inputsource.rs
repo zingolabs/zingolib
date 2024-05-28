c7dd3e63eef50720d4bf5d0a709923c965361204
fluidvanadium <fluidvanadium@zingolabs.org>
2024-05-28 04:58:09 +0000

    cargo doc attempt 2

--- zingolib/src/wallet/transaction_records_by_id/trait_inputsource.rs
+++ zingolib/src/wallet/transaction_records_by_id/trait_inputsource.rs
@@ -103,7 +103,7 @@ impl InputSource for TransactionRecordsById {
     /// the trait method below is used as a TxMapAndMaybeTrees trait method by propose_transaction.
     /// this function is used inside a loop that calculates a fee and balances change
     /// this algorithm influences strategy for user fee minimization
-    /// see [create_send_proposal]
+    /// see [LightClient::create_send_proposal]
     /// TRAIT DOCUMENTATION
     /// Returns a list of spendable notes sufficient to cover the specified target value, if
     /// possible. Only spendable notes corresponding to the specified shielded protocol will
