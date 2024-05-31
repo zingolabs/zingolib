3767bcb8bc297c2385129b4bf41748943f7d3672
zancas <zancas@zingolabs.org>
2024-05-30 22:11:27 -0600

    make assertions explicitly log and panic on calculate_transaction_fee error

--- zingo-testutils/src/assertions.rs
+++ zingo-testutils/src/assertions.rs
@@ -28,7 +28,13 @@ pub async fn assert_send_outputs_match_sender<NoteId>(
         let record = records.get(&txids[i]).expect("sender must recognize txid");
         // does this record match this step?
         // may fail in uncertain ways if used on a transaction we dont have an OutgoingViewingKey for
-        let recorded_fee = records.calculate_transaction_fee(record).unwrap();
+        let recorded_fee = match records.calculate_transaction_fee(record) {
+            Ok(calculate_fee) => calculate_fee,
+            Err(e) => {
+                dbg!(format!("{}", e.to_string()));
+                panic!();
+            }
+        };
         assert_eq!(recorded_fee, step.balance().fee_required().into_u64());
 
         total_fee += recorded_fee;
