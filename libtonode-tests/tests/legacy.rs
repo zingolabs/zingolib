9daece1517d50c852ce7b1a6e65e2ee7cdccfeb5
fluidvanadium <fluidvanadium@zingolabs.org>
2024-06-12 14:52:05 +0000

    removed the legacy test

--- libtonode-tests/tests/legacy.rs
+++ libtonode-tests/tests/legacy.rs
@@ -43,10 +43,6 @@ use zingolib::{
     },
 };
 
-fn extract_value_as_u64(input: &JsonValue) -> u64 {
-    let note = &input["value"].as_fixed_point_u64(0).unwrap();
-    *note
-}
 fn check_expected_balance_with_fvks(
     fvks: &Vec<&Fvk>,
     balance: PoolBalances,
@@ -2237,120 +2233,6 @@ mod slow {
         faucet.do_rescan().await.unwrap();
         let post_rescan_transactions = faucet.do_list_transactions().await;
         assert_eq!(transactions, post_rescan_transactions);
-    }
-    #[ignore]
-    #[tokio::test]
-    async fn note_selection_order() {
-        // In order to fund a transaction multiple notes may be selected and consumed.
-        // The algorithm selects the smallest covering note(s).
-        // In addition to testing the order in which notes are selected this test:
-        //   * sends to a sapling address
-        //   * sends back to the original sender's UA
-        let (regtest_manager, _cph, faucet, recipient) =
-            scenarios::faucet_recipient_default().await;
-        zingo_testutils::increase_height_and_wait_for_client(&regtest_manager, &faucet, 5)
-            .await
-            .unwrap();
-
-        let client_2_saplingaddress = get_base_address_macro!(recipient, "sapling");
-        // Send three transfers in increasing 10_000 zat increments
-        // These are sent from the coinbase funded client which will
-        // subsequently receive funding via it's orchard-packed UA.
-        let memos = ["1", "2", "3"];
-        from_inputs::quick_send(
-            &faucet,
-            (1..=3)
-                .map(|n| {
-                    (
-                        client_2_saplingaddress.as_str(),
-                        n * 10_000,
-                        Some(memos[(n - 1) as usize]),
-                    )
-                })
-                .collect(),
-        )
-        .await
-        .unwrap();
-
-        zingo_testutils::increase_height_and_wait_for_client(&regtest_manager, &recipient, 5)
-            .await
-            .unwrap();
-        // We know that the largest single note that 2 received from 1 was 30_000, for 2 to send
-        // 30_000 back to 1 it will have to collect funds from two notes to pay the full 30_000
-        // plus the transaction fee.
-        from_inputs::quick_send(
-            &recipient,
-            vec![(
-                &get_base_address_macro!(faucet, "unified"),
-                30_000,
-                Some("Sending back, should have 2 inputs"),
-            )],
-        )
-        .await
-        .unwrap();
-        let client_2_notes = recipient.do_list_notes(false).await;
-        // The 30_000 zat note to cover the value, plus another for the tx-fee.
-        let first_value = client_2_notes["pending_sapling_notes"][0]["value"]
-            .as_fixed_point_u64(0)
-            .unwrap();
-        let second_value = client_2_notes["pending_sapling_notes"][1]["value"]
-            .as_fixed_point_u64(0)
-            .unwrap();
-        assert!(
-            first_value == 30_000u64 && second_value == 20_000u64
-                || first_value == 20_000u64 && second_value == 30_000u64
-        );
-        //);
-        // Because the above tx fee won't consume a full note, change will be sent back to 2.
-        // This implies that client_2 will have a total of 2 unspent notes:
-        //  * one (sapling) from client_1 sent above (and never used) + 1 (orchard) as change to itself
-        assert_eq!(client_2_notes["unspent_sapling_notes"].len(), 1);
-        assert_eq!(client_2_notes["unspent_orchard_notes"].len(), 1);
-        let change_note = client_2_notes["unspent_orchard_notes"]
-            .members()
-            .filter(|note| note["is_change"].as_bool().unwrap())
-            .collect::<Vec<_>>()[0];
-        // Because 2000 is the size of the second largest note.
-        assert_eq!(change_note["value"], 20000 - u64::from(MINIMUM_FEE));
-        let non_change_note_values = client_2_notes["unspent_sapling_notes"]
-            .members()
-            .filter(|note| !note["is_change"].as_bool().unwrap())
-            .map(extract_value_as_u64)
-            .collect::<Vec<_>>();
-        // client_2 got a total of 3000+2000+1000
-        // It sent 3000 to the client_1, and also
-        // paid the default transaction fee.
-        // In non change notes it has 1000.
-        // There is an outstanding 2000 that is marked as change.
-        // After sync the unspent_sapling_notes should go to 3000.
-        assert_eq!(non_change_note_values.iter().sum::<u64>(), 10000u64);
-
-        zingo_testutils::increase_height_and_wait_for_client(&regtest_manager, &recipient, 5)
-            .await
-            .unwrap();
-        let client_2_post_transaction_notes = recipient.do_list_notes(false).await;
-        assert_eq!(
-            client_2_post_transaction_notes["pending_sapling_notes"].len(),
-            0
-        );
-        assert_eq!(
-            client_2_post_transaction_notes["unspent_sapling_notes"].len(),
-            1
-        );
-        assert_eq!(
-            client_2_post_transaction_notes["unspent_orchard_notes"].len(),
-            1
-        );
-        assert_eq!(
-            client_2_post_transaction_notes["unspent_sapling_notes"]
-                .members()
-                .chain(client_2_post_transaction_notes["unspent_orchard_notes"].members())
-                .map(extract_value_as_u64)
-                .sum::<u64>(),
-            20000u64 // 10000 received and unused + (20000 - 10000 txfee)
-        );
-
-        // More explicit than ignoring the unused variable, we only care about this in order to drop it
     }
     #[tokio::test]
     async fn multiple_outgoing_metadatas_work_right_on_restore() {
