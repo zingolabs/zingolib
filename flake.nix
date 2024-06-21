c74ddc35537b347dcf12ea9936c6c39d3d8c33c8
Nate Wilcox <nejucomo+dev@gmail.com>
2024-06-20 18:49:20 -0700

    Clean up flake. `nix build` outputs only `zingo-cli` currently. `nix flake check` fails because `cargo audit` fails. `nix develop` installs all "compiling from source" prerequisites.

--- flake.nix
+++ flake.nix
@@ -26,6 +26,13 @@
   outputs = { self, nixpkgs, crane, fenix, flake-utils, advisory-db, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
+        pname = "zingolib-workspace";
+        version =
+          let
+            inherit (builtins) readFile fromTOML;
+          in
+            (fromTOML (readFile "${src}/zingolib/Cargo.toml")).package.version;
+
         pkgs = nixpkgs.legacyPackages.${system};
 
         inherit (pkgs) lib;
@@ -37,11 +44,42 @@
         # For now we cast a wide net just to get the build to work.
         src = ./.;
 
+        # See: https://github.com/zcash/librustzcash/issues/1420
+        # -and: https://github.com/zingolabs/zingolib/pull/1214#issuecomment-2168047527
+        zcbi1420-workaround = pkgs.writeScript "zcbi1420-workaround.sh" ''
+          #! ${pkgs.bash}/bin/bash
+          set -efuo pipefail
+
+          cargo_config='.cargo-home/config.toml'
+
+          function get_nix_store_cargo_prereqs {
+            grep '^directory' "$cargo_config" \
+              | sed 's/^directory = "//; s/"$//'
+          }
+
+          mkdir ./rw_dependencies
+          for store_src in $(get_nix_store_cargo_prereqs)
+          do
+            rw_src_name="$(echo "$store_src" | tr '/' '_')"
+            rw_src="./rw_dependencies/$rw_src_name"
+            cp -r --dereference "$store_src" "$rw_src"
+            chmod -R u+w "$rw_src"
+            sed -i "s|$store_src|$rw_src|" "$cargo_config"
+          done
+
+          # For diagnostics (not necessary for build):
+          echo "+- Contents of $cargo_config:"
+          echo '|'
+          sed 's/^/| /' "$cargo_config"
+        '';
+
         # Common arguments can be set here to avoid repeating them later
         # Note: we include system dependencies commonly that only some (transitive) crates require.
         commonArgs = {
-          inherit src;
+          inherit pname version src;
+
           strictDeps = true;
+          doCheck = false;
 
           # All packages in `nativeBuildInputs` are required at build time but _must not_ be required at runtime:
           nativeBuildInputs = with pkgs; [
@@ -51,65 +89,49 @@
             # We include git because `zingolib` requires it in its `build.rs` script via the `build_utils` crate.
             git
 
-            # We include `protobuf` because `darkside-tests` requires it.
-            protobuf
+            # Note that the default package derivation runs nextest, as well as some flake check targets
+            cargo-nextest
           ];
 
           buildInputs = [
             # We include `openssl` lib in all crates, even though it is only necessary for the `openssl-sys` crate.
             pkgs.openssl
-          ] ++ lib.optionals pkgs.stdenv.isDarwin [
-            # Additional darwin specific inputs can be set here
-            pkgs.libiconv
           ];
 
           # Additional environment variables can be set directly
           # MY_CUSTOM_VAR = "some value";
-
-          # See: https://github.com/zcash/librustzcash/issues/1420
-          # -and: https://github.com/zingolabs/zingolib/pull/1214#issuecomment-2168047527
-          buildPhaseCargoCommand = pkgs.writeScript "zcbi1420-workaround.sh" ''
-            #! ${pkgs.bash}/bin/bash
-            set -efuxo pipefail
-
-            cargo_config='.cargo-home/config.toml'
-
-            function get_nix_store_cargo_prereqs {
-              grep '^directory' "$cargo_config" \
-                | sed 's/^directory = "//; s/"$//'
-            }
-
-            mkdir ./rw_dependencies
-            for store_src in $(get_nix_store_cargo_prereqs)
-            do
-              rw_src_name="$(echo "$store_src" | tr '/' '_')"
-              rw_src="./rw_dependencies/$rw_src_name"
-              cp -r --dereference "$store_src" "$rw_src"
-              chmod -R u+w "$rw_src"
-              sed -i "s|$store_src|$rw_src|" "$cargo_config"
-            done
-
-            # For diagnostics (not necessary for build):
-            cat "$cargo_config"
-
-            set +x
-          '';
         };
 
-        craneLibLLvmTools = craneLib.overrideToolchain
-          (fenix.packages.${system}.complete.withComponents [
-            "cargo"
-            "llvm-tools"
-            "rustc"
-          ]);
-
-        # Build *just* the cargo dependencies (of the entire workspace),
-        # so we can reuse all of that work (e.g. via cachix) when running in CI
-        # It is *highly* recommended to use something like cargo-hakari to avoid
-        # cache misses when building individual top-level-crates
         cargoArtifacts = craneLib.buildDepsOnly commonArgs;
       in
       {
+        packages = {
+          default = craneLib.buildPackage (commonArgs // {
+            inherit cargoArtifacts;
+
+            nativeBuildInputs = commonArgs.nativeBuildInputs ++ [
+              pkgs.protobuf
+            ];
+          });
+        };
+
+        apps = {};
+
+        devShells.default = craneLib.devShell {
+          inherit cargoArtifacts;
+
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            pkgs.protobuf
+          ];
+        };
+
         checks = {
           # Run clippy (and deny all warnings) on the workspace source,
           # again, reusing the dependency artifacts from above.
@@ -128,17 +150,17 @@
 
           # Check formatting
           my-workspace-fmt = craneLib.cargoFmt {
-            inherit src;
+            inherit cargoArtifacts pname version src;
           };
 
           # Audit dependencies
-          my-workspace-audit = craneLib.cargoAudit {
-            inherit src advisory-db;
-          };
+          my-workspace-audit = craneLib.cargoAudit (commonArgs // {
+            inherit advisory-db;
+          });
 
           # Audit licenses
           my-workspace-deny = craneLib.cargoDeny {
-            inherit src;
+            inherit cargoArtifacts pname version src;
           };
 
           # Run tests with cargo-nextest
@@ -146,50 +168,13 @@
           # if you do not want the tests to run twice
           my-workspace-nextest = craneLib.cargoNextest (commonArgs // {
             inherit cargoArtifacts;
+
+            doCheck = true;
+            cargoTestCommand = "cargo nextest run -E'not (package(libtonode-tests) + package(darkside-tests))'";
+
             partitions = 1;
             partitionType = "count";
           });
-
-          # Ensure that cargo-hakari is up to date
-          my-workspace-hakari = craneLib.mkCargoDerivation {
-            inherit src;
-            pname = "my-workspace-hakari";
-            cargoArtifacts = null;
-            doInstallCargoArtifacts = false;
-
-            buildPhaseCargoCommand = ''
-              cargo hakari generate --diff  # workspace-hack Cargo.toml is up-to-date
-              cargo hakari manage-deps --dry-run  # all workspace crates depend on workspace-hack
-              cargo hakari verify
-            '';
-
-            nativeBuildInputs = [
-              pkgs.cargo-hakari
-            ];
-          };
-        };
-
-        packages = {
-          default = craneLib.buildPackage (commonArgs // { inherit cargoArtifacts; });
-
-          my-workspace-llvm-coverage = craneLibLLvmTools.cargoLlvmCov (commonArgs // {
-            inherit cargoArtifacts;
-          });
-        };
-
-        apps = {};
-
-        devShells.default = craneLib.devShell {
-          # Inherit inputs from checks.
-          checks = self.checks.${system};
-
-          # Additional dev-shell environment variables can be set directly
-          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
-
-          # Extra inputs can be added here; cargo and rustc are provided by default.
-          packages = [
-            pkgs.cargo-hakari
-          ];
         };
       });
 }
