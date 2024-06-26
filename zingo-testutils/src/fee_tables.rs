e01863d693465d54c1587c98c884dd27715346a6
fluidvanadium <fluidvanadium@zingolabs.org>
2024-06-26 03:27:41 +0000

    fee calculator has half a brain

--- zingo-testutils/src/fee_tables.rs
+++ zingo-testutils/src/fee_tables.rs
@@ -1,7 +1,29 @@
-use zcash_client_backend::{PoolType, ShieldedProtocol};
+use orchard::tree::MerkleHashOrchard;
+use zcash_client_backend::PoolType;
+use zcash_client_backend::PoolType::Shielded;
+use zcash_client_backend::PoolType::Transparent;
+use zcash_client_backend::ShieldedProtocol;
+use zcash_client_backend::ShieldedProtocol::Orchard;
+use zcash_client_backend::ShieldedProtocol::Sapling;
+use zcash_primitives::transaction::fees::zip317::GRACE_ACTIONS;
+use zcash_primitives::transaction::fees::zip317::MARGINAL_FEE;
 
-/// calculates the expected fee for a transaction with no change
 /// wip
 pub fn one_to_one_no_change(source_pool: ShieldedProtocol, target_pool: PoolType) -> u64 {
-    10_000
+    let mut transparent_actions = 0;
+    let mut sapling_actions = 0;
+    let mut orchard_actions = 0;
+    match target_pool {
+        Transparent => transparent_actions += 1,
+        Shielded(Sapling) => sapling_actions += 1,
+        Shielded(Orchard) => orchard_actions += 1,
+    }
+    let whattype = MARGINAL_FEE
+        * std::cmp::max(
+            transparent_actions + sapling_actions + orchard_actions,
+            GRACE_ACTIONS,
+        );
+    whattype
+        .expect("actions expected to be in numberical range")
+        .into_u64()
 }
