ed86342a5ef258f5fe13d5b6517f6a1ad9939ed7
fluidvanadium <fluidvanadium@zingolabs.org>
2024-06-26 03:28:41 +0000

    another half. i half expect most of the tests to work now

--- zingo-testutils/src/fee_tables.rs
+++ zingo-testutils/src/fee_tables.rs
@@ -9,10 +9,14 @@ use zcash_primitives::transaction::fees::zip317::GRACE_ACTIONS;
 use zcash_primitives::transaction::fees::zip317::MARGINAL_FEE;
 
 /// wip
-pub fn one_to_one_no_change(source_pool: ShieldedProtocol, target_pool: PoolType) -> u64 {
+pub fn one_to_one_no_change(source_protocol: ShieldedProtocol, target_pool: PoolType) -> u64 {
     let mut transparent_actions = 0;
     let mut sapling_actions = 0;
     let mut orchard_actions = 0;
+    match source_protocol {
+        Sapling => sapling_actions += 1,
+        Orchard => orchard_actions += 1,
+    }
     match target_pool {
         Transparent => transparent_actions += 1,
         Shielded(Sapling) => sapling_actions += 1,
