7b35a0da15b4b7a26efba2139cf77e846f387de7
fluidvanadium <fluidvanadium@zingolabs.org>
2024-07-22 22:01:16 +0000

    fixed trait_stub signature

--- zingolib/src/wallet/tx_map_and_maybe_trees/trait_stub_inputsource.rs
+++ zingolib/src/wallet/tx_map_and_maybe_trees/trait_stub_inputsource.rs
@@ -52,14 +52,14 @@ impl InputSource for TxMapAndMaybeTrees {
         unimplemented!()
     }
 
-    fn get_unspent_transparent_outputs(
+    fn get_spendable_transparent_outputs(
         &self,
         address: &zcash_primitives::legacy::TransparentAddress,
-        max_height: zcash_primitives::consensus::BlockHeight,
-        exclude: &[zcash_primitives::transaction::components::OutPoint],
+        target_height: zcash_primitives::consensus::BlockHeight,
+        min_confirmations: u32,
     ) -> Result<Vec<zcash_client_backend::wallet::WalletTransparentOutput>, Self::Error> {
         self.transaction_records_by_id
-            .get_unspent_transparent_outputs(address, max_height, exclude)
+            .get_spendable_transparent_outputs(address, target_height, min_confirmations)
             .map_err(TxMapAndMaybeTreesTraitError::InputSource)
     }
 }
