a5dfbe361d9dc1afcd15d087cf16a2dd36d6d7a7
fluidvanadium <fluidvanadium@zingolabs.org>
2024-07-22 23:21:42 +0000

    transaction_request_from_receivers can Err MemoDisallowed

--- zingolib/src/data.rs
+++ zingolib/src/data.rs
@@ -4,6 +4,7 @@ pub mod witness_trees;
 
 /// transforming data related to the destination of a send.
 pub mod receivers {
+    use zcash_address::ZcashAddress;
     use zcash_client_backend::zip321::Payment;
     use zcash_client_backend::zip321::TransactionRequest;
     use zcash_client_backend::zip321::Zip321Error;
@@ -17,14 +18,14 @@ pub mod receivers {
     /// The superficial representation of the the consumer's intended receiver
     #[derive(Clone, Debug, PartialEq)]
     pub struct Receiver {
-        pub(crate) recipient_address: address::Address,
+        pub(crate) recipient_address: ZcashAddress,
         pub(crate) amount: NonNegativeAmount,
         pub(crate) memo: Option<MemoBytes>,
     }
     impl Receiver {
         /// Create a new Receiver
         pub fn new(
-            recipient_address: address::Address,
+            recipient_address: ZcashAddress,
             amount: NonNegativeAmount,
             memo: Option<MemoBytes>,
         ) -> Self {
@@ -35,28 +36,34 @@ pub mod receivers {
             }
         }
     }
-    impl From<Receiver> for Payment {
-        fn from(receiver: Receiver) -> Self {
-            Self {
-                recipient_address: receiver.recipient_address,
-                amount: receiver.amount,
-                memo: receiver.memo,
-                label: None,
-                message: None,
-                other_params: vec![],
-            }
-        }
+
+    pub enum ReceiverParseError {
+        AddressParse,
+        MemoDisallowed,
+        Request(Zip321Error),
     }
 
     /// Creates a [`zcash_client_backend::zip321::TransactionRequest`] from receivers.
     pub fn transaction_request_from_receivers(
         receivers: Receivers,
-    ) -> Result<TransactionRequest, Zip321Error> {
+    ) -> Result<TransactionRequest, ReceiverParseError> {
         let payments = receivers
             .into_iter()
-            .map(|receiver| receiver.into())
-            .collect();
+            .map(|receiver| {
+                Payment::new(
+                    receiver.recipient_address,
+                    receiver.amount,
+                    receiver.memo,
+                    None,
+                    None,
+                    vec![],
+                )
+                .ok_or(())
+                // .ok_or(ReceiverParseError::MemoDisallowed)
+            })
+            .collect::<Result<Vec<Payment>, ()>>()
+            .map_err(|_| ReceiverParseError::MemoDisallowed)?;
 
-        TransactionRequest::new(payments)
+        TransactionRequest::new(payments).map_err(ReceiverParseError::Request)
     }
 }
