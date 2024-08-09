e446933659efb052156b8068a4da320029951089
fluidvanadium <fluidvanadium@zingolabs.org>
2024-08-09 01:07:34 +0000

    merged config test dep

--- libtonode-tests/tests/sync.rs
+++ libtonode-tests/tests/sync.rs
@@ -1,15 +1,23 @@
 use tempfile::TempDir;
-use zingo_testvectors::seeds::HOSPITAL_MUSEUM_SEED;
-use zingoconfig::{construct_lightwalletd_uri, load_clientconfig, DEFAULT_LIGHTWALLETD_SERVER};
-use zingolib::{lightclient::LightClient, wallet::WalletBase};
+use zingolib::{
+    config::{construct_lightwalletd_uri, load_clientconfig, DEFAULT_LIGHTWALLETD_SERVER},
+    lightclient::LightClient,
+    testvectors::seeds::HOSPITAL_MUSEUM_SEED,
+    wallet::WalletBase,
+};
 
 #[tokio::test]
 async fn sync_test() {
     let uri = construct_lightwalletd_uri(Some(DEFAULT_LIGHTWALLETD_SERVER.to_string()));
     let temp_dir = TempDir::new().unwrap();
     let temp_path = temp_dir.path().to_path_buf();
-    let config =
-        load_clientconfig(uri, Some(temp_path), zingoconfig::ChainType::Mainnet, true).unwrap();
+    let config = load_clientconfig(
+        uri,
+        Some(temp_path),
+        zingolib::config::ChainType::Mainnet,
+        true,
+    )
+    .unwrap();
     let lightclient = LightClient::create_from_wallet_base_async(
         WalletBase::from_string(HOSPITAL_MUSEUM_SEED.to_string()),
         &config,
