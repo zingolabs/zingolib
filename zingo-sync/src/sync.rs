9b1d9706908e01196cc80eb88e365d50241f4bf8
Oscar Pepper <oscarpepper@zingolabs.org>
2024-08-14 12:30:40 +0100

    prep for Scanner struct

--- zingo-sync/src/sync.rs
+++ zingo-sync/src/sync.rs
@@ -9,6 +9,7 @@ use crate::keys::ScanningKeys;
 use crate::primitives::SyncState;
 use crate::scan::{scan, ScanData};
 
+use tokio::task::JoinHandle;
 use zcash_client_backend::{
     data_api::scanning::{ScanPriority, ScanRange},
     proto::service::compact_tx_streamer_client::CompactTxStreamerClient,
@@ -18,6 +19,7 @@ use futures::future::try_join_all;
 use tokio::sync::mpsc::{unbounded_channel, UnboundedSender};
 use zcash_primitives::consensus::{BlockHeight, NetworkUpgrade, Parameters};
 
+const SCAN_WORKERS: usize = 2;
 const BATCH_SIZE: u32 = 10;
 // const BATCH_SIZE: u32 = 1_000;
 
@@ -45,74 +47,32 @@ where
         .await
         .unwrap();
 
-    let mut scan_handles = Vec::new();
+    // let scan_handle_a = run_scan_task(
+    //     fetch_request_sender.clone(),
+    //     parameters.clone(),
+    //     wallet,
+    //     &sync_state,
+    // )
+    // .await;
 
-    let scan_handle = if let Some(scan_range) = prepare_next_scan_range(&sync_state).await {
-        let previous_wallet_block = wallet
-            .get_wallet_block(scan_range.block_range().start - 1)
-            .ok();
-        let fetch_request_sender = fetch_request_sender.clone();
-        let parameters = parameters.clone();
-        let ufvks = wallet.get_unified_full_viewing_keys().unwrap();
-        let scanning_keys = ScanningKeys::from_account_ufvks(ufvks);
-        Some(tokio::spawn(async move {
-            scan(
-                fetch_request_sender,
-                &parameters,
-                scan_range.clone(),
-                &scanning_keys,
-                previous_wallet_block,
-            )
-            .await
-        }))
-    } else {
-        None
-    };
-    scan_handles.push(scan_handle);
+    // if let Some(handle) = scan_handle_a {
+    //     let scan_data = handle.await.unwrap().unwrap();
 
-    let scan_handle = if let Some(scan_range) = prepare_next_scan_range(&sync_state).await {
-        let previous_wallet_block = wallet
-            .get_wallet_block(scan_range.block_range().start - 1)
-            .ok();
-        let fetch_request_sender = fetch_request_sender.clone();
-        let parameters = parameters.clone();
-        let ufvks = wallet.get_unified_full_viewing_keys().unwrap();
-        let scanning_keys = ScanningKeys::from_account_ufvks(ufvks);
-        Some(tokio::spawn(async move {
-            scan(
-                fetch_request_sender,
-                &parameters,
-                scan_range.clone(),
-                &scanning_keys,
-                previous_wallet_block,
-            )
-            .await
-        }))
-    } else {
-        None
-    };
-    scan_handles.push(scan_handle);
+    //     let ScanData {
+    //         nullifiers,
+    //         wallet_blocks,
+    //         shard_tree_data,
+    //     } = scan_data;
 
-    for scan_handle in scan_handles {
-        if let Some(handle) = scan_handle {
-            let scan_data = handle.await.unwrap().unwrap();
+    //     // TODO: if scan priority is historic, retain only relevent blocks and nullifiers as we have all information and requires a lot of memory / storage
+    //     // must still retain top 100 blocks for re-org purposes
+    //     wallet.append_wallet_blocks(wallet_blocks).unwrap();
+    //     wallet.append_nullifiers(nullifiers).unwrap();
+    //     wallet.update_shard_trees(shard_tree_data).unwrap();
+    //     // TODO: add trait to save wallet data to persistance for in-memory wallets
 
-            let ScanData {
-                nullifiers,
-                wallet_blocks,
-                shard_tree_data,
-            } = scan_data;
-
-            // TODO: if scan priority is historic, retain only relevent blocks and nullifiers as we have all information and requires a lot of memory / storage
-            // must still retain top 100 blocks for re-org purposes
-            wallet.append_wallet_blocks(wallet_blocks).unwrap();
-            wallet.append_nullifiers(nullifiers).unwrap();
-            wallet.update_shard_trees(shard_tree_data).unwrap();
-            // TODO: add trait to save wallet data to persistance for in-memory wallets
-
-            // TODO: set scanned range to `scanned`
-        }
-    }
+    //     // TODO: set scanned range to `scanned`
+    // }
 
     drop(fetch_request_sender);
     try_join_all(vec![fetcher_handle]).await.unwrap();
@@ -196,5 +156,36 @@ async fn prepare_next_scan_range(sync_state: &SyncState) -> Option<ScanRange> {
         scan_ranges.splice(index..=index, vec![selected_range_ignored]);
 
         Some(selected_scan_range.clone())
+    }
+}
+
+async fn run_scan_task<P, W>(
+    fetch_request_sender: UnboundedSender<FetchRequest>,
+    parameters: P,
+    wallet: &mut W,
+    sync_state: &SyncState, // temp
+) -> Result<Option<JoinHandle<Result<ScanData, ()>>>, ()>
+where
+    P: Parameters + Sync + Send + 'static,
+    W: SyncWallet + SyncBlocks + SyncNullifiers + SyncShardTrees,
+{
+    if let Some(scan_range) = prepare_next_scan_range(&sync_state).await {
+        let previous_wallet_block = wallet
+            .get_wallet_block(scan_range.block_range().start - 1)
+            .ok();
+        let ufvks = wallet.get_unified_full_viewing_keys().unwrap();
+        let scanning_keys = ScanningKeys::from_account_ufvks(ufvks);
+        Ok(Some(tokio::spawn(async move {
+            scan(
+                fetch_request_sender,
+                &parameters,
+                scan_range.clone(),
+                &scanning_keys,
+                previous_wallet_block,
+            )
+            .await
+        })))
+    } else {
+        Ok(None)
     }
 }
