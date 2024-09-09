464aa8c399cd3a101483cec09392380483017e31
fluidvanadium <fluidvanadium@zingolabs.org>
2024-09-09 21:21:36 +0000

    added Client executor

--- darkside-tests/src/utils.rs
+++ darkside-tests/src/utils.rs
@@ -75,7 +75,11 @@ impl DarksideConnector {
             let mut http_connector = HttpConnector::new();
             http_connector.enforce_http(false);
             let connector = tower::ServiceBuilder::new().service(http_connector);
-            let client = Box::new(Client::builder().http2_only(true).build(connector));
+            let client = Box::new(
+                Client::builder(hyper_util::rt::TokioExecutor::new())
+                    .http2_only(true)
+                    .build(connector),
+            );
             let uri = uri.clone();
             let svc = tower::ServiceBuilder::new()
                 //Here, we take all the pieces of our uri, and add in the path from the Requests's uri
