cbc64d93ff5691f285684a2ed8d087cecdbd291e
fluidvanadium <fluidvanadium@zingolabs.org>
2024-09-10 23:36:18 +0000

    changed cache to raw

--- zingolib/src/wallet/tx_map/spending_data.rs
+++ zingolib/src/wallet/tx_map/spending_data.rs
@@ -9,14 +9,14 @@ use crate::data::witness_trees::WitnessTrees;
 /// the subsection of TxMap that only applies to spending wallets
 pub(crate) struct SpendingData {
     witness_trees: WitnessTrees,
-    cached_transactions: HashMap<TxId, Transaction>,
+    cached_raw_transactions: HashMap<TxId, Vec<u8>>,
 }
 
 impl SpendingData {
     pub fn new(witness_trees: WitnessTrees) -> Self {
         SpendingData {
             witness_trees,
-            cached_transactions: HashMap::new(),
+            cached_raw_transactions: HashMap::new(),
         }
     }
     pub fn witness_trees(&self) -> &WitnessTrees {
@@ -25,10 +25,10 @@ impl SpendingData {
     pub fn witness_trees_mut(&mut self) -> &mut WitnessTrees {
         &mut self.witness_trees
     }
-    pub fn cached_transactions(&self) -> &HashMap<TxId, Transaction> {
-        &self.cached_transactions
+    pub fn cached_transactions(&self) -> &HashMap<TxId, Vec<u8>> {
+        &self.cached_raw_transactions
     }
-    pub fn cached_transactions_mut(&mut self) -> &mut HashMap<TxId, Transaction> {
-        &mut self.cached_transactions
+    pub fn cached_transactions_mut(&mut self) -> &mut HashMap<TxId, Vec<u8>> {
+        &mut self.cached_raw_transactions
     }
 }
