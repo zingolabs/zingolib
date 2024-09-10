84513904a6a6b3358166a5167da685b86788d0a6
fluidvanadium <fluidvanadium@zingolabs.org>
2024-09-10 22:52:44 +0000

    added cached_transactions HashMap

--- zingolib/src/wallet/tx_map/spending_data.rs
+++ zingolib/src/wallet/tx_map/spending_data.rs
@@ -1,15 +1,23 @@
 //! the subsection of TxMap that only applies to spending wallets
 
+use std::collections::HashMap;
+
+use zcash_primitives::transaction::{Transaction, TxId};
+
 use crate::data::witness_trees::WitnessTrees;
 
 /// the subsection of TxMap that only applies to spending wallets
 pub(crate) struct SpendingData {
     witness_trees: WitnessTrees,
+    cached_transactions: HashMap<TxId, Transaction>,
 }
 
 impl SpendingData {
     pub fn new(witness_trees: WitnessTrees) -> Self {
-        SpendingData { witness_trees }
+        SpendingData {
+            witness_trees,
+            cached_transactions: HashMap::new(),
+        }
     }
     pub fn witness_trees(&self) -> &WitnessTrees {
         &self.witness_trees
