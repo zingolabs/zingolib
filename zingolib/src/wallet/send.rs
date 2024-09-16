2189eb517d983d38e0743203192827487fa3f8ad
Oscar Pepper <oscarpepper@zingolabs.org>
2024-09-16 12:06:34 +0100

    fix address index in usk_to_tkey

--- zingolib/src/wallet/send.rs
+++ zingolib/src/wallet/send.rs
@@ -164,7 +164,7 @@ impl LightWallet {
         ) -> secp256k1::SecretKey {
             hdwallet::ExtendedPrivKey::deserialize(&unified_spend_key.transparent().to_bytes())
                 .expect("This a hack to do a type conversion, and will not fail")
-                .derive_private_key(t_metadata.address_index().into())
+                .derive_private_key(t_metadata.address_index().index().into())
                 // This is unwrapped in librustzcash, so I'm not too worried about it
                 .expect("private key derivation failed")
                 .private_key
