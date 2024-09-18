5b2b4e88d9c3f3c6533233605e54523225e3bfaf
Oscar Pepper <oscarpepper@zingolabs.org>
2024-09-18 12:51:42 +0100

    remove commented code

--- zingolib/src/wallet/send.rs
+++ zingolib/src/wallet/send.rs
@@ -98,7 +98,6 @@ pub enum BuildTransactionError {
     #[error("Could not load sapling_params: {0:?}")]
     SaplingParams(String),
     #[error("Could not find UnifiedSpendKey: {0:?}")]
-    // UnifiedSpendKey(std::io::Error),
     UnifiedSpendKey(zcash_keys::keys::DerivationError),
     #[error("Can't Calculate {0:?}")]
     Calculation(
@@ -138,8 +137,6 @@ impl LightWallet {
                 .map_err(BuildTransactionError::SaplingParams)?;
         let sapling_prover =
             zcash_proofs::prover::LocalTxProver::from_bytes(&sapling_spend, &sapling_output);
-        // let unified_spend_key = UnifiedSpendingKey::try_from(self.wallet_capability().as_ref())
-        //     .map_err(BuildTransactionError::UnifiedSpendKey)?;
         let unified_spend_key = UnifiedSpendingKey::from_seed(
             &self.transaction_context.config.chain,
             self.mnemonic().unwrap().0.to_seed("").as_slice(),
