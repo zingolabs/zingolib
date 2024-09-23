2d2cfa7ad199ed1c21516a70f4908988541943ff
fluidvanadium <fluidvanadium@zingolabs.org>
2024-09-23 17:30:32 +0000

    cargo clippy --fix --tests --all-features

--- zingolib/src/wallet/send.rs
+++ zingolib/src/wallet/send.rs
@@ -137,6 +137,7 @@ impl LightWallet {
                 .map_err(BuildTransactionError::SaplingParams)?;
         let sapling_prover =
             zcash_proofs::prover::LocalTxProver::from_bytes(&sapling_spend, &sapling_output);
+
         let unified_spend_key = UnifiedSpendingKey::try_from(self.wallet_capability().as_ref())
             .map_err(BuildTransactionError::UnifiedSpendKey)?;
 
