fec775604e0b4d3f6854a5ee45efa6be5d5734a7
Oscar Pepper <oscarpepper@zingolabs.org>
2024-10-15 15:05:20 +0100

    replace usk re-derivation from seed with keystore usk

--- zingolib/src/wallet/send.rs
+++ zingolib/src/wallet/send.rs
@@ -2,7 +2,6 @@
 
 use log::error;
 use zcash_client_backend::proposal::Proposal;
-use zcash_keys::keys::UnifiedSpendingKey;
 
 use std::cmp;
 use std::ops::DerefMut as _;
@@ -97,7 +96,7 @@ pub enum BuildTransactionError {
     #[error("Could not load sapling_params: {0:?}")]
     SaplingParams(String),
     #[error("Could not find UnifiedSpendKey: {0:?}")]
-    UnifiedSpendKey(zcash_keys::keys::DerivationError),
+    UnifiedSpendKey(#[from] crate::wallet::error::KeyError),
     #[error("Can't Calculate {0:?}")]
     Calculation(
         #[from]
@@ -136,12 +135,6 @@ impl LightWallet {
                 .map_err(BuildTransactionError::SaplingParams)?;
         let sapling_prover =
             zcash_proofs::prover::LocalTxProver::from_bytes(&sapling_spend, &sapling_output);
-        let unified_spend_key = UnifiedSpendingKey::from_seed(
-            &self.transaction_context.config.chain,
-            self.mnemonic().unwrap().0.to_seed("").as_slice(),
-            zip32::AccountId::ZERO,
-        )
-        .map_err(BuildTransactionError::UnifiedSpendKey)?;
 
         // We don't support zip320 yet. Only one step.
         if proposal.steps().len() != 1 {
@@ -157,7 +150,11 @@ impl LightWallet {
             &self.transaction_context.config.chain,
             &sapling_prover,
             &sapling_prover,
-            &unified_spend_key,
+            &self
+                .transaction_context
+                .key
+                .unified_key_store()
+                .try_into()?,
             zcash_client_backend::wallet::OvkPolicy::Sender,
             proposal,
             Some(self.wallet_capability().first_sapling_address()),
