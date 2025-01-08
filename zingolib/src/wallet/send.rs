69f913ff88f2545f35380a66aaa1199c11f485f3
fluidvanadium <fluidvanadium@zingolabs.org>
2025-01-08 13:21:38 +0000

    get_send_result now matches sibling functions

--- zingolib/src/wallet/send.rs
+++ zingolib/src/wallet/send.rs
@@ -55,7 +55,7 @@ impl LightWallet {
     }
 
     /// Get the current sending status.
-    pub async fn get_send_progress(&self) -> SendProgress {
+    pub async fn get_send_result(&self) -> SendProgress {
         self.send_progress.read().await.clone()
     }
 }
