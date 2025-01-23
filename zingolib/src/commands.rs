f05ce54a02f99b34d4368b1b466c2fa793477058
zancas <zancas@zingolabs.org>
2025-01-22 20:12:42 -0700

    exclude 0 arg case

--- zingolib/src/commands.rs
+++ zingolib/src/commands.rs
@@ -242,7 +242,7 @@ impl Command for ParseAddressCommand {
     }
 
     fn exec(&self, args: &[&str], _lightclient: &LightClient) -> String {
-        if args.len() > 1 {
+        if args.len() > 1 || args.len() == 0 {
             return self.help().to_string();
         }
         fn make_decoded_chain_pair(
