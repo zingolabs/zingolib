65897a2689de6b6167052fab3c5f68a282784825
fluidvanadium <fluidvanadium@zingolabs.org>
2025-02-02 09:13:49 +0000

    comment

--- libtonode-tests/tests/chain_generics.rs
+++ libtonode-tests/tests/chain_generics.rs
@@ -26,6 +26,7 @@ proptest::proptest! {
      }
 }
 
+// extra mod. to be removed, and tests coalesced
 mod chain_generics {
     use zcash_client_backend::PoolType::Shielded;
     use zcash_client_backend::PoolType::Transparent;
