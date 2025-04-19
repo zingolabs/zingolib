7665436656429d5afbaf416e90f4f448c2b31843
Oscar Pepper <oscarpepper@zingolabs.org>
2025-04-19 08:28:31 +0100

    add deprecation confirmation to zcashd conf

--- zingolib/src/testutils/scenarios/config_templaters.rs
+++ zingolib/src/testutils/scenarios/config_templaters.rs
@@ -69,6 +69,8 @@ rpcallowip=127.0.0.1
 # https://zcash.readthedocs.io/en/latest/rtd_pages/zcash_conf_guide.html#zcash-conf-guide
 listen=0
 
+i-am-aware-zcashd-will-be-replaced-by-zebrad-and-zallet-in-2025=1
+
 {extra}"
             )
     }
