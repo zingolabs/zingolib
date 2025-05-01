469faebab4a33e6efc9dd84235e7412948210db7
Oscar Pepper <oscarpepper@zingolabs.org>
2025-05-01 19:00:40 +0100

    turn off tracing

--- zingocli/src/main.rs
+++ zingocli/src/main.rs
@@ -4,6 +4,5 @@ pub fn main() {
     if let Err(e) = rustls::crypto::ring::default_provider().install_default() {
         eprintln!("Error installing crypto provider: {:?}", e)
     };
-    tracing_subscriber::fmt().init();
     zingo_cli::run_cli();
 }
