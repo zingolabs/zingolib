b47e331d286aaef0c29e8c0b2c70d0dae1680057
dorianvp <dorianvp@zingolabs.org>
2025-09-11 17:58:57 -0300

    format: `cargo-fmt`

--- zingolib/src/wallet/network.rs
+++ zingolib/src/wallet/network.rs
@@ -44,39 +44,37 @@ impl From<ActivationHeights> for ZingolibLocalNetwork {
             inner: zcash_protocol::local_consensus::LocalNetwork {
                 overwinter: Some(
                     activation_heights
-                            .activation_height(
-                                zcash_protocol::consensus::NetworkUpgrade::Overwinter,
-                            )
-                            .unwrap_or(1.into()),
+                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Overwinter)
+                        .unwrap_or(1.into()),
                 ),
                 sapling: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Sapling)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Sapling)
+                    .unwrap_or(1.into())
+                    .into(),
                 blossom: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Blossom)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Blossom)
+                    .unwrap_or(1.into())
+                    .into(),
                 heartwood: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Heartwood)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Heartwood)
+                    .unwrap_or(1.into())
+                    .into(),
                 canopy: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Canopy)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Canopy)
+                    .unwrap_or(1.into())
+                    .into(),
                 nu5: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu5)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu5)
+                    .unwrap_or(1.into())
+                    .into(),
                 nu6: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu6)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu6)
+                    .unwrap_or(1.into())
+                    .into(),
                 nu6_1: activation_heights
-                        .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu6_1)
-                        .unwrap_or(1.into())
-                .into(),
+                    .activation_height(zcash_protocol::consensus::NetworkUpgrade::Nu6_1)
+                    .unwrap_or(1.into())
+                    .into(),
             },
         }
     }
