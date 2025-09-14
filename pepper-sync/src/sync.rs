2a8fffbdbca074756a0ed9c56167c05ecb615dd6
dorianvp <dorianvp@zingolabs.org>
2025-09-14 20:32:24 -0300

    fix(`pepper-sync`): clamp percentage values

--- pepper-sync/src/sync.rs
+++ pepper-sync/src/sync.rs
@@ -633,11 +633,12 @@ where
 
     let session_blocks_scanned =
         total_blocks_scanned - sync_state.initial_sync_state.previously_scanned_blocks;
-    let percentage_session_blocks_scanned = (session_blocks_scanned as f32
+    let percentage_session_blocks_scanned = ((session_blocks_scanned as f32
         / (total_blocks - sync_state.initial_sync_state.previously_scanned_blocks) as f32)
-        * 100.0;
+        * 100.0)
+        .clamp(0.0, 100.0);
     let percentage_total_blocks_scanned =
-        (total_blocks_scanned as f32 / total_blocks as f32) * 100.0;
+        ((total_blocks_scanned as f32 / total_blocks as f32) * 100.0).clamp(0.0, 100.0);
 
     let session_sapling_outputs_scanned = total_sapling_outputs_scanned
         - sync_state
@@ -654,11 +655,12 @@ where
         + sync_state
             .initial_sync_state
             .previously_scanned_orchard_outputs;
-    let percentage_session_outputs_scanned = (session_outputs_scanned as f32
+    let percentage_session_outputs_scanned = ((session_outputs_scanned as f32
         / (total_outputs - previously_scanned_outputs) as f32)
-        * 100.0;
+        * 100.0)
+        .clamp(0.0, 100.0);
     let percentage_total_outputs_scanned =
-        (total_outputs_scanned as f32 / total_outputs as f32) * 100.0;
+        ((total_outputs_scanned as f32 / total_outputs as f32) * 100.0).clamp(0.0, 100.0);
 
     Ok(SyncStatus {
         scan_ranges: sync_state.scan_ranges.clone(),
