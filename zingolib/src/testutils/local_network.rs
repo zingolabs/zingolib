dcc8708aaeb8266f7c92fea9970d088122f7ae54
zancas <zancas@zingolabs.org>
2025-09-17 21:28:50 -0700

    remove redundant gates

--- zingolib/src/testutils/local_network.rs
+++ zingolib/src/testutils/local_network.rs
@@ -1,6 +1,5 @@
 //! Local network configuration for testing
 use zcash_protocol::consensus::{BlockHeight, Parameters};
-#[cfg(any(test, feature = "test-elevation"))]
 use zingo_infra_services::network::ActivationHeights;
 
 /// A struct representing a Local Network.
@@ -40,7 +39,6 @@ impl Default for ZingolibLocalNetwork {
     }
 }
 
-#[cfg(any(test, feature = "test-elevation"))]
 impl From<ActivationHeights> for ZingolibLocalNetwork {
     fn from(activation_heights: ActivationHeights) -> Self {
         ZingolibLocalNetwork {
@@ -83,7 +81,6 @@ impl From<ActivationHeights> for ZingolibLocalNetwork {
     }
 }
 
-#[cfg(any(test, feature = "test-elevation"))]
 impl From<ZingolibLocalNetwork> for ActivationHeights {
     fn from(zingolib_local_network: ZingolibLocalNetwork) -> Self {
         ActivationHeights::new(zingolib_local_network.inner)
