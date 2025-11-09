dd4c8e8d183244db26fc24c21c97231d64104079
fluidvanadium <fluidvanadium@zingolabs.org>
2025-11-09 20:30:29 +0000

    Minimum test.

--- zingo_wallet/tests/creation.rs
+++ zingo_wallet/tests/creation.rs
@@ -3,7 +3,7 @@ use zingo_wallet::ZingoWallet;
 use zingolib::config::DEFAULT_TESTNET_LIGHTWALLETD_SERVER;
 
 #[tokio::test]
-async fn create_connect_get_height() {
+async fn create_default() {
     let mut wallet = ZingoWallet::new_wallet().await;
     let single_server_url = DEFAULT_TESTNET_LIGHTWALLETD_SERVER;
     wallet
@@ -14,8 +14,8 @@ async fn create_connect_get_height() {
         .add_server(single_server_url.to_string())
         .await
         .unwrap();
-    wallet
-        .get_max_scanned_height_for_server(single_server_url.to_string())
-        .await
-        .unwrap();
+    // wallet
+    //     .get_max_scanned_height_for_server(single_server_url.to_string())
+    //     .await
+    //     .unwrap();
 }
