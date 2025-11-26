003424088e36822e68d6fd53aa86f1d2771b9d05
dorianvp <dorianvp@zingolabs.org>
2025-11-26 14:02:06 -0300

    fix(`shardtree`): do not ignore `marks_removed`

--- pepper-sync/src/wallet/traits.rs
+++ pepper-sync/src/wallet/traits.rs
@@ -370,8 +370,8 @@ where
             })
             .expect("infallible");
 
-        let tree_state = if let Some(checkpoint) = previous_checkpoint {
-            checkpoint.tree_state()
+        if let Some(prev) = previous_checkpoint {
+            prev
         } else {
             let frontiers =
                 client::get_frontiers(fetch_request_sender.clone(), checkpoint_height - 1).await?;
@@ -379,14 +379,14 @@ where
                 ShieldedProtocol::Sapling => frontiers.final_sapling_tree().tree_size(),
                 ShieldedProtocol::Orchard => frontiers.final_orchard_tree().tree_size(),
             };
-            if tree_size == 0 {
+            let tree_state = if tree_size == 0 {
                 TreeState::Empty
             } else {
                 TreeState::AtPosition(incrementalmerkletree::Position::from(tree_size - 1))
-            }
-        };
+            };
 
-        Checkpoint::from_parts(tree_state, BTreeSet::new())
+            Checkpoint::from_parts(tree_state, BTreeSet::new())
+        }
     };
 
     shard_tree
