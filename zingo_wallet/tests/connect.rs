7b12ecf934af48c48c2d7813c05e206ca34024c5
fluidvanadium <fluidvanadium@zingolabs.org>
2026-01-16 04:21:21 +0000

    polishing tests

--- zingo_wallet/tests/connect.rs
+++ zingo_wallet/tests/connect.rs
@@ -4,29 +4,33 @@ use zingo_wallet::ZingoWallet;
 use zingolib::testutils::chain_generics::conduct_chain::ConductChain;
 use zingolib::testutils::chain_generics::networked::TestnetEnvironment;
 
-async fn connect<CC>()
-where
-    CC: ConductChain,
-{
-    let chain = CC::setup().await;
+async fn connect(single_server_url: String) {
     let mut wallet = ZingoWallet::new_wallet().await;
     wallet
         .add_key(zingo_test_vectors::seeds::ABANDON_ART_SEED.to_owned())
         .await
         .unwrap();
     wallet
-        .begin_scanning_server_range(chain.lightserver_uri().unwrap().to_string(), None, None)
+        .begin_scanning_server_range(single_server_url, None, None)
         .await
         .unwrap();
+}
+
+async fn connect_fixture<CC>()
+where
+    CC: ConductChain,
+{
+    let chain = CC::setup().await;
+    connect(chain.lightserver_uri().unwrap().to_string()).await;
 }
 
 #[tokio::test]
 #[test_log::test]
 async fn regtest() {
-    connect::<RegtestEnvironment>().await;
+    connect_fixture::<RegtestEnvironment>().await;
 }
 #[tokio::test]
 #[test_log::test]
 async fn testnet() {
-    connect::<TestnetEnvironment>().await;
+    connect_fixture::<TestnetEnvironment>().await;
 }
--- zingo_wallet/tests/scan.rs
+++ zingo_wallet/tests/scan.rs
@@ -6,12 +6,7 @@ use zingo_wallet::ZingoWallet;
 use zingolib::testutils::chain_generics::conduct_chain::ConductChain;
 use zingolib::testutils::chain_generics::networked::TestnetEnvironment;
 
-async fn sleepy_scan<CC>(duration: usize)
-where
-    CC: ConductChain,
-{
-    let chain = CC::setup().await;
-    let single_server_url = chain.lightserver_uri().unwrap().to_string();
+async fn sleepy_scan(single_server_url: String, duration: u64) {
     let mut wallet = ZingoWallet::new_wallet().await;
     wallet
         .add_key(zingo_test_vectors::seeds::ABANDON_ART_SEED.to_string())
@@ -26,7 +21,7 @@ where
         .await
         .unwrap();
     tracing::debug!("{initial_scan_height:#?}");
-    tokio::time::sleep(Duration::from_secs(10)).await;
+    tokio::time::sleep(Duration::from_secs(duration)).await;
     let later_scan_height = wallet
         .get_max_scanned_height_for_server(single_server_url.to_string())
         .await
@@ -35,25 +30,28 @@ where
     assert!(initial_scan_height <= later_scan_height);
 }
 
+async fn sleepy_scan_fixture<CC>(duration: u64)
+where
+    CC: ConductChain,
+{
+    let chain = CC::setup().await;
+    sleepy_scan(chain.lightserver_uri().unwrap().to_string(), duration).await;
+}
+
 #[tokio::test]
 #[test_log::test]
 async fn regtest_10_seconds() {
-    sleepy_scan::<RegtestEnvironment>(10).await;
+    sleepy_scan_fixture::<RegtestEnvironment>(10).await;
 }
 
 #[tokio::test]
 #[test_log::test]
 async fn testnet_10_seconds() {
-    sleepy_scan::<TestnetEnvironment>(10).await;
+    sleepy_scan_fixture::<TestnetEnvironment>(10).await;
 }
 
 // NOTE: zingolib doesnt stop scanning, this test just ends when the specified height is reached.
-async fn scan_wallet_range<CC>(seed: String, start: u32, end: u32)
-where
-    CC: ConductChain,
-{
-    let chain = CC::setup().await;
-    let single_server_url = chain.lightserver_uri().unwrap().to_string();
+async fn scan_wallet_range(single_server_url: String, seed: String, start: u32, end: u32) {
     let mut wallet = ZingoWallet::new_wallet().await;
     wallet.add_key(seed).await.unwrap();
     wallet
