bd70749738730acd071150201ec3f2d076d1a32f
zancas <zancas@zingolabs.org>
2026-02-16 20:34:25 -0800

    minor formatting tweaks

--- pepper-sync/src/error.rs
+++ pepper-sync/src/error.rs
@@ -4,8 +4,8 @@ use std::{array::TryFromSliceError, convert::Infallible};
 
 use shardtree::error::ShardTreeError;
 use zcash_primitives::{block::BlockHash, transaction::TxId};
-use zcash_protocol::consensus::BlockHeight;
 use zcash_protocol::PoolType;
+use zcash_protocol::consensus::BlockHeight;
 
 use crate::wallet::OutputId;
 
