d31a00dda5208324a714ead8b692bd5d54550404
al amoda <alamoda@tutamail.com>
2026-02-23 10:32:55 -0500

    entrypoint.sh overhaul: simple sync, version pint, info, tail for persistance

--- utils/entrypoint.sh
+++ utils/entrypoint.sh
@@ -17,62 +17,9 @@
 
 set -eo pipefail
 
-# Use setpriv to drop privileges and execute the given command as the specified UID:GID
-exec_as_user() {
-  user=$(id -u)
-  if [[ ${user} == '0' ]]; then
-    which setpriv
-    ls -la /usr/bin/setpriv
-    ldd /usr/bin/setpriv
-    file /usr/bin/setpriv
-    readelf -l /usr/bin/setpriv
-    /usr/bin/setpriv -d
-    /usr/bin/setpriv --help
-    /usr/priv/setpriv --reuid "${UID}" --regid "${GID}" --init-groups "$@"
-  else
-    exec "$@"
-  fi
-}
-
-# Helper function
-exit_error() {
-  echo "$1" >&2
-  exit 1
-}
-
-# Creates a default wallet directory if it doesn't exist (If the directory
-# already exists, it does nothing) and sets ownership to specified UID:GID.
+# Currently there is no support for running tests in-container, due to
+# requiring additional binaries.
 #
-# ## Parameters
-#
-# - $1: Directory path to create and own
-create_owned_directory() {
-  local dir="$1"
-  # Skip if directory is empty
-  [[ -z ${dir} ]] && return
-
-  # Create directory with parents
-  mkdir -p "${dir}" || exit_error "Failed to create directory: ${dir}"
-
-  # Set ownership for the created directory
-  chown -R "${UID}:${GID}" "${dir}" || exit_error "Failed to secure directory: ${dir}"
-
-  ls -la /usr/local/bin
-  ls -la /usr/local/bin/wallets
-
-  # Set ownership for parent directory (but not if it's root or home)
-  local parent_dir
-  parent_dir="$(dirname "${dir}")"
-  if [[ "${parent_dir}" != "/" && "${parent_dir}" != "${HOME}" ]]; then
-    chown "${UID}:${GID}" "${parent_dir}"
-  fi
-
-}
-
-whoami
-# Create and own wallet directory
-[[ -n /usr/local/bin/wallets ]] && create_owned_directory "/usr/local/bin/wallets"
-
 # Main Script Logic
 #
 # 1. Print environment variables and config for debugging
@@ -81,14 +28,14 @@ whoami
 echo "INFO: Using the following environment variables:"
 printenv
 
-# - If "$1" is "zingo-cli", run `zingo-cli` with the remaining provided params.
-# - If "$1" is not "zingo-cli" run "$@" directly.
-if [[ "$1" == "zingo-cli" ]]; then
-  shift
-  exec_as_user zingo-cli "$@"
-else
-  exec_as_user "$@"
-fi
+echo "starting zingo-cli. A wallet will be created in this container if there is none. A version will be printed after sync."
+./zingo-cli --server https://zzz.stripest.online:443 --waitsync version
 
-# Currently there is no support for running tests in-container, due to
-# requiring additional binaries.
+# Keep container running for re-attach
+echo "--  This container has succeeded in making a wallet!"
+echo "Use 'docker exec -it <container> /bin/sh' in another active terminal for manual use."
+# TODO add handle for manual commands
+echo "'tail' executing which will keep this container running until stopped."
+echo "3 SIGTERM/SIGINTs will forcefully exit."
+echo "Restarting the same container will re-sync, but retain the existing wallet."
+exec tail -f /dev/null
