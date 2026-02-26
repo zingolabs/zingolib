a1958170c3c713f90a5f49e03cbd4464e5c3ac75
al amoda <alamoda@tutamail.com>
2026-02-26 08:44:37 -0500

    correct load_image.sh

--- utils/load_image.sh
+++ utils/load_image.sh
@@ -4,14 +4,15 @@ set -e
 
 REPO_ROOT="$(git rev-parse --show-toplevel)"
 OCI_OUTPUT="$REPO_ROOT/build/oci"
+TARBALL="${OCI_OUTPUT}/zingo-cli.tar"
 
 # Build runtime image for docker run
 echo "Checking if the OCI output from build is present."
-if [ -f $OCI_OUTPUT/zingo.cli.tar ];
+if [ -f "$TARBALL" ];
 then
-  echo "OCI output file not present."
-else
   echo "OCI output file present, loading tar file into local docker image store."
-  docker load < $OCI_OUTPUT/zingo-cli.tar
+  docker load < $TARBALL
   echo "...Done!"
+else
+  echo "OCI output file not present."
 fi
