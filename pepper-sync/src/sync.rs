1e35e9c718eda400b02864dc6ba6a2fa9eff94e9
Oscar Pepper <oscar.pepper1@gmail.com>
2026-06-12 18:34:14 +0100

    change mempool counter from atomic u8 to u32

--- pepper-sync/src/sync.rs
+++ pepper-sync/src/sync.rs
@@ -3,7 +3,7 @@
 use std::collections::{BTreeMap, HashMap};
 use std::ops::Range;
 use std::sync::Arc;
-use std::sync::atomic::{self, AtomicBool, AtomicU8};
+use std::sync::atomic::{self, AtomicBool, AtomicU8, AtomicU32};
 use std::time::{Duration, SystemTime};
 
 use tokio::sync::{RwLock, mpsc};
@@ -345,7 +345,7 @@ where
     let (mempool_transaction_sender, mut mempool_transaction_receiver) = mpsc::channel(100);
     let shutdown_mempool = Arc::new(AtomicBool::new(false));
     let shutdown_mempool_clone = shutdown_mempool.clone();
-    let unprocessed_mempool_transactions_count = Arc::new(AtomicU8::new(0));
+    let unprocessed_mempool_transactions_count = Arc::new(AtomicU32::new(0));
     let unprocessed_mempool_transactions_count_clone =
         unprocessed_mempool_transactions_count.clone();
     let mempool_handle = tokio::spawn(async move {
@@ -963,7 +963,7 @@ pub fn set_transactions_failed(
 /// Returns true if the scanner and mempool are shutdown.
 fn is_shutdown<P>(
     scanner: &Scanner<P>,
-    mempool_unprocessed_transactions_count: Arc<AtomicU8>,
+    mempool_unprocessed_transactions_count: Arc<AtomicU32>,
 ) -> bool
 where
     P: consensus::Parameters + Sync + Send + 'static,
@@ -1814,7 +1814,7 @@ where
 async fn mempool_monitor<C>(
     mut client: C,
     mempool_transaction_sender: mpsc::Sender<RawTransaction>,
-    unprocessed_transactions_count: Arc<AtomicU8>,
+    unprocessed_transactions_count: Arc<AtomicU32>,
     shutdown_mempool: Arc<AtomicBool>,
 ) -> Result<(), MempoolError>
 where
